8a95667b32a91ab7bac6ff417021eaabe7843824
Lukas Polacek <polacek@csc.kth.se>
2011-11-12 15:13:23 +0100

    Small fixes in templates and overflow fix in Chinese Remainder Theorem

--- content/number theory/chinese.h
+++ content/number theory/chinese.h
@@ -12,14 +12,13 @@
 #include "euclid.h"
 
 template <class Z> inline Z chinese(Z a, Z m, Z b, Z n) {
-	Z x, y;  euclid(m, n, x, y);
-	return (a * n * (y < 0 ? y + m : y) +
-		b * m * (x < 0 ? x + n : x)) % (m*n);
+	Z x, y; euclid(m, n, x, y);
+	return a * (y + m) % m * n + b * (x + n) % n * m;
 }
 
 template <class Z> inline Z chinese_common(Z a, Z m, Z b, Z n) {
 	Z d = gcd(m, n);
 	if (((b -= a) %= n) < 0) b += n;
 	if (b % d) return -1; // No solution
-	return d*chinese(0, m/d, b/d, n/d) + a;
+	return d * chinese(Z(0), m/d, b/d, n/d) + a;
 }
