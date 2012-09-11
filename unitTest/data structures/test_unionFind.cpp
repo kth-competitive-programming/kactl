b6f47683010449816a00242666f573c0a3147f37
Lukas Polacek <polacek@kth.se>
2012-09-11 17:55:49 +0200

    Unit tests

--- unitTest/data structures/test_unionFind.cpp
+++ unitTest/data structures/test_unionFind.cpp
@@ -20,15 +20,22 @@ public:
 			if (u.same_set(0, 2))
 				fail("0 and 2 should not be in the same set");
 			u.join(0, 2);
+			if (u.size(2) != 3)
+				fail("The size of the set where 2 is should be 3");
 			if (!u.same_set(0, 2))
 				fail("0 and 2 should be in the same set");
 			if (u.same_set(3, 2))
 				fail("3 and 2 should not be in the same set");
+			if (u.size(3) != 1)
+				fail("The size of the set where 3 is should be 1");
 			u.join(3, 4);
 			if (!u.same_set(3, 4))
 				fail("3 and 4 should be in the same set");
 			if (u.same_set(4, 2))
 				fail("4 and 2 should not be in the same set");
+			if (u.size(3) != 2)
+				fail("The size of the set where 3 is should be 2");
+
 		}
 	}
 
