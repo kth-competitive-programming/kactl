d8ef0935a1de70b1c752f23a2897f0c0df9c419a
Johan Sannemo <johan.sannemo@gmail.com>
2014-11-28 15:14:00 +0100

    Add interval cover

--- content/various/intervalCover.h
+++ content/various/intervalCover.h
@@ -12,30 +12,30 @@
 #include <vector>
 using namespace std;
 
-template<typename It> struct Cmp {
-	bool operator()(const It& first, const It& second) const {
-		return (*first) < (*second);
-	}
+template<typename It> struct Cmp {                                                                        
+    bool operator()(const It& first, const It& second) const {
+        return (*first) < (*second);
+    }   
 };
 
-template<typename C> vi intervalCover(pair<C, C> toCover, const vector<pair<C, C> >& is) {
-	vector<const pair<C, C>*> sorted;
-	vi answer;
-	const C* nextCover = &(toCover.first);
-	sorted.reserve(is.size());
-	rep(i,0,is.size() sorted.push_back(&is[i]);
-	sort(sorted.begin(), sorted.end(), Cmp<const pair<C, C>*>());
-	int scan = 0;
-	while(answer.empty() || *nextCover < toCover.second) {
-		int best = 1<<29;
-		while(scan < sorted.size() && sorted[scan]->first <= *nextCover) {
-			if(best == (1<<29) || sorted[scan]->second > is[best].second)
-				best = sorted[scan] - &is[0];
-			++scan;
-		}
-		if(best == (size_t)-1) return vi;
-		answer.push_back(best);
-		nextCover = &is[best].second;
-	}
-	return answer;
+template<typename C> vi intervalCover(pair<C, C> toCover, vector<pair<C, C> >& is) {
+    vector<typeof(is.begin())> sorted;
+    vi answer;
+    C nextCover = toCover.first;
+    sorted.reserve(is.size());
+    rep(i,0,is.size()) sorted.push_back(is.begin() + i); 
+    sort(sorted.begin(), sorted.end(), Cmp<typeof(is.begin())>());
+    int scan = 0;
+    while(answer.empty() || nextCover < toCover.second) {
+        int best = 1<<29;
+        while(scan < sorted.size() && sorted[scan]->first <= nextCover ) { 
+            if(best == (1<<29) || sorted[scan]->second > is[best].second)
+                best = sorted[scan] - is.begin();
+            ++scan;
+        }   
+        if(best == (1<<29)) return vi();
+        answer.push_back(best);
+        nextCover = is[best].second;
+    }   
+    return answer;
 }
