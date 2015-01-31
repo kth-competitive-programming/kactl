6117044cda1f58792d284ecfdda532b3d7bee7db
Simon Lindholm <simon.lindholm10@gmail.com>
2015-01-31 21:54:04 +0300

    Simplify interval cover

--- content/various/intervalCover.h
+++ content/various/intervalCover.h
@@ -12,28 +12,29 @@
 #include <vector>
 using namespace std;
 
-template<typename It> struct Cmp {                                                                        
+template<typename It> struct Cmp {
     bool operator()(const It& first, const It& second) const {
         return (*first) < (*second);
-    }   
+    }
 };
 
 template<typename C> vi intervalCover(pair<C, C> toCover, vector<pair<C, C> >& is) {
-    vector<typeof(is.begin())> sorted;
+    typedef typeof(is.begin()) It;
+    vector<It> sorted;
     vi answer;
     C nextCover = toCover.first;
     sorted.reserve(is.size());
-    rep(i,0,is.size()) sorted.push_back(is.begin() + i); 
-    sort(sorted.begin(), sorted.end(), Cmp<typeof(is.begin())>());
+    rep(i,0,is.size()) sorted.push_back(is.begin() + i);
+    sort(sorted.begin(), sorted.end(), Cmp<It>());
     int scan = 0;
-    while(answer.empty() || nextCover < toCover.second) {
+    while (answer.empty() || nextCover < toCover.second) {
         int best = 1<<29;
-        while(scan < sorted.size() && sorted[scan]->first <= nextCover ) { 
-            if(best == (1<<29) || sorted[scan]->second > is[best].second)
+        while (scan < sorted.size() && sorted[scan]->first <= nextCover) {
+            if (best == (1<<29) || sorted[scan]->second > is[best].second)
                 best = sorted[scan] - is.begin();
             ++scan;
         }   
-        if(best == (1<<29)) return vi();
+        if (best == (1<<29)) return vi();
         answer.push_back(best);
         nextCover = is[best].second;
     }   
