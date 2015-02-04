2f42a1bd10831807253a40a6af045a21d61c838a
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-04 17:22:19 +0300

    Remove Simplex, add back comments to it
    for the next time someone tries to understand it.

--- content/numerical/Simplex.h
+++ content/numerical/Simplex.h
@@ -11,6 +11,7 @@ enum Result { OK, UNBOUNDED, NO_SOLUTION };
 template <class M, class I>
 Result simplex(M &a, I &var, int m, int n, int twophase=0) {
 	while (true) {
+		/// Choose a variable to enter the basis
 		int idx = 0;
 		rep(j,1,n+1)
 			if (a[0][j] > 0 && (!idx || a[0][j] > a[0][idx]))
@@ -20,6 +21,7 @@ Result simplex(M &a, I &var, int m, int n, int twophase=0) {
 		rep(i,1,m)
 			if (a[i][j] > 0 && (!idx || a[i][0]/a[i][j] < a[idx][0]/a[idx][j]))
 				idx = i;
+		/// Problem unbounded if all a[m][j] <= 0
 		if (!idx) return UNBOUNDED;
 		int i = idx;
 		for (int l = 0; l <= n; ++l)
@@ -30,19 +32,25 @@ Result simplex(M &a, I &var, int m, int n, int twophase=0) {
 				if (l != j) a[k][l] -= a[k][j] * a[i][l];
 			a[k][j] = 0;
 		}
+		/// Keep track of the variable change.
+		/// XXX this doesn't seem to be used? it is meaningful as output?
 		var[i] = j;
 	}
 }
 template <class M, class I>
 Result twophase_simplex(M &a, I &var, int m, int n, int artificial) {
+	/// Save primary objective, clear phase I objective
 	rep(j,0,n+artificial+1)
 		a[m + 1][j] = a[0][j], a[0][j] = 0;
+	/// Express phase I objective in terms of non-basic variables
 	rep(i,1,m+1) rep(j,n+1,n+artificial+1)
 			if (a[i][j] == 1)
 				rep(l,0,n+1)
 					if (l != j) a[0][l] += a[i][l];
-						simplex(a, var, m, n + artificial, 1); 
+	simplex(a, var, m, n + artificial, 1); /// Phase I
+	/// Check solution
 	rep(j,n+1,n+artificial+1) if (a[0][j] >= 0) return NO_SOLUTION;
+	/// Restore primary objective
 	rep(j,0,n+1) a[0][j] = a[m + 1][j];
 	return simplex(a, var, m, n);
 }
