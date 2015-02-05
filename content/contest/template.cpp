51af322a8bbbf55df5d5733c1c951c8a673e68d5
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-05 18:58:53 +0300

    Update template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = (a); i < int(b); ++i)
-#define trav(it, v) for(auto it = (v).begin(); \
-		it != (v).end(); ++it)
-#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(int i = a; i < int(b); ++i)
+#define trav(it, v) for(auto it = v.begin(); \
+		it != v.end(); ++it)
+#define all(x) x.begin(), x.end()
 
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
-
-void solve() {
-}
+int cin;
 
 int main() {
-	solve();
+	int N;
+	ignore = scanf("%d", &N);
 }
