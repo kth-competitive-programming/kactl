7702149ff2546fc1e385fef9d6e623e999cbd1d6
Johan Sannemo <johan.sannemo@gmail.com>
2015-02-08 22:52:57 +0100

    Remove old flows and prettify the min-cost max flow

---
+++ content/graph/MinCostMaxFlow.h
@@ -0,0 +1,73 @@
+/**
+ * Author: Unknown, Johan Sannemo
+ * Date: 2009-04-17
+ * Source: tinyKACTL
+ * Description: Min-cost max-flow
+ * Status: Tested
+ */
+#pragma once
+#include <vector>
+using namespace std;
+
+typedef int Flow;
+
+struct FlowEdge {
+	int dest, back;
+	Flow c, f, cost; 
+	Flow r() { return c - f; } 
+	FlowEdge(int d, int b, Flow c, Flow cost = 0)
+		: dest(d), back(b), c(c), f(0), cost(cost) {}
+};
+
+template<class G>
+void flow_add_edge(G& g, int s, int t,
+		Flow c, Flow cost = 0) {
+	g[s].push_back(FlowEdge(t, g[t].size(), c, cost));
+	g[t].push_back(FlowEdge(s, g[s].size() - 1, 0, cost));
+}
+
+Flow inf = 1<<28;
+
+template<class G>
+pair<Flow, int> aug(G &g, int s, int t) {
+	int n = g.size();
+	vector<int> mark(n, -1);
+	vector<Flow> mindist(n, inf);
+	bool changed = true; mindist[s] = 0;
+	for (int i = 1; !(changed = !changed); ++i)
+		for (int v = 0; v < n; ++v)
+			trav(it, g[v]) {
+				Flow dist =mindist[v]+(it->f<0 ? -it->cost : it->cost);
+				if (it->r() > 0 && dist < mindist[it->dest]) {
+					if (i >= n) assert(0);// negative cycle! shouldn't be
+					mindist[it->dest] = dist;
+					mark[it->dest] = it->back;
+					changed = true;
+				}
+			}
+	if (mark[t] < 0) return make_pair(0, 0);
+	Flow inc = inf;
+	FlowEdge* e; int v = t;
+	while (v != s){
+		e = &g[v][mark[v]];
+		v = e->dest;
+		inc = min(inc, g[v][e->back].r());
+	}
+	v = t;
+	while (v != s)
+		e = &g[v][mark[v]], e->f -= inc,
+		  v = e->dest, g[v][e->back].f += inc;
+	return make_pair(inc, inc * mindist[t]);
+}
+
+template<class G>
+pair<Flow, int> min_cost_max_flow(G& graph, int s, int t) {
+	pair<Flow, int> flow, inc;
+	while ((inc = aug(graph, s, t)).first){
+		flow.first += inc.first;
+		flow.second += inc.second;
+	}
+	return flow;
+}
+
+
