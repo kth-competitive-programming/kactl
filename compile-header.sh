09b4797c6baa68e7dcac7800b5d1cacd7c7a7958
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-10 20:54:24 +0100

    Add a script for checking that header files compile

---
+++ compile-header.sh
@@ -0,0 +1,6 @@
+FILE="$1"
+echo "
+#include \"../content/contest/template.cpp\"
+#include \"../$FILE\"
+" >build/temp.cpp
+g++ -Wall -Wextra -Wconversion -std=c++11 build/temp.cpp && rm a.out build/temp.cpp
