2ba8792b552a8035f5b9d002d34022a57f320be5
Johan Sannemo <johan.sannemo@gmail.com>
2015-02-10 10:17:22 +0100

    improve tree jumps

--- content/graph/TreePower.h
+++ content/graph/TreePower.h
@@ -9,14 +9,17 @@
  */
 
 vector<vi> treeJump(vi& P){
-	int on = 1, depth = 1;
-	while(on < (int)P.size()) on *= 2, depth++;
-	vector<vi> jmp;
-	jmp.assign(depth, vi(P.size()));
+	int on = 1, d = 1;
+	while(on < (int)P.size()) on *= 2, d++;
+	vector<vi> jmp(d, vi(P.size()));
 	jmp[0] = P;
-	rep(i,1,depth) rep(j,0,P.size())
+	rep(i,1,d) rep(j,0,P.size())
 		jmp[i][j] = jmp[i-1][jmp[i-1][j]];
 	return jmp;
 }
 
-
+int jmp(vector<vi>& tbl, int nod, int steps){
+	rep(i,0,tbl.size())
+		if(steps&(1<<i)) nod = tbl[i][nod];
+	return nod;
+}
