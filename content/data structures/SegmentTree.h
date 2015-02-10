0a409700edb5cc3362915c89679135310af90373
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-10 09:46:36 +0300

    Make segment tree bounds exclusive

--- content/data structures/SegmentTree.h
+++ content/data structures/SegmentTree.h
@@ -2,7 +2,7 @@
  * Author: Lukas Polacek
  * Date: 2009-10-26
  * Source: folklore
- * Description: Zero-indexed max-tree. Bounds are inclusive!
+ * Description: Zero-indexed max-tree. Bounds are exclusive!
  * Time: O(\log N).
  */
 #pragma once
@@ -23,7 +23,7 @@ struct Tree {
 		for (pos /= 2; pos >= 1; pos /= 2)
 			s[pos] = max(s[pos * 2], s[pos * 2 + 1]);
 	}
-	int query(int a, int b) { return que(1, a, b + 1, 0, n); }
+	int query(int a, int b) { return que(1, a, b, 0, n); }
 	int que(int pos, int a, int b, int x, int y) {
 		if (a >= b) return LOW;
 		if (a == x && b == y) return s[pos];
