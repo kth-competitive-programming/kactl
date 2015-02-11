8f0e29e13a0d950f357aeca8b3b79883a626f852
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-12 00:37:02 +0100

    Add integration algorithms

---
+++ content/numerical/Integrate.h
@@ -0,0 +1,20 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-02-11
+ * Source: Wikipedia
+ * Description: Simple integration of a function over an interval using
+ *  Simpson's rule. The error should be proportional to $h^4$, although in
+ *  practice you will want to verify that the result is stable to desired
+ *  precision when epsilon changes.
+ * Status: mostly untested
+ */
+#pragma once
+
+double quad(double (*f)(double), double a, double b) {
+	const int n = 1000;
+	double h = (b - a) / 2 / n;
+	double v = f(a) + f(b);
+	rep(i,1,n*2)
+		v += f(a + i*h) * (i&1 ? 4 : 2);
+	return v * h / 3;
+}
---
+++ content/numerical/IntegrateAdaptive.h
@@ -0,0 +1,31 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-02-11
+ * Source: Wikipedia
+ * Description: Fast integration using an adaptive Simpson's rule.
+ * Status: mostly untested
+ * Usage:
+	double z, y;
+	double h(double x) { return x*x + y*y + z*z <= 1; }
+	double g(double y) { ::y = y; return quad(h, -1, 1); }
+	double f(double z) { ::z = z; return quad(g, -1, 1); }
+	double sphereVol = quad(f, -1, 1), pi = sphereVol*3/4;
+ */
+#pragma once
+
+typedef double d;
+d simpson(d (*f)(d), d a, d b) {
+	d c = (a+b) / 2;
+	return (f(a) + 4*f(c) + f(b)) * (b-a) / 6;
+}
+d rec(d (*f)(d), d a, d b, d eps, d S) {
+	d c = (a+b) / 2;
+	d S1 = simpson(f, a, c);
+	d S2 = simpson(f, c, b), T = S1 + S2;
+	if (abs (T - S) <= 15*eps || b-a < 1e-10)
+		return T + (T - S) / 15;
+	return rec(f, a, c, eps/2, S1) + rec(f, c, b, eps/2, S2);
+}
+d quad(d (*f)(d), d a, d b, d eps = 1e-8) {
+	return rec(f, a, b, eps, simpson(f, a, b));
+}
