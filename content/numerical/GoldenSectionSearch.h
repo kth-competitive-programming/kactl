38f530dc2806075bc2efd06c8b4004b5d813fc97
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-11 23:57:26 +0100

    Tweak golden section search

--- content/numerical/GoldenSectionSearch.h
+++ content/numerical/GoldenSectionSearch.h
@@ -14,17 +14,17 @@
 
 #include <cmath>
 
-double gss(double a, double b, double (*f)(double), double e = 1e-6) {
-	double r = (sqrt(5)-1)/2; //=.618...=golden ratio-1
-	double x1 = b-r*(b-a), x2 = a+r*(b-a);
+double gss(double a, double b, double (*f)(double)) {
+	double r = .618, eps = 1e-7;
+	double x1 = b - r*(b-a), x2 = a + r*(b-a);
 	double f1 = f(x1), f2 = f(x2);
-	while (b-a > e) {
+	while (b-a > eps) {
 		if (f1 < f2) { //change to > to find maximum
 			b = x2; x2 = x1; f2 = f1;
-			x1 = b-r*(b-a); f1 = f(x1);
+			x1 = b - r*(b-a); f1 = f(x1);
 		} else {
 			a = x1; x1 = x2; f1 = f2;
-			x2 = a+r*(b-a); f2 = f(x2);
+			x2 = a + r*(b-a); f2 = f(x2);
 		}
 	}
 	return a;
