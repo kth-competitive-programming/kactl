974a0596d6d3c54679f159b7867e8d4bea6e3e84
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-21 13:59:55 +0100

    Reindent 2-sat

--- content/graph/2sat.h
+++ content/graph/2sat.h
@@ -18,69 +18,69 @@
 using namespace std;
 
 struct TwoSat {
-        vector<char> values; //0 = false, 1 = true
-        int N;
-        
-        struct Node {
-                vi ins, outs;
-                int comp_id, vis;
-                Node() : vis(0) {}
-        };
-        vector<Node> nodes;
-        
-        TwoSat(int n) : values(n), N(n), nodes(2*n) {}
-        
-        void add_clause(int a_index, bool a_value, int b_index, bool b_value){
-                int a = 2*a_index + a_value, b = 2*b_index + b_value;
-                nodes[a^1].outs.push_back(b), nodes[b].ins.push_back(a^1);
-                nodes[b^1].outs.push_back(a), nodes[a].ins.push_back(b^1);
+    vector<char> values; //0 = false, 1 = true
+    int N;
+
+    struct Node {
+        vi ins, outs;
+        int comp_id, vis;
+        Node() : vis(0) {}
+    };
+    vector<Node> nodes;
+
+    TwoSat(int n) : values(n), N(n), nodes(2*n) {}
+
+    void add_clause(int a_index, bool a_value, int b_index, bool b_value){
+        int a = 2*a_index + a_value, b = 2*b_index + b_value;
+        nodes[a^1].outs.push_back(b), nodes[b].ins.push_back(a^1);
+        nodes[b^1].outs.push_back(a), nodes[a].ins.push_back(b^1);
+    }
+
+    void set_value(int index, bool value){
+        add_clause(index, value, index, value);
+    }
+
+    struct Comp {
+        vi nodes;
+        bool value;
+        Comp() : value(0) {}
+    };
+    vector<Comp> comp;
+    vi st;
+
+    void dfs1(int v){
+        Node& n = nodes[v];
+        if (n.vis) return;
+        n.vis = 1;
+        for (int e : n.outs) dfs1(e);
+        st.push_back(v);
+    }
+
+    void dfs2(int v){
+        nodes[v].vis = 1;
+        nodes[v].comp_id = sz(comp)-1;
+        comp.back().nodes.push_back(v);
+        for (int e : nodes[v].ins) if (!nodes[e].vis) dfs2(e);
+    }
+
+    bool solve(){
+        st.reserve(2*N);
+        rep(i, 0, 2*N) dfs1(i);
+        rep(i, 0, 2*N) nodes[i].vis = 0;
+        while(!st.empty()){
+            if (!nodes[st.back()].vis){
+                comp.push_back(Comp());
+                dfs2(st.back());
+            }
+            st.pop_back();
         }
-        
-        void set_value(int index, bool value){
-                add_clause(index, value, index, value);
+        for (Comp& d : comp) {
+            if (d.value) continue;
+            Comp& c = comp[nodes[d.nodes.front()^1].comp_id];
+            if (&c == &d) return 0;
+            c.value = 1;
+            for (int e : d.nodes) values[e/2] = !(e%2);
         }
-        
-        struct Comp {
-                vi nodes;
-                bool value;
-                Comp() : value(0) {}
-        };
-        vector<Comp> comp;
-        vi st;
-        
-        void dfs1(int v){
-                Node& n = nodes[v];
-                if (n.vis) return;
-                n.vis = 1;
-                for (int e : n.outs) dfs1(e);
-                st.push_back(v);
-        }
-        
-        void dfs2(int v){
-                nodes[v].vis = 1;
-                nodes[v].comp_id = sz(comp)-1;
-                comp.back().nodes.push_back(v);
-                for (int e : nodes[v].ins) if (!nodes[e].vis) dfs2(e);
-        }
-        
-        bool solve(){
-                st.reserve(2*N);
-                rep(i, 0, 2*N) dfs1(i);
-                rep(i, 0, 2*N) nodes[i].vis = 0;
-                while(!st.empty()){
-                        if (!nodes[st.back()].vis){
-                                comp.push_back(Comp());
-                                dfs2(st.back());
-                        }
-                        st.pop_back();
-                }
-                for (Comp& d : comp) {
-                        if (d.value) continue;
-                        Comp& c = comp[nodes[d.nodes.front()^1].comp_id];
-                        if (&c == &d) return 0;
-                        c.value = 1;
-                        for (int e : d.nodes) values[e/2] = !(e%2);
-                }
-                return 1;
-        }
+        return 1;
+    }
 };
