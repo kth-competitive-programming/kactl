91e51f002573cd7f02a957417601819cfbe22277
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-23 01:10:30 +0100

    Improve/shorten bellmanFord2

--- content/graph/bellmanFord2.h
+++ content/graph/bellmanFord2.h
@@ -1,74 +1,42 @@
 /**
- * Author: Chen Xing
- * Date: 2009-10-13
+ * Author: Simon Lindholm
+ * Date: 2015-02-23
  * Source: http://en.wikipedia.org/wiki/Bellman-Ford_algorithm
- * Description: Calculates shortest path in a graph that might have negative edge distances. Propagates negative infinity distances.
- * Time: O(E * V) where E is the number of edges and V is the number of vertices.
- * Memory: O(1)
- * Status: Working
+ * Description: Calculates shortest path in a graph that might have negative edge distances.
+ * Propagates negative infinity distances (sets dist = -inf), and returns true
+ * if there is some negative cycle. Unreachable nodes get dist = inf.
+ * Time: O(EV)
+ * Status: Untested
  */
 #pragma once
 
 #include <vector>
 using namespace std;
 
-template <typename T>
-struct Edge {
-  int source;
-  int destination;
-  T weight;
-};
+typedef ll T; // or whatever
+struct Edge { int src, dest; T weight; };
+struct Node { T dist; int prev; };
+struct Graph { vector<Node> nodes; vector<Edge> edges; };
 
-template <typename T>
-struct Node {
-  T source_dist;
-  int prev_node;
-};
+const T inf = numeric_limits<T>::max();
+bool bellmanFord2(Graph& g, int start_node){
+	trav(n, g.nodes) { n.dist = inf; n.prev = -1; }
+	g.nodes[start_node].dist = 0;
 
-template <typename T>
-struct Graph {
-  vector<Node<T> > nodes;
-  vector<Edge<T> > edges;
-};
-
-template <typename T>
-void bellmanFord2(Graph<T> &graph, int start_node){
-  for (int i=0; i<graph.nodes.size(); i++){
-    graph.nodes[i].source_dist = numeric_limits<T>::max();
-    graph.nodes[i].prev_node = -1;
-  }
-
-  graph.nodes[start_node].source_dist = 0;
-
-  for (int i=0; i<graph.nodes.size(); i++){
-    for (int j=0; j<graph.edges.size(); j++){
-      Edge<T> &edge = graph.edges[j];
-      Node<T> &curr = graph.nodes[edge.source];
-      Node<T> &dest = graph.nodes[edge.destination];
-
-      if (curr.source_dist != numeric_limits<T>::max()){
-        T new_dist = curr.source_dist + edge.weight;
-        if (new_dist < dest.source_dist){
-          dest.source_dist = new_dist;
-          dest.prev_node = edge.source;
-          if (i >= graph.nodes.size()-1){
-            dest.prev_node = -2;
-          }
-        }
-      }
-    }
-  }
-
-  for (int i=0; i<graph.nodes.size(); i++){
-    for (int j=0; j<graph.edges.size(); j++){
-      Edge<T> &edge = graph.edges[j];
-      Node<T> &curr = graph.nodes[edge.source];
-	  Node<T> &dest = graph.nodes[edge.destination];
-
-	  if(curr.prev_node == -2) {
-		  dest.source_dist = -1;
-		  dest.prev_node = -2;
-	  }
+	rep(i,0,sz(g.nodes)) trav(e, g.edges) {
+		Node& cur = g.nodes[e.src];
+		Node& dest = g.nodes[e.dest];
+		if (cur.dist == inf) continue;
+		T ndist = cur.dist + (cur.dist == -inf ? 0 : e.weight);
+		if (ndist < dest.dist) {
+			dest.prev = e.src;
+			dest.dist = (i >= sz(g.nodes)-1 ? -inf : ndist);
+		}
 	}
-  }
+	bool ret = 0;
+	rep(i,0,sz(g.nodes)) trav(e, g.edges) {
+		if (g.nodes[e.src].dist == -inf)
+			g.nodes[e.dest].dist = -inf, ret = 1;
+	}
+	return ret;
 }
