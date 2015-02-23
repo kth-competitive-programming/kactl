898eacc949540ab8dba9e6324b9bf81a6a47c63c
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-23 01:16:36 +0100

    Remove bellmanFord, GraphDPAB, rename bellmanFord2 -> bellmanFord

--- content/graph/bellmanFord.h
+++ content/graph/bellmanFord.h
@@ -1,38 +1,42 @@
 /**
- * Author: Chen Xing
- * Date: 2009-04-10
+ * Author: Simon Lindholm
+ * Date: 2015-02-23
  * Source: http://en.wikipedia.org/wiki/Bellman-Ford_algorithm
- * Description: Calculates shortest path in a graph that might have negative edge distances. Returns true if negative-distance cycles exist.
+ * Description: Calculates shortest path in a graph that might have negative edge distances.
+ * Propagates negative infinity distances (sets dist = -inf), and returns true
+ * if there is some negative cycle. Unreachable nodes get dist = inf.
  * Time: O(EV)
- * Status: Unknown
- * Usage:
- *  typedef Graph<NodeDPAB<int>, EdgeDPAB<int> > G;
- *  G g; bool negCyc = bellmanFord(g, 10);
+ * Status: Untested
  */
 #pragma once
 
-#include "GraphDPAB.h"
+#include <vector>
+using namespace std;
 
-template<class G>
-bool bellmanFord(G& g, int src) {
-	trav(node, g.nodes) { node.srcDist = 0; node.srcPrev = -1; }
-	g.nodes[src].srcPrev = src;
-	rep(i, 0, g.nodes.size()) trav(e, g.edges) {
-		int a = e.a, b = e.b;
-		rep(j, 0, 1 + e.bidir) {
-			auto& cur = g.nodes[a];
-			if(cur.srcPrev != -1) {
-				auto nextDistNew = cur.srcDist + e.dist;
-				auto& nextDist = g.nodes[b].srcDist;
-				int& nextPrev = g.nodes[b].srcPrev;
-				if(nextPrev == -1 || nextDistNew < nextDist) {
-					if(i == sz(g.nodes) - 1) return true;
-					nextPrev = a;
-					nextDist = nextDistNew;
-				}
-			}
-			swap(a, b);
+typedef ll T; // or whatever
+struct Edge { int src, dest; T weight; };
+struct Node { T dist; int prev; };
+struct Graph { vector<Node> nodes; vector<Edge> edges; };
+
+const T inf = numeric_limits<T>::max();
+bool bellmanFord2(Graph& g, int start_node){
+	trav(n, g.nodes) { n.dist = inf; n.prev = -1; }
+	g.nodes[start_node].dist = 0;
+
+	rep(i,0,sz(g.nodes)) trav(e, g.edges) {
+		Node& cur = g.nodes[e.src];
+		Node& dest = g.nodes[e.dest];
+		if (cur.dist == inf) continue;
+		T ndist = cur.dist + (cur.dist == -inf ? 0 : e.weight);
+		if (ndist < dest.dist) {
+			dest.prev = e.src;
+			dest.dist = (i >= sz(g.nodes)-1 ? -inf : ndist);
 		}
 	}
-	return false;
+	bool ret = 0;
+	rep(i,0,sz(g.nodes)) trav(e, g.edges) {
+		if (g.nodes[e.src].dist == -inf)
+			g.nodes[e.dest].dist = -inf, ret = 1;
+	}
+	return ret;
 }
