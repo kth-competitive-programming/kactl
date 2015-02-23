844eb72b6d1da5e55724e36eac1017a2c69befbd
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-23 01:14:29 +0100

    Shorten bellmanFord
    Pointless given that I'll remove it, but possibly worth keeping in git
    history.

--- content/graph/bellmanFord.h
+++ content/graph/bellmanFord.h
@@ -3,13 +3,11 @@
  * Date: 2009-04-10
  * Source: http://en.wikipedia.org/wiki/Bellman-Ford_algorithm
  * Description: Calculates shortest path in a graph that might have negative edge distances. Returns true if negative-distance cycles exist.
- * Time: O(EV) where $E$ is the number of edges and $V$ is the number of vertices.
- * Memory: O(1)
+ * Time: O(EV)
  * Status: Unknown
  * Usage:
- *  typedef Graph<NodeDPAB<double>, EdgeDPAB<double> > G;
- *  G g;
- *  bool  = bellmanFord(g, 10);
+ *  typedef Graph<NodeDPAB<int>, EdgeDPAB<int> > G;
+ *  G g; bool negCyc = bellmanFord(g, 10);
  */
 #pragma once
 
@@ -17,28 +15,24 @@
 
 template<class G>
 bool bellmanFord(G& g, int src) {
-	typedef typename G::NodeT N;
-	typedef typename N::DistanceT D;
-
 	trav(node, g.nodes) { node.srcDist = 0; node.srcPrev = -1; }
 	g.nodes[src].srcPrev = src;
-	rep(i, 0, g.nodes.size())
-		trav(e, g.edges) {
-			int a = e.a, b = e.b;
-			rep(j, 0, 1 + e.bidir) {
-				N& cur = g.nodes[a];
-				if(cur.srcPrev != -1) {
-					D nextDistNew = cur.srcDist + e.dist;
-					D& nextDist = g.nodes[b].srcDist;
-					int& nextPrev = g.nodes[b].srcPrev;
-					if(nextPrev == -1 || nextDistNew < nextDist) {
-						if(i == (g.nodes.size() - 1)) return true;
-						nextPrev = a;
-						nextDist = nextDistNew;
-					}
+	rep(i, 0, g.nodes.size()) trav(e, g.edges) {
+		int a = e.a, b = e.b;
+		rep(j, 0, 1 + e.bidir) {
+			auto& cur = g.nodes[a];
+			if(cur.srcPrev != -1) {
+				auto nextDistNew = cur.srcDist + e.dist;
+				auto& nextDist = g.nodes[b].srcDist;
+				int& nextPrev = g.nodes[b].srcPrev;
+				if(nextPrev == -1 || nextDistNew < nextDist) {
+					if(i == sz(g.nodes) - 1) return true;
+					nextPrev = a;
+					nextDist = nextDistNew;
 				}
-				swap(a, b);
 			}
+			swap(a, b);
 		}
+	}
 	return false;
 }
