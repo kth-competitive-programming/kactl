1dc80aef2a9aaa89a4cba89573ef1ec841f85815
Simon Lindholm <simon.lindholm10@gmail.com>
2015-02-25 01:36:18 +0100

    Replace lift_to_front by a faster push-relabel max-flow algorithm

---
+++ content/graph/PushRelabel.h
@@ -0,0 +1,62 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-02-24
+ * Source: Wikipedia, tinyKACTL
+ * Description: Push-relabel using the highest label selection rule and the gap heuristic. Quite fast in practice.
+ * Time: $O(V^2\sqrt E)$
+ * Status: Tested on kattis and SPOJ
+ */
+
+typedef ll Flow;
+struct Edge {
+	int dest, back;
+	Flow f, c;
+};
+typedef vector<vector<Edge> > graph;
+
+struct PushRelabel {
+	graph g;
+	vector<Flow> exc;
+	vector<Edge*> cur;
+	vector<vi> hs; vi H;
+	PushRelabel(int n) : g(n), exc(n), cur(n), hs(2*n), H(n) {}
+
+	void add_edge(int s, int t, Flow cap, Flow rcap=0) {
+		if (s == t) return;
+		Edge a = {t, sz(g[t]), 0, cap};
+		Edge b = {s, sz(g[s]), 0, rcap};
+		g[s].push_back(a);
+		g[t].push_back(b);
+	}
+
+	void add_flow(Edge& e, Flow f) {
+		Edge &back = g[e.dest][e.back];
+		if (!exc[e.dest]) hs[H[e.dest]].push_back(e.dest);
+		e.f += f; e.c -= f; exc[e.dest] += f;
+		back.f -= f; back.c += f; exc[back.dest] -= f;
+	}
+	Flow maxflow(int s, int t) {
+		int v = sz(g); H[s] = v; exc[t] = 1;
+		vi co(2*v); co[0] = v-1;
+		rep(i,0,v) cur[i] = g[i].data();
+		trav(e, g[s])
+			if (e.c > 0) add_flow(e, e.c);
+
+		for (int hi = 0;;) {
+			while (hs[hi].empty()) if (!hi--) return -exc[s];
+			int u = hs[hi].back(); hs[hi].pop_back();
+			while (exc[u] > 0)  // discharge u
+				if (cur[u] == g[u].data() + sz(g[u])) {
+					H[u] = 1e9;
+					trav(e, g[u]) if (e.c > 0 && H[u] > H[e.dest]+1)
+						H[u] = H[e.dest]+1, cur[u] = &e;
+					if (++co[H[u]], !--co[hi] && hi < v)
+						rep(i,0,v) if (hi < H[i] && H[i] < v)
+							--co[H[i]], H[i] = v + 1;
+					hi = H[u];
+				} else if (cur[u]->c > 0 && H[u] == H[cur[u]->dest]+1)
+					add_flow(*cur[u], min(exc[u], cur[u]->c));
+				else ++cur[u];
+		}
+	}
+};
