fe592790c18cabeedee45af5e8d23aaa607277a0
Simon Lindholm <simon.lindholm10@gmail.com>
2015-04-25 23:52:34 +0200

    Remove Point::R, simplify constructor

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -14,17 +14,16 @@ using namespace std;
 template <class T>
 struct Point {
 	typedef Point P;
-	typedef const P& R;
 	T x, y;
-	explicit Point(T x=T(), T y=T()) : x( x), y( y) { }
-	bool operator<(R p) const { return x<p.x || x==p.x && y<p.y; }
-	bool operator==(R p) const { return x==p.x && y==p.y; }
-	P operator+(R p) const { return P(x+p.x, y+p.y); }
-	P operator-(R p) const { return P(x-p.x, y-p.y); }
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return x<p.x || x==p.x && y<p.y; }
+	bool operator==(P p) const { return x==p.x && y==p.y; }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
 	P operator*(T d) const { return P(x*d, y*d); }
 	P operator/(T d) const { return P(x/d, y/d); }
-	T dot(R p) const { return x*p.x + y*p.y; }
-	T cross(R p) const { return x*p.y - y*p.x; }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
 	T dist2() const { return x*x + y*y; } //distance^2
 	double dist() const { return sqrt((double)dist2()); } //length
 	//angle to x-axis in interval [-pi, pi]
