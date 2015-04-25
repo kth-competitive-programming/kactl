16c286bb7054377bfc9d5122b302326861da4a43
Simon Lindholm <simon.lindholm10@gmail.com>
2015-04-26 00:39:52 +0200

    Shorten Point

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -11,28 +11,27 @@
 #include <iostream>
 using namespace std;
 
+#define R(x) const { return x; }
 template <class T>
 struct Point {
 	typedef Point P;
 	T x, y;
 	explicit Point(T x=0, T y=0) : x(x), y(y) {}
-	bool operator<(P p) const { return x<p.x || x==p.x && y<p.y;}
-	bool operator==(P p) const { return x==p.x && y==p.y; }
-	P operator+(P p) const { return P(x+p.x, y+p.y); }
-	P operator-(P p) const { return P(x-p.x, y-p.y); }
-	P operator*(T d) const { return P(x*d, y*d); }
-	P operator/(T d) const { return P(x/d, y/d); }
-	T dot(P p) const { return x*p.x + y*p.y; }
-	T cross(P p) const { return x*p.y - y*p.x; }
-	T dist2() const { return x*x + y*y; } //distance^2
-	double dist() const { return sqrt((double)dist2()); } //length
-	//angle to x-axis in interval [-pi, pi]
-	double angle() const { return atan2(y, x); } 
-	P unit() const { return *this/(T)dist(); } //makes dist()=1
-	P perp() const { return P(-y, x); } //rotates +pi/2 radians
-	P normal() const { return perp().unit(); }
-	//returns point rotated 'a' radians counterclockwise around origin
-	P rotate(double a) const {
-		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a));
-	}
+	bool operator<(P p) R(x<p.x || x==p.x && y<p.y)
+	bool operator==(P p) R(x==p.x && y==p.y)
+	P operator+(P p) R(P(x+p.x, y+p.y))
+	P operator-(P p) R(P(x-p.x, y-p.y))
+	P operator*(T d) R(P(x*d, y*d))
+	P operator/(T d) R(P(x/d, y/d))
+	T dot(P p) R(x*p.x + y*p.y)
+	T cross(P p) R(x*p.y - y*p.x)
+	T dist2() R(x*x + y*y) // distance^2
+	double dist() R(sqrt((double)dist2())) //length
+	// angle to x-axis in interval [-pi, pi]
+	double angle() R(atan2(y, x))
+	P unit() R(*this/(T)dist()) // makes dist()=1
+	P perp() R(P(-y, x)) // rotates +pi/2 radians
+	P normal() R(perp().unit())
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) R(P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)))
 };
