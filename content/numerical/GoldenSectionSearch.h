1927d19dfc992fff67d3053aca1c76f5b2ff43a1
Simon Lindholm <simon.lindholm10@gmail.com>
2015-05-12 21:24:21 +0200

    Revert incorrect change to golden section search

--- content/numerical/GoldenSectionSearch.h
+++ content/numerical/GoldenSectionSearch.h
@@ -2,20 +2,21 @@
  * Author: Ulf Lundstrom
  * Date: 2009-04-17
  * Source: Numeriska algoritmer med matlab, Gerd Eriksson, NADA, KTH
- * Description: Finds the argument minimizing the function $f$ in the interval $[a,b]$ assuming $f$ is unimodal on the interval, i.e. has only one local minima. The maximum error in the result is $e$. Works equally well for maximization with a small change in the code.
+ * Description: Finds the argument minimizing the function $f$ in the interval $[a,b]$ assuming $f$ is unimodal on the interval, i.e. has only one local minimum. The maximum error in the result is $eps$. Works equally well for maximization with a small change in the code.
  * Status: tested
  * Usage:
 	double func(double x) { return 4+x+.3*x*x; }
 	double xmin = gss(-1000,1000,func);
 	double pi = gss(0,6,cos);
- * Time: O(\log\left(\frac{b-a}{e}\right))
+ * Time: O(\log\left(\frac{b-a}{\epsilon}\right))
  */
 #pragma once
 
 #include <cmath>
 
+/// It is important for r to be precise, otherwise we don't necessarily maintain the inequality a < x1 < x2 < b.
 double gss(double a, double b, double (*f)(double)) {
-	double r = .618, eps = 1e-7;
+	double r = (sqrt(5)-1)/2, eps = 1e-7;
 	double x1 = b - r*(b-a), x2 = a + r*(b-a);
 	double f1 = f(x1), f2 = f(x2);
 	while (b-a > eps) {
