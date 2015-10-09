f591c0a64ceb5cad3657fb3f0fc43cf2c4d89b43
Simon Lindholm <simon.lindholm10@gmail.com>
2015-10-09 17:53:54 +0200

    Update Hashing.h to cope with Thue-Morse
    Not happy with this, but it's probably better than we have now...

--- content/strings/Hashing.h
+++ content/strings/Hashing.h
@@ -2,14 +2,26 @@
  * Author: Simon Lindholm
  * Date: 2015-03-15
  * Source: own work
- * Description: Various self-explanatory methods for string hashing. If it helps, $333411^{-1} = 113144412244155211$ mod $2^{64}$.
+ * Description: Various self-explanatory methods for string hashing.
  * Status: tested
  */
 
 typedef unsigned long long H;
-static const H C = 123891739; // arbitrary but always odd
+static const H C = 123891739; // arbitrary
+
+// Arithmetic mod 2^64-1. 5x slower than mod 2^64 and more
+// code, but works on evil test data (e.g. Thue-Morse).
+// "typedef H K;" instead if you think test data is random.
+struct K {
+	typedef __uint128_t H2;
+	H x; K(H x=0) : x(x) {}
+	K operator+(K o){ return x + o.x + H(((H2)x + o.x)>>64); }
+	K operator*(K o){ return K(x*o.x)+ H(((H2)x * o.x)>>64); }
+	H operator-(K o) { K a = *this + ~o.x; return a.x + !~a.x; }
+};
+
 struct HashInterval {
-	vector<H> ha, pw;
+	vector<K> ha, pw;
 	HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
 		pw[0] = 1;
 		rep(i,0,sz(str))
@@ -23,19 +35,19 @@ struct HashInterval {
 
 vector<H> getHashes(string& str, int length) {
 	if (sz(str) < length) return {};
-	H h = 0, pw = 1;
+	K h = 0, pw = 1;
 	rep(i,0,length)
-		h = h * C + str[i], pw *= C;
-	vector<H> ret = {h};
+		h = h * C + str[i], pw = pw * C;
+	vector<H> ret = {h - 0};
 	rep(i,length,sz(str)) {
-		h = h * C + str[i] - pw * str[i-length];
-		ret.push_back(h);
+		ret.push_back(h * C + str[i] - pw * str[i-length]);
+		h = ret.back();
 	}
 	return ret;
 }
 
 H hashString(string& s) {
-	H h = 0;
-	trav(c, s) h = h*C + c;
-	return h;
+	K h = 0;
+	trav(c, s) h = h * C + c;
+	return h - 0;
 }
