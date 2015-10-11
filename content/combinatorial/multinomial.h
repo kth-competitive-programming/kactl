ac1ea5235fbe9725a6ec16c009ffe0aef84cfbb0
Simon Lindholm <simon.lindholm10@gmail.com>
2015-10-11 14:56:42 +0200

    Update multinomial even more

--- content/combinatorial/multinomial.h
+++ content/combinatorial/multinomial.h
@@ -11,9 +11,8 @@
 using namespace std;
 
 ll multinomial(vi& v) {
-	ll c = 1;
-	int m = v.empty() ? 1 : v[0];
+	ll c = 1, m = v.empty() ? 1 : v[0];
 	rep(i,1,sz(v)) rep(j,0,v[i])
-		c = c * ll(++m) / (j+1);
+		c = c * ++m / (j+1);
 	return c;
 }
