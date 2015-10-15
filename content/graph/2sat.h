98f3654771ddc7e34a96233962233ca1d988ea68
Simon Lindholm <simon.lindholm10@gmail.com>
2015-10-15 21:59:18 +0200

    Add add_var, at_most_one helper functions to 2-SAT

--- content/graph/2sat.h
+++ content/graph/2sat.h
@@ -5,10 +5,11 @@
  * Description: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem, so that an expression of the type $(a\|\|b)\&\&(!a\|\|c)\&\&(d\|\|!b)\&\&...$ becomes true, or reports that it is unsatisfiable.
  * Usage:
  *  TwoSat ts(number of boolean variables);
- *  ts.add_clause(0, true, 3, false); // Variable 0 must be true or variable 3 must be false (or both)
- *  ts.set_value(2, true); // We already know that variable 2 must be true
- *  ts.solve(); // Returns true if it is solvable, otherwise false
- *  ts.values[0..N-1] holds all the assigned values to the variables
+ *  ts.add_clause(0, true, 3, false); // Var 0 is true or var 3 is false (or both)
+ *  ts.set_value(2, true); // Var 2 is true
+ *  ts.at_most_one({0,1,2}); // <= 1 of vars 0, 1 and 2 are true
+ *  ts.solve(); // Returns true iff it is solvable
+ *  ts.values[0..N-1] holds the assigned values to the vars
  * Time: O(N+E), where N is the number of boolean variables, and E is the number of clauses.
  */
 #pragma once
@@ -28,16 +29,36 @@ struct TwoSat {
 	};
 	vector<Node> nodes;
 
-	TwoSat(int n) : values(n), N(n), nodes(2*n) {}
+	TwoSat(int n = 0) : values(n), N(n), nodes(2*n) {}
 
-	void add_clause(int a_index, bool a_value, int b_index, bool b_value){
+	int add_var() { // (optional)
+		values.emplace_back();
+		nodes.emplace_back();
+		nodes.emplace_back();
+		return N++;
+	}
+
+	void add_clause(int a_index, bool a_value, int b_index, bool b_value) {
 		int a = 2*a_index + a_value, b = 2*b_index + b_value;
 		nodes[a^1].outs.push_back(b), nodes[b].ins.push_back(a^1);
 		nodes[b^1].outs.push_back(a), nodes[a].ins.push_back(b^1);
 	}
 
-	void set_value(int index, bool value){
+	void set_value(int index, bool value) { // (optional)
 		add_clause(index, value, index, value);
+	}
+
+	void at_most_one(const vi& li, bool val=1) { // (optional)
+		if (sz(li) <= 1) return;
+		int cur = li[0];
+		rep(i,2,sz(li)) {
+			int next = add_var();
+			add_clause(cur, !val, li[i], !val);
+			add_clause(cur, !val, next, val);
+			add_clause(li[i], !val, next, val);
+			cur = next;
+		}
+		add_clause(cur, !val, li[1], !val);
 	}
 
 	struct Comp {
