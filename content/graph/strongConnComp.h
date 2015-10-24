9bff28cd3e9337b7d87dbac0236645db546b9f99
Simon Lindholm <simon.lindholm10@gmail.com>
2015-10-24 17:34:12 +0200

    Simplify SCC a bit and add a note about topological sorting

--- content/graph/strongConnComp.h
+++ content/graph/strongConnComp.h
@@ -1,47 +1,48 @@
 /**
  * Author: Lukas Polacek
  * Date: 2009-10-28
- * Source: Czech graph algorithms book, by Demel.
+ * Source: Czech graph algorithms book, by Demel. (Tarjan's algorithm)
  * Description: Finds strongly connected components in a
  * directed graph. If vertices $u, v$ belong to the same component,
  * we can reach $u$ from $v$ and vice versa.
- * Time: O(E + V) where $E$ is the number of edges and $V$ is the number of vertices.
+ * Time: O(E + V)
  * Status: Works
- * Usage: Create a graph, then run go(graph). As a result
- * you get a number for each vertex - a number of SC-component
- * containing this vertex.
+ * Usage: Create a graph, then run scc(graph). As a result
+ * you get a number for each vertex - the index of the component
+ * containing that vertex. Components are topologically sorted:
+ * they only have edges to components with lower index.
+ * no_components will contain the number of connected components.
  */
-#include <stack>
+#include <vector>
 using namespace std;
 
-vi val, num, comp;
+vi orig, low, comp, z;
 int no_vertices, no_components;
-stack<int> z;
 template<class G> void dfs(int j, G &g) {
-	num[j] = val[j] = no_vertices++; comp[j] = 0; z.push(j);
+	low[j] = orig[j] = no_vertices++;
+	comp[j] = -2; z.push_back(j);
 	trav(e,g[j])
 		if (comp[e] == -1) {
 			dfs(e, g);
-			num[j] = min(num[j], num[e]);
+			low[j] = min(low[j], low[e]);
 		}
-		else if (comp[e] == 0)
-			num[j] = min(num[j], val[e]);
+		else if (comp[e] == -2)
+			low[j] = min(low[j], orig[e]);
 
-	if (val[j] == num[j]) {
-		no_components++;
-		while (true) {
-			comp[z.top()] = no_components;
-			if (z.top() == j) { z.pop(); break; }
-			z.pop();
+	if (orig[j] == low[j]) {
+		for (;;) {
+			int x = z.back(); z.pop_back();
+			comp[x] = no_components;
+			if (x == j) break;
 		}
+		no_components++;
 	}
 }
-template<class G> vi go(G &g) {
+template<class G> vi scc(G &g) {
 	int n = sz(g);
-	val.assign(n, 0); num = val;
+	orig.assign(n, 0); low = orig;
 	no_vertices = no_components = 0;
 	comp.assign(n, -1);
 	rep(i,0,n) if (comp[i] == -1) dfs(i, g);
-	rep(i,0,n) comp[i]--;
 	return comp;
 }
