63adcd34cc7b18f5e9712b307dde2bb9ae011032
Simon Lindholm <simon.lindholm10@gmail.com>
2015-10-26 11:20:00 +0100

    Shorter matrix_tree

--- content/graph/matrix_tree.h
+++ content/graph/matrix_tree.h
@@ -6,10 +6,11 @@
  * Usage:
  *  vector<vi> mat(N, vi(N));
  *  addEdge(mat, u, v); // for each edge (u, v)
- *  int nSpanningTrees = det(reduce(mat));
+ *  ll nSpanningTrees = det(reduce(mat));
  */
 
-ll det(vector<vector<ll>>& a) { // integer determinant
+typedef vector<vector<ll>> vvll;
+ll det(vvll& a) { // integer determinant
 	ll ans = 1;
 	rep(i,0,a.size()) {
 		rep(j,i+1,a.size()) {
@@ -22,21 +23,19 @@ ll det(vector<vector<ll>>& a) { // integer determinant
 				ans = -ans;
 			}
 		}
-		if (!a[i][i]){
-			return 0;
-		}
+		if (!a[i][i]) return 0;
 		ans *= a[i][i];
 	}
 	return ans;
 }
 
-void addEdge(vector<vector<ll>>& mat, int a, int b){
+void addEdge(vvll& mat, int a, int b){
 	mat[a][a]++; mat[b][b]++;
 	mat[a][b]--; mat[b][a]--;
 }
 
-vector<vector<ll>> reduce(vector<vector<ll>>& mat){
-	vector<vector<ll>> ret = mat;
+vvll reduce(vvll& mat){
+	vvll ret = mat;
 	ret.pop_back();
 	trav(r, ret) r.pop_back();
 	return ret;
