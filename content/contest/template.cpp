2633c6442473109829376512eb200b563fa5b697
Simon Lindholm <simon.lindholm10@gmail.com>
2016-03-03 23:25:09 +0000

    Fix bug in rep macro

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < int(b); ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define trav(a, v) for(auto& a : v)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
