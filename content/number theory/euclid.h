429e30e32f521856b7f3214b5185ebe50815da2f
Simon Lindholm <simon.lindholm10@gmail.com>
2016-03-07 15:11:34 +0100

    Simpler gcd

--- content/number theory/euclid.h
+++ content/number theory/euclid.h
@@ -6,13 +6,10 @@
  */
 #pragma once
 
-template <class Z> Z gcd(Z a, Z b) {
-	return b==0 ? a : gcd(b, a%b);
-}
+ll gcd(ll a, ll b) { return __gcd(a, b); }
 
-template <class Z> 
-Z euclid(Z a, Z b, Z &x, Z &y) {
-	if (b) { Z d = euclid(b, a % b, y, x);
+ll euclid(ll a, ll b, ll &x, ll &y) {
+	if (b) { ll d = euclid(b, a % b, y, x);
 		return y -= a/b * x, d; }
 	return x = 1, y = 0, a;
 }
