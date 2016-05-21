65d540e631a1fd02025095c2c8512d97b512e2d9
Simon Lindholm <simon.lindholm10@gmail.com>
2016-05-22 01:30:47 +0200

    Remove A(i,j) macro from solve_linear
    25% faster and less code.

--- content/numerical/SolveLinear.h
+++ content/numerical/SolveLinear.h
@@ -5,7 +5,7 @@
  * Description: Solves $A * x = b$, returning either YES, NO, MULT depending on whether there
  *  are 1, 0 or infinitely many solutions.
  *  Data in $A$ and $b$ is lost.
- * Time: O(n^2*m)
+ * Time: O(n^2 m)
  * Status: tested on kattis:equationsolver, and bruteforce-tested mod 3 and 5 for n,m <= 3
  */
 #pragma once
@@ -16,7 +16,6 @@ using namespace std;
 
 typedef vector<double> vd;
 const double eps = 1e-12;
-#define A(i,j) A[i][col[j]]
 
 enum { YES, NO, MULT };
 int solve_linear(vector<vd>& A, vd& b, vd& x) {
@@ -26,7 +25,7 @@ int solve_linear(vector<vd>& A, vd& b, vd& x) {
 	rep(i,0,n) {
 		double v, bv = -1;
 		rep(r,i,n) rep(c,i,m)
-			if ((v = fabs(A(r,c))) > bv)
+			if ((v = fabs(A[r][c])) > bv)
 				br = r, bc = c, bv = v;
 		if (bv <= eps) {
 			rep(j,i,n) if (fabs(b[j]) > eps) return NO;
@@ -36,20 +35,20 @@ int solve_linear(vector<vd>& A, vd& b, vd& x) {
 		swap(A[i], A[br]);
 		swap(b[i], b[br]);
 		swap(col[i], col[bc]);
-		bv = 1/A(i,i);
+		rep(j,0,n) swap(A[j][i], A[j][bc]);
+		bv = 1/A[i][i];
 		rep(j,i+1,n) {
-			double fac = A(j,i) * bv;
+			double fac = A[j][i] * bv;
 			b[j] -= fac * b[i];
-			rep(k,i+1,m) A(j,k) -= fac*A(i,k);
+			rep(k,i+1,m) A[j][k] -= fac*A[i][k];
 		}
 	}
 	if (n < m) return MULT;
 
 	for (int i = m; i--;) {
-		x[col[i]] = (b[i] /= A(i,i));
+		x[col[i]] = (b[i] /= A[i][i]);
 		rep(j,0,i)
-			b[j] -= A(j,i) * b[i];
+			b[j] -= A[j][i] * b[i];
 	}
 	return YES;
 }
-#undef A
