b8f47568a62535c6be1864eb334716dc78890ddb
Simon Lindholm <simon.lindholm10@gmail.com>
2016-05-22 00:55:01 +0200

    Support non-square matrices in solve_linear

--- content/numerical/SolveLinear.h
+++ content/numerical/SolveLinear.h
@@ -5,8 +5,8 @@
  * Description: Solves $A * x = b$, returning either YES, NO, MULT depending on whether there
  *  are 1, 0 or infinitely many solutions.
  *  Data in $A$ and $b$ is lost.
- * Time: O(n^3)
- * Status: tested on kattis:equationsolver
+ * Time: O(n^2*m)
+ * Status: tested on kattis:equationsolver, and bruteforce-tested mod 3 and 5 for n,m <= 3
  */
 #pragma once
 
@@ -16,41 +16,40 @@ using namespace std;
 
 typedef vector<double> vd;
 const double eps = 1e-12;
-#define A(i,j) A[row[i]][col[j]]
-#define B(i) b[row[i]]
+#define A(i,j) A[i][col[j]]
 
 enum { YES, NO, MULT };
 int solve_linear(vector<vd>& A, vd& b, vd& x) {
-	int n = sz(A);
-	vi row(n), col(n);
-	rep(i,0,n) row[i] = col[i] = i;
+	int n = sz(A), m = sz(x), br = -1, bc = -1;
+	vi col(m); iota(all(col), 0);
 
 	rep(i,0,n) {
-		int br = i, bc = i;
 		double v, bv = -1;
-		rep(r,i,n) rep(c,i,n)
+		rep(r,i,n) rep(c,i,m)
 			if ((v = fabs(A(r,c))) > bv)
 				br = r, bc = c, bv = v;
-		if (bv < eps) {
-			rep(j,i,n) if (fabs(B(j)) > eps) return NO;
+		if (bv <= eps) {
+			rep(j,i,n) if (fabs(b[j]) > eps) return NO;
+			if (i == m) break;
 			return MULT;
 		}
-		bv = A(br,bc);
-		swap(row[i], row[br]);
+		swap(A[i], A[br]);
+		swap(b[i], b[br]);
 		swap(col[i], col[bc]);
+		bv = 1/A(i,i);
 		rep(j,i+1,n) {
-			double fac = A(j,i) / bv;
-			B(j) -= fac * B(i);
-			rep(k,i+1,n) A(j,k) -= fac*A(i,k);
+			double fac = A(j,i) * bv;
+			b[j] -= fac * b[i];
+			rep(k,i+1,m) A(j,k) -= fac*A(i,k);
 		}
 	}
+	if (n < m) return MULT;
 
-	for (int i = n-1; i >= 0; --i) {
-		B(i) /= A(i,i);
+	for (int i = m; i--;) {
+		x[col[i]] = (b[i] /= A(i,i));
 		rep(j,0,i)
-			B(j) -= A(j,i) * B(i);
+			b[j] -= A(j,i) * b[i];
 	}
-	rep(i,0,n) x[col[i]] = B(i);
 	return YES;
 }
 #undef A
