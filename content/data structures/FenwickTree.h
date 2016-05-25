7a3a509c2bf8a26efcabc81c69acccfca767d5d7
Simon Lindholm <simon.lindholm10@gmail.com>
2016-05-26 00:08:52 +0200

    Half-open and shorter Fenwick tree

--- content/data structures/FenwickTree.h
+++ content/data structures/FenwickTree.h
@@ -3,7 +3,7 @@
  * Date: 2009-10-30
  * Source: folklore/TopCoder
  * Description: In the beginning there is an array $a[n]$ full of zeroes.
- * Fenwick tree can be used to retrieve sum $a[0]+\dots+a[x]$. You
+ * Fenwick tree can be used to retrieve sum $a[i]$ over any half-open interval $[0, x)$. You
  * can also update the value $a[x]$ (update function takes the difference
  * between the old and new value).
  * Time: Both operations are $O(\log N)$.
@@ -13,19 +13,16 @@
 using namespace std;
 
 struct FT {
-	int n;
-	vi s;
-	FT(int _n) : n(_n) {
-		s.assign(n, 0);
-	}
-	void update(int pos, int dif) {
-		for (; pos < n; pos |= pos + 1)
+	vector<ll> s;
+	FT(int n) : s(n) {}
+	void update(int pos, ll dif) { // a[pos] += dif
+		for (; pos < sz(s); pos |= pos + 1)
 			s[pos] += dif;
 	}
-	int query(int pos) {
-		int count = 0;
-		for (; pos >= 0; pos = (pos & (pos + 1)) - 1)
-			count += s[pos];
-		return count;
+	ll query(int pos) { // sum of values in [0, pos)
+		ll res = 0;
+		for (; pos > 0; pos &= pos - 1)
+			res += s[pos-1];
+		return res;
 	}
 };
