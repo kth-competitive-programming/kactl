6250e7af97912bf4e45302e26103447b21369bb9
Simon Lindholm <simon.lindholm10@gmail.com>
2016-07-23 01:35:52 +0200

    alignas is a keyword

--- content/various/SIMD.h
+++ content/various/SIMD.h
@@ -7,7 +7,7 @@
  * Operations follow the pattern \texttt{"\_mm(256)?\_name\_(si(128|256)|epi(8|16|32|64)|pd|ps)"}. Not all are described here;
  * grep for \texttt{\_mm\_} in \texttt{/usr/lib/gcc/{*}/4.9/include/} for more.
  * If AVX is unsupported, try 128-bit operations, "emmintrin.h" and \#define \texttt{\_\_SSE\_\_} and \texttt{\_\_MMX\_\_} before including it.
- * Memory must be aligned, so (for 256 bits) use \texttt{\_mm\_malloc(size, 32)} or \texttt{\_\_attribute\_\_((aligned(32)))}.
+ * Memory must be aligned, so (for 256 bits) use \texttt{\_mm\_malloc(size, 32)} or \texttt{int buf[N] alignas(32)}.
  */
 #pragma once
 
