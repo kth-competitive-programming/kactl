6955aa6451a84fe35cd190c44aade6d8aa879d77
Simon Lindholm <simon.lindholm10@gmail.com>
2016-08-25 15:21:51 +0300

    Update BumpAllocator and add an associated pointer class

--- content/various/BumpAllocator.h
+++ content/various/BumpAllocator.h
@@ -4,14 +4,14 @@
  * Source: me
  * Description: When you need to dynamically allocate many objects and don't care about freeing them.
  * "new X" otherwise has an overhead of something like 0.05us + 16 bytes per allocation.
- * Usage: struct Node : Small { ... }; // (or inline it)
  * Status: tested
  */
 
+// Either globally or in a single class:
 static char buf[450 << 20] alignas(16);
-struct Small {
-	void* operator new(size_t s) {
-		static size_t i = sizeof buf;
-		return (void*)&buf[i -= s];
-	}
-};
+void* operator new(size_t s) {
+	static size_t i = sizeof buf;
+	assert(s < i);
+	return (void*)&buf[i -= s];
+}
+void operator delete(void*) {}
---
+++ content/various/SmallPtr.h
@@ -0,0 +1,19 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2016-08-23
+ * Source: me
+ * Description: A 32-bit pointer that points into BumpAllocator memory.
+ * Status: tested
+ */
+#include "BumpAllocator.h"
+
+template<class T> struct ptr {
+	unsigned ind;
+	ptr(T* p = 0) : ind(p ? unsigned((char*)p - buf) : 0) {
+		assert(ind < sizeof buf);
+	}
+	T& operator*() const { return *(T*)(buf + ind); }
+	T* operator->() const { return &**this; }
+	T& operator[](int a) const { return (&**this)[a]; }
+	explicit operator bool() const { return ind; }
+};
