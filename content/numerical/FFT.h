226fbf46a60c343396999e48450f9b13a5c1a48e
Simon Lindholm <simon.lindholm10@gmail.com>
2016-08-26 21:01:57 +0300

    Optimize FFT

--- content/numerical/FFT.h
+++ content/numerical/FFT.h
@@ -21,34 +21,31 @@
 using namespace std;
 
 typedef valarray<complex<double> > carray;
-const double PI = acos(-1);
-void fft(carray& x) {
+void fft(carray& x, carray& roots) {
 	int N = sz(x);
 	if (N <= 1) return;
 	carray even = x[slice(0, N/2, 2)];
 	carray odd = x[slice(1, N/2, 2)];
-	fft(even);
-	fft(odd);
+	carray rs = roots[slice(0, N/2, 2)];
+	fft(even, rs);
+	fft(odd, rs);
 	rep(k,0,N/2) {
-		auto t = polar(1.0, -2 * M_PI * k / N) * odd[k];
+		auto t = roots[k] * odd[k];
 		x[k    ] = even[k] + t;
 		x[k+N/2] = even[k] - t;
 	}
 }
 
-void ifft(carray& x) {
-	fft(x = x.apply(conj));
-	x = x.apply(conj) / sz(x);
-}
-
 typedef vector<double> vd;
 vd conv(const vd& a, const vd& b) {
-	int s = sz(a) + sz(b) - 1, L = 32 - __builtin_clz(s);
+	int s = sz(a)+sz(b)-1, L = 32-__builtin_clz(s), n = 1 << L;
 	if (s <= 0) return {};
-	carray av(1 << L), bv = av;
-	copy(all(a), begin(av)); fft(av);
-	copy(all(b), begin(bv)); fft(bv);
-	carray cv = av * bv; ifft(cv);
+	carray av(n), bv(n), roots(n);
+	rep(i,0,n) roots[i] = polar(1.0, -2 * M_PI * i / n);
+	copy(all(a), begin(av)); fft(av, roots);
+	copy(all(b), begin(bv)); fft(bv, roots);
+	roots = roots.apply(conj);
+	carray cv = av * bv; fft(cv, roots); cv /= n;
 	vd c(s); rep(i,0,s) c[i] = cv[i].real();
 	return c;
 }
