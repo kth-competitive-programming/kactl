123ff3e9648e7d1b5a60c3425ee7c337e4ca8d67
Simon Lindholm <simon.lindholm10@gmail.com>
2016-09-06 16:02:56 +0200

    Rewrite determinant code for general moduli and move it to IntDeterminant.h, add float determinant
    Mostly cribbed from Matrix Tree Theorem code.

--- content/numerical/Determinant.h
+++ content/numerical/Determinant.h
@@ -1,33 +1,23 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-27
+ * Author: Simon Lindholm
+ * Date: 2016-09-06
  * Source: folklore
- * Description: Calculates determinant using modular arithmetics.
- * Can be rewritten to use doubles, but then use some epsilon to avoid division
- * by zero. The modulus is assumed to be a prime number. For composite moduli,
- * use CRT; for prime powers, you are on your own, but at least A is invertible iff
- * it is invertible mod p, and the identity $\det A = (\Pi^n_{k=1}(A^{-1}_k)_{k,k})^{-1}$
- * could be used on a LU decomposition of A ($A_k$ is the $k \times k$ upper-left part of A).
- * Status: tested
+ * Description: Calculates determinant of a matrix. Destroys the matrix.
+ * Status: somewhat tested
  * Time: $O(N^3)$
  */
-#include "../number-theory/ModularArithmetic.h"
 
-Mod det(Mod a[100][100], int n) {
-	Mod res(1);
-	rep(k,0,n) {
-		if (a[k][k].x == 0) rep(i,k+1,n) if (a[i][k].x != 0) {
-			res = res * Mod(mod - 1);
-			rep(j,k,n) swap(a[k][j], a[i][j]);
-			break;
-		}
-		if (a[k][k].x == 0) return 0;
-		Mod v = a[k][k];
-		rep(i,k,n) a[k][i] = a[k][i] / v;
-		res = res * v;
-		rep(i,k+1,n) if (a[i][k].x != 0) {
-			v = Mod(0) - a[i][k];
-			rep(j,k,n) a[i][j] = a[i][j] + v * a[k][j];
+double det(vector<vector<double>>& a) {
+	int n = sz(a); double res = 1;
+	rep(i,0,n) {
+		int b = i;
+		rep(j,i+1,n) if (fabs(a[j][i]) > fabs(a[b][i])) b = j;
+		if (i != b) swap(a[i], a[b]), res *= -1;
+		res *= a[i][i];
+		if (res == 0) return 0;
+		rep(j,i+1,n) {
+			double v = a[j][i] / a[i][i];
+			if (v != 0) rep(k,i+1,n) a[j][k] -= v * a[i][k];
 		}
 	}
 	return res;
---
+++ content/numerical/IntDeterminant.h
@@ -0,0 +1,28 @@
+/**
+ * Author: Unknown
+ * Date: 2014-11-27
+ * Source: Unknown
+ * Description: Calculates determinant using modular arithmetics.
+ * Modulos can also be removed to get a pure-integer version.
+ * Status: bruteforce-tested for N <= 3, mod <= 7
+ * Time: $O(N^3)$
+ */
+
+const ll mod = 12345;
+ll det(vector<vector<ll>>& a) {
+	int n = sz(a); ll ans = 1;
+	rep(i,0,n) {
+		rep(j,i+1,n) {
+			while (a[j][i] != 0) { // gcd step
+				ll t = a[i][i] / a[j][i];
+				if (t) rep(k,i,n)
+					a[i][k] = (a[i][k] - a[j][k] * t) % mod;
+				swap(a[i], a[j]);
+				ans *= -1;
+			}
+		}
+		ans = ans * a[i][i] % mod;
+		if (!ans) return 0;
+	}
+	return (ans + mod) % mod;
+}
