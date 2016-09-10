fa2edd86f7ef73ced1f0a1c292e8946b224efef3
Simon Lindholm <simon.lindholm10@gmail.com>
2016-09-10 17:34:45 +0200

    Add ModSqrt

---
+++ content/number-theory/ModSqrt.h
@@ -0,0 +1,41 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2016-08-31
+ * Source: http://eli.thegreenplace.net/2009/03/07/computing-modular-square-roots-in-python/
+ * Description: Tonelli-Shanks algorithm for modular square roots.
+ * Time: O(\log^2 p) worst case, often O(\log p)
+ * Status: Tested for all a,p <= 10000
+ */
+
+#include "ModPow.h"
+
+ll sqrt(ll a, ll p) {
+	a %= p; if (a < 0) a += p;
+	if (a == 0) return 0;
+	assert(modpow(a, (p-1)/2, p) == 1);
+	if (p % 4 == 3) return modpow(a, (p+1)/4, p);
+	// a^(n+3)/8 or 2^(n+3)/8 * 2^(n-1)/4 works if p % 8 == 5
+	ll s = p - 1;
+	int r = 0;
+	while (s % 2 == 0)
+		++r, s /= 2;
+	ll n = 2; // find a non-square mod p
+	while (modpow(n, (p - 1) / 2, p) != p - 1) ++n;
+	ll x = modpow(a, (s + 1) / 2, p);
+	ll b = modpow(a, s, p);
+	ll g = modpow(n, s, p);
+	for (;;) {
+		ll t = b;
+		int m = 0;
+		for (; m < r; ++m) {
+			if (t == 1) break;
+			t = t * t % p;
+		}
+		if (m == 0) return x;
+		ll gs = modpow(g, 1 << (r - m - 1), p);
+		g = gs * gs % p;
+		x = x * gs % p;
+		b = b * g % p;
+		r = m;
+	}
+}
