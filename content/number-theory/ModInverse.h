66a3f4c8e7561a6b285d03c827105556ea309a88
Simon Lindholm <simon.lindholm10@gmail.com>
2016-09-10 17:27:08 +0200

    Make ASAN catch accesses to inv[0] in ModInverse

--- content/number-theory/ModInverse.h
+++ content/number-theory/ModInverse.h
@@ -2,10 +2,10 @@
  * Author: Simon Lindholm
  * Date: 2016-07-24
  * Source: Russian page
- * Description: Pre-computation of modular inverses. Assumes LIM $\le$ M and that M is a prime.
+ * Description: Pre-computation of modular inverses. Assumes LIM $\le$ mod and that mod is a prime.
  * Status: Works
  */
 
-const ll M = 1e9+7, LIM = 200000;
-ll inv[LIM] = {1e5, 1};
-rep(i,2,LIM) inv[i] = M - (M / i) * inv[M % i] % M;
+const ll mod = 1000000007, LIM = 200000;
+ll* inv = new ll[LIM] - 1; inv[1] = 1;
+rep(i,2,LIM) inv[i] = mod - (mod / i) * inv[mod % i] % mod;
