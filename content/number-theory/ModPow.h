6c27008b1acadbd5dbd9fe5f2c98a5988430fc4f
Simon Lindholm <simon.lindholm10@gmail.com>
2016-09-10 17:30:26 +0200

    Add ModPow
    Because why not

---
+++ content/number-theory/ModPow.h
@@ -0,0 +1,14 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2016-09-10
+ * Source: folklore
+ * Description:
+ * Status: tested
+ */
+
+const ll mod = 1000000007; // faster if const
+ll modpow(ll a, ll e) {
+	if (e == 0) return 1;
+	ll x = modpow(a * a % mod, e >> 1);
+	return e & 1 ? x * a % mod : x;
+}
