d7ba23bb395595d310f127a43704311d6a940848
Simon Lindholm <simon.lindholm10@gmail.com>
2016-11-23 23:55:50 +0100

    Shuffle things around for space

--- content/data-structures/UnionFind.h
+++ content/data-structures/UnionFind.h
@@ -2,7 +2,7 @@
  * Author: Lukas Polacek
  * Date: 2009-10-26
  * Source: folklore
- * Description: Disjoint-set data structure
+ * Description: Disjoint-set data structure.
  * Time: $O(\alpha(N))$
  */
 #pragma once
@@ -14,14 +14,11 @@ struct UF {
 	UF(int n) : e(n, -1) {}
 	bool same_set(int a, int b) { return find(a) == find(b); }
 	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
 	void join(int a, int b) {
 		a = find(a), b = find(b);
 		if (a == b) return;
 		if (e[a] > e[b]) swap(a, b);
 		e[a] += e[b]; e[b] = a;
-	}
-	int find(int x) {
-		if (e[x] < 0) return x;
-		return e[x] = find(e[x]);
 	}
 };
