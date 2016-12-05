ccf217d5780454cb6efb31dfaeb64760be501dd1
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-05 21:58:25 +0100

    exc -> ec in PushRelabel

--- content/graph/PushRelabel.h
+++ content/graph/PushRelabel.h
@@ -16,10 +16,10 @@ typedef vector<vector<Edge> > graph;
 
 struct PushRelabel {
 	graph g;
-	vector<Flow> exc;
+	vector<Flow> ec;
 	vector<Edge*> cur;
 	vector<vi> hs; vi H;
-	PushRelabel(int n) : g(n), exc(n), cur(n), hs(2*n), H(n) {}
+	PushRelabel(int n) : g(n), ec(n), cur(n), hs(2*n), H(n) {}
 
 	void add_edge(int s, int t, Flow cap, Flow rcap=0) {
 		if (s == t) return;
@@ -31,20 +31,20 @@ struct PushRelabel {
 
 	void add_flow(Edge& e, Flow f) {
 		Edge &back = g[e.dest][e.back];
-		if (!exc[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
-		e.f += f; e.c -= f; exc[e.dest] += f;
-		back.f -= f; back.c += f; exc[back.dest] -= f;
+		if (!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
+		e.f += f; e.c -= f; ec[e.dest] += f;
+		back.f -= f; back.c += f; ec[back.dest] -= f;
 	}
 	Flow maxflow(int s, int t) {
-		int v = sz(g); H[s] = v; exc[t] = 1;
+		int v = sz(g); H[s] = v; ec[t] = 1;
 		vi co(2*v); co[0] = v-1;
 		rep(i,0,v) cur[i] = g[i].data();
 		trav(e, g[s]) add_flow(e, e.c);
 
 		for (int hi = 0;;) {
-			while (hs[hi].empty()) if (!hi--) return -exc[s];
+			while (hs[hi].empty()) if (!hi--) return -ec[s];
 			int u = hs[hi].back(); hs[hi].pop_back();
-			while (exc[u] > 0)  // discharge u
+			while (ec[u] > 0)  // discharge u
 				if (cur[u] == g[u].data() + sz(g[u])) {
 					H[u] = 1e9;
 					trav(e, g[u]) if (e.c && H[u] > H[e.dest]+1)
@@ -54,7 +54,7 @@ struct PushRelabel {
 							--co[H[i]], H[i] = v + 1;
 					hi = H[u];
 				} else if (cur[u]->c && H[u] == H[cur[u]->dest]+1)
-					add_flow(*cur[u], min(exc[u], cur[u]->c));
+					add_flow(*cur[u], min(ec[u], cur[u]->c));
 				else ++cur[u];
 		}
 	}
