1a18b75cee6cf8c524edb190174edb63d06f0e1b
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-13 00:29:50 +0100

    Lmate, Rmate -> L, R in WeightedMatching

--- content/graph/WeightedMatching.h
+++ content/graph/WeightedMatching.h
@@ -17,7 +17,7 @@ using namespace std;
 
 typedef vector<double> vd;
 bool zero(double x) { return fabs(x) < 1e-10; }
-double MinCostMatching(const vector<vd>& cost, vi& Lmate, vi& Rmate) {
+double MinCostMatching(const vector<vd>& cost, vi& L, vi& R) {
 	int n = sz(cost), mated = 0;
 	vd dist(n), u(n), v(n);
 	vi dad(n), seen(n);
@@ -33,13 +33,13 @@ double MinCostMatching(const vector<vd>& cost, vi& Lmate, vi& Rmate) {
 	}
 
 	/// find primal solution satisfying complementary slackness
-	Lmate = vi(n, -1);
-	Rmate = vi(n, -1);
+	L = vi(n, -1);
+	R = vi(n, -1);
 	rep(i,0,n) rep(j,0,n) {
-		if (Rmate[j] != -1) continue;
+		if (R[j] != -1) continue;
 		if (zero(cost[i][j] - u[i] - v[j])) {
-			Lmate[i] = j;
-			Rmate[j] = i;
+			L[i] = j;
+			R[j] = i;
 			mated++;
 			break;
 		}
@@ -47,7 +47,7 @@ double MinCostMatching(const vector<vd>& cost, vi& Lmate, vi& Rmate) {
 
 	for (; mated < n; mated++) { // until solution is feasible
 		int s = 0;
-		while (Lmate[s] != -1) s++;
+		while (L[s] != -1) s++;
 		fill(all(dad), -1);
 		fill(all(seen), 0);
 		rep(k,0,n)
@@ -61,7 +61,7 @@ double MinCostMatching(const vector<vd>& cost, vi& Lmate, vi& Rmate) {
 				if (j == -1 || dist[k] < dist[j]) j = k;
 			}
 			seen[j] = 1;
-			int i = Rmate[j];
+			int i = R[j];
 			if (i == -1) break;
 			rep(k,0,n) { /// relax neighbors
 				if (seen[k]) continue;
@@ -77,22 +77,22 @@ double MinCostMatching(const vector<vd>& cost, vi& Lmate, vi& Rmate) {
 		rep(k,0,n) {
 			if (k == j || !seen[k]) continue;
 			auto w = dist[k] - dist[j];
-			v[k] += w, u[Rmate[k]] -= w;
+			v[k] += w, u[R[k]] -= w;
 		}
 		u[s] += dist[j];
 
 		/// augment along path
 		while (dad[j] >= 0) {
 			int d = dad[j];
-			Rmate[j] = Rmate[d];
-			Lmate[Rmate[j]] = j;
+			R[j] = R[d];
+			L[R[j]] = j;
 			j = d;
 		}
-		Rmate[j] = s;
-		Lmate[s] = j;
+		R[j] = s;
+		L[s] = j;
 	}
 
 	auto value = vd(1)[0];
-	rep(i,0,n) value += cost[i][Lmate[i]];
+	rep(i,0,n) value += cost[i][L[i]];
 	return value;
 }
