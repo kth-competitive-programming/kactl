23f920d6035022ab5f1801bb0c7dde4ed67bd8e3
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-15 18:57:21 +0100

    Replace floydWarshall.h by a rewritten floydWarshall2.h

---
+++ content/graph/FloydWarshall.h
@@ -0,0 +1,26 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2016-12-15
+ * Source: http://en.wikipedia.org/wiki/Floyd–Warshall_algorithm
+ * Description: Calculates all-pairs shortest path in a directed graph that might have negative edge distances.
+ * Input is an distance matrix $m$, where $m[i][j] = \texttt{inf}$ if $i$ and $j$ are not adjacent.
+ * As output, $m[i][j]$ is set to the shortest distance between $i$ and $j$, \texttt{inf} if no path, or \texttt{-inf} if the path goes through a negative-weight cycle.
+ * Time: O(N^3)
+ * Status: slightly tested
+ */
+#pragma once
+#include <algorithm>
+using namespace std;
+
+const ll inf = 1LL << 62;
+void floydWarshall(vector<vector<ll>>& m) {
+	int n = sz(m);
+	rep(i,0,n) m[i][i] = min(m[i][i], {});
+	rep(k,0,n) rep(i,0,n) rep(j,0,n)
+		if (m[i][k] != inf && m[k][j] != inf) {
+			auto newDist = max(m[i][k] + m[k][j], -inf);
+			m[i][j] = min(m[i][j], newDist);
+		}
+	rep(k,0,n) if (m[k][k] < 0) rep(i,0,n) rep(j,0,n)
+		if (m[i][k] != inf && m[k][j] != inf) m[i][j] = -inf;
+}
