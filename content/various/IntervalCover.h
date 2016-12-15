65bc488983111759be7adae8303f37377723937e
Johan Sannemo <johan.sannemo@gmail.com>
2016-12-15 21:22:16 +0100

    interval cover

--- content/various/IntervalCover.h
+++ content/various/IntervalCover.h
@@ -1,6 +1,8 @@
 /**
  * Author: Johan Sannemo
  * Description: Compute indices of smallest set of intervals covering another interval.
+ * Intervalls should be [inclusive, exclusive). To support [inclusive, inclusive),
+ * change (A) to add || R.empty().
  * Time: O(N \log N)
  * Status: Tested on Kattis, intervalcover
  */
@@ -16,7 +18,7 @@ vi cover(pair<T, T> G, vector<pair<T, T>> I) {
     sort(all(S), [&](int a, int b) { return I[a] < I[b]; });
     T cur = G.first;
     int at = -1;
-    while (R.empty() || cur < G.second) {
+    while (cur < G.second) { // (A)
         pair<T, int> mx = make_pair(cur, -1);
         while (at + 1 < sz(I) && I[S[at + 1]].first <= cur) {
             at++;
