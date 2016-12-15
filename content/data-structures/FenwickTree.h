3314779debb34dc9126322328b5edbdd5cb472dc
Johan Sannemo <johan.sannemo@gmail.com>
2016-12-15 18:36:45 +0100

    Math fixes

--- content/data-structures/FenwickTree.h
+++ content/data-structures/FenwickTree.h
@@ -2,10 +2,8 @@
  * Author: Lukas Polacek
  * Date: 2009-10-30
  * Source: folklore/TopCoder
- * Description: In the beginning there is an array $a[n]$ full of zeroes.
- * Fenwick tree can be used to retrieve sum $a[i]$ over any half-open interval $[0, x)$. You
- * can also update the value $a[x]$ (update function takes the difference
- * between the old and new value).
+ * Description: Computes partial sums a[0] + a[1] + ... + a[pos - 1], and updates single elements a[i],
+ * taking the difference between the old and new value.
  * Time: Both operations are $O(\log N)$.
  */
 #pragma once
@@ -16,13 +14,11 @@ struct FT {
 	vector<ll> s;
 	FT(int n) : s(n) {}
 	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < sz(s); pos |= pos + 1)
-			s[pos] += dif;
+		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
 	}
 	ll query(int pos) { // sum of values in [0, pos)
 		ll res = 0;
-		for (; pos > 0; pos &= pos - 1)
-			res += s[pos-1];
+		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
 		return res;
 	}
 };
