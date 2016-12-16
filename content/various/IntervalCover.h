423524c3167c22b79bb414ef01f6d4ee1623cb9a
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-16 01:18:38 +0100

    Simplify IntervalCover slightly

--- content/various/IntervalCover.h
+++ content/various/IntervalCover.h
@@ -1,8 +1,8 @@
 /**
  * Author: Johan Sannemo
  * Description: Compute indices of smallest set of intervals covering another interval.
- * Intervalls should be [inclusive, exclusive). To support [inclusive, inclusive],
- * change (A) to add || R.empty().
+ * Intervals should be [inclusive, exclusive). To support [inclusive, inclusive],
+ * change (A) to add || R.empty(). Returns empty set on failure (or if G is empty).
  * Time: O(N \log N)
  * Status: Tested on Kattis, intervalcover
  */
@@ -17,12 +17,12 @@ vi cover(pair<T, T> G, vector<pair<T, T>> I) {
 	iota(all(S), 0);
 	sort(all(S), [&](int a, int b) { return I[a] < I[b]; });
 	T cur = G.first;
-	int at = -1;
+	int at = 0;
 	while (cur < G.second) { // (A)
 		pair<T, int> mx = make_pair(cur, -1);
-		while (at + 1 < sz(I) && I[S[at + 1]].first <= cur) {
-			at++;
+		while (at < sz(I) && I[S[at]].first <= cur) {
 			mx = max(mx, make_pair(I[S[at]].second, S[at]));
+			at++;
 		}
 		if (mx.second == -1) return {};
 		cur = mx.first;
