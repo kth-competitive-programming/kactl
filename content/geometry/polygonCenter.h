86888864a4d219a8d0046fbaeac41c75f2f3ba72
Simon Lindholm <simon.lindholm10@gmail.com>
2017-04-12 17:37:57 +0300

    Update polygonCenter

--- content/geometry/polygonCenter.h
+++ content/geometry/polygonCenter.h
@@ -2,23 +2,19 @@
  * Author: Ulf Lundstrom
  * Date: 2009-04-08
  * Source:
- * Description: Returns the center of mass for the polygon specified by the points between iterators begin and end. It should be an iterator or const\_iterator with value type Point<double>.
- * Status: Tested with unitTest and Kattis problem centerofmass
- * Usage:
- * 	typedef Point<double> P;
- * 	vector<P> p = ...;
- * 	P com = polygonCenter(p.begin(),p.end());
+ * Description: Returns the center of mass for a polygon.
+ * Status: Tested
  */
 #pragma once
 #include "Point.h"
 
-template <class It>
-Point<double> polygonCenter(It begin, It end) {
-	Point<double> res;
-	double A = 0;
-	for (It i = begin, j = end-1; i != end; j=i++) {
-		res = res + (*i+*j)*j->cross(*i);
+typedef Point<double> P;
+Point<double> polygonCenter(vector<P>& v) {
+	auto i = v.begin(), end = v.end(), j = end-1;
+	Point<double> res{0,0}; double A = 0;
+	for (; i != end; j=i++) {
+		res = res + (*i + *j) * j->cross(*i);
 		A += j->cross(*i);
 	}
-	return res/A/3;
+	return res / A / 3;
 }
