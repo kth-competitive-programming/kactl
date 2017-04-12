79a3b4643c4918d24755744a385ddba535ee3cb0
Johan Sannemo <jsannemo@kth.se>
2017-04-12 16:53:52 +0300

    Fix and document polygonDiameter

--- content/geometry/polygonDiameter.h
+++ content/geometry/polygonDiameter.h
@@ -1,27 +1,35 @@
 /**
- * Author: André Susano Pinto
- * Date: 2010-11-18
- * Source: My other KACTL
- * Description: Calculates the max diameter of a polygon. L is the convex hull and i is the index of separation betwwen the upper and lower hull.
- * Status: Not tested.
+ * Author: Johan Sannemo
+ * Date: 2017-03-12
+ * Source: Wikipedia
+ * Description: Calculates the max diameter of a set of points.
+ * Status: Tested.
  */
 #pragma once
 #include <vector>
 using namespace std;
 
-template<class T, class P>
-T PolygonDiameter( vector<P> L, int i ) {
-	reverse( L.begin()+i , L.end() );
-	L.push_back( L[i-1] ); //duplicates the last point of lower hull.
-	T dist = -1;
-	int j = i - 1 , m = L.size()-1;
-	while ( i < m || j > 0 ) {
-		dist = max( dist , sq_distance( L[i] , L[j] ) );		
-		if  (i == m) j--;
-		else if (j == 0) i++;
-		else if ((L[i+1].y-L[i].y)*(L[j].x-L[j-1].x) > (L[j].y-L[j-1].y)*(L[i+1].x-L[i].x))
-			i++;
-		else j--;
-	}
-	return dist;  // returns square distance.
+template <class P>
+ll polygonDiameter(vector<P> points) {
+    sort(all(points));
+    vector<P> U, L;
+    trav(it, points) {
+        while (sz(U) > 1 && U[sz(U)-2].cross(it, U.back()) <= 0) U.pop_back();
+        while (sz(L) > 1 && L[sz(L)-2].cross(it, L.back()) >= 0) L.pop_back();
+        U.push_back(it);
+        L.push_back(it);
+    }
+    vector<pair<P, P>> antipodal;
+    int i = 0;
+    int j = sz(L) - 1;
+    while (i < sz(U) - 1 || j > 0) {
+        antipodal.emplace_back(U[i], L[j]);
+        if (i == sz(U) - 1) --j;
+        else if (j == 0) ++i;
+        else if ((U[i+1].y - U[i].y) * (L[j].x - L[j-1].x) > (U[i+1].x - U[i].x) * (L[j].y - L[j-1].y)) ++i;
+        else --j;
+    }
+    ll ans = 0;
+    trav(it, antipodal) ans = max(ans, (it.first - it.second).dist2());
+    return ans;
 }
