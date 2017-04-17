c6ee6897287963b18b395f34ceaf00dbbdb2e1c7
Simon Lindholm <simon.lindholm10@gmail.com>
2017-04-17 21:33:27 +0300

    Update biconnected components, remove bridges

---
+++ content/graph/BiconnectedComponents.h
@@ -0,0 +1,55 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2017-04-17
+ * Source: folklore
+ * Description: Finds all biconnected components in an undirected graph, and
+ *  runs a callback for the edges in each. In a biconnected component there
+ *  are at least two distinct paths between any two nodes. Note that a node can
+ *  be in several components. An edge which is not in a component is a bridge,
+ *  i.e., not part of any cycle.
+ * Time: O(E + V)
+ * Status: tested during MIPT ICPC Workshop 2017
+ * Usage:
+ *  int eid = 0; ed.resize(N);
+ *  for each edge (a,b) {
+ *    ed[a].emplace_back(b, eid);
+ *    ed[b].emplace_back(a, eid++); }
+ *  bicomps([\&](const vi\& edgelist) {...});
+ */
+#include <vector>
+using namespace std;
+
+vi num, st;
+vector<vector<pii>> ed;
+int Time;
+template<class F>
+int dfs(int at, int par, F f) {
+	int me = num[at] = ++Time, e, y, top = me;
+	trav(pa, ed[at]) if (pa.second != par) {
+		tie(y, e) = pa;
+		if (num[y]) {
+			top = min(top, num[y]);
+			if (num[y] < num[at])
+				st.push_back(e);
+		} else {
+			int si = sz(st);
+			int up = dfs(y, e, f);
+			top = min(top, up);
+			if (up == me) {
+				st.push_back(e);
+				f(vi(st.begin() + si, st.end()));
+				st.resize(si);
+			}
+			else if (up < me)
+				st.push_back(e);
+			// else e is a bridge
+		}
+	}
+	return top;
+}
+
+template<class F>
+void bicomps(F f) {
+	num.assign(sz(ed), 0);
+	rep(i,0,sz(ed)) if (!num[i]) dfs(i, -1, f);
+}
