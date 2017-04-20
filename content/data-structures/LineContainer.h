359fb0be2ea40a33f8bf598698130ddfc6b36bc0
Simon Lindholm <simon.lindholm10@gmail.com>
2017-04-20 19:56:51 +0200

    Add LineContainer

---
+++ content/data-structures/LineContainer.h
@@ -0,0 +1,44 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2017-04-20
+ * Source: own work
+ * Description: Container where you can add lines of the form kx+m, and query maximum values at points x.
+ * Time: O(\log N)
+ * Status: tested
+ */
+#pragma once
+#include <vector>
+using namespace std;
+
+bool Q;
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const {
+		return Q ? p < o.p : k < o.k;
+	}
+};
+
+struct LineContainer : multiset<Line> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) { x->p = inf; return false; }
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+		return l.k * x + l.m;
+	}
+};
