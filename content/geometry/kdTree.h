11050d50378200129f4a91ef2d197e6b95c7cee2
Simon Lindholm <simon.lindholm10@gmail.com>
2017-04-23 14:26:20 +0200

    Shorter KD-tree

--- content/geometry/kdTree.h
+++ content/geometry/kdTree.h
@@ -22,7 +22,7 @@ bool on_y(const P& a, const P& b) { return a.y < b.y; }
 
 struct Node {
 	P pt; // if this is a leaf, the single point in it
-	T x0, x1, y0, y1; // bounds
+	T x0 = INF, x1 = -INF, y0 = INF, y1 = -INF; // bounds
 	Node *first = 0, *second = 0;
 
 	T distance(const P& p) { // min squared distance to a point
@@ -31,19 +31,14 @@ struct Node {
 		return (P(x,y) - p).dist2();
 	}
 
-	Node(vector<P>&& vp) : pt(vp[0]), x0(INF), x1(-INF),
-			y0(INF), y1(-INF) {
+	Node(vector<P>&& vp) : pt(vp[0]) {
 		for (P p : vp) {
 			x0 = min(x0, p.x); x1 = max(x1, p.x);
 			y0 = min(y0, p.y); y1 = max(y1, p.y);
 		}
 		if (vp.size() > 1) {
 			// split on x if the box is wider than high (not best heuristic...)
-			if (x1 - x0 >= y1 - y0)
-				sort(vp.begin(), vp.end(), on_x);
-			else
-				sort(vp.begin(), vp.end(), on_y);
-
+			sort(all(vp), x1 - x0 >= y1 - y0 ? on_x : on_y);
 			// divide by taking half the array for each child (not
 			// best performance with many duplicates in the middle)
 			int half = sz(vp)/2;
@@ -54,31 +49,24 @@ struct Node {
 };
 
 struct KDTree {
-	Node *root;
+	Node* root;
 	KDTree(const vector<P>& vp) : root(new Node({all(vp)})) {}
 
 	pair<T, P> search(Node *node, const P& p) {
 		if (!node->first) {
 			// uncomment if we should not find the point itself:
-			// if (p == node->pt) return INF;
+			// if (p == node->pt) return {INF, P()};
 			return make_pair((p - node->pt).dist2(), node->pt);
 		}
 
-		T bfirst = node->first->distance(p);
-		T bsecond = node->second->distance(p);
+		Node *f = node->first, *s = node->second;
+		T bfirst = f->distance(p), bsec = s->distance(p);
+		if (bfirst > bsec) swap(bsec, bfirst), swap(f, s);
 
 		// search closest side first, other side if needed
-		pair<T, P> best;
-		if (bfirst < bsecond) {
-			best = search(node->first, p);
-			if (bsecond < best.first)
-				best = min(best, search(node->second, p));
-		}
-		else {
-			best = search(node->second, p);
-			if (bfirst < best.first)
-				best = min(best, search(node->first, p));
-		}
+		auto best = search(f, p);
+		if (bsec < best.first)
+			best = min(best, search(s, p));
 		return best;
 	}
 
