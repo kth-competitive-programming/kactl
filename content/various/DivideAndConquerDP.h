668f624c3067533e9341ce2f37e31d5bc98c11c0
Simon Lindholm <simon.lindholm10@gmail.com>
2017-05-11 15:54:44 +0200

    Add divide-and-conquer DP

---
+++ content/various/DivideAndConquerDP.h
@@ -0,0 +1,27 @@
+/**
+ * Author: Simon Lindholm
+ * Source: Codeforces
+ * Description: Given $a[i] = \min_{lo(i) \le k \le hi(i)}(f(i, k))$ where the (minimal) optimal $k$ increases with $i$, computes $a[i]$ for $i = L..R-1$.
+ * Status: tested on http://codeforces.com/contest/321/problem/E
+ * Time: O((N + (hi-lo)) \log N)
+ */
+#pragma once
+
+struct DP { // Modify at will:
+	int lo(int ind) { return 0; }
+	int hi(int ind) { return ind; }
+	ll f(int ind, int k) { return dp[ind][k]; }
+	void store(int ind, int k, ll v) { res[ind] = pii(k, v); }
+
+	void rec(int L, int R, int LO, int HI) {
+		if (L >= R) return;
+		int mid = (L + R) >> 1;
+		pair<ll, int> best(1LL << 60, LO);
+		rep(k,max(LO, lo(mid)),min(HI, hi(mid))+1)
+			best = min(best, make_pair(f(mid, k), k));
+		store(mid, best.second, best.first);
+		rec(L, mid, LO, best.second);
+		rec(mid+1, R, best.second, HI);
+	}
+	void solve(int L, int R) { rec(L, R, INT_MIN, INT_MAX); }
+};
