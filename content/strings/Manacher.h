d97e8a73320b5d32963a976aabbbdf52e01c5595
Simon Lindholm <simon.lindholm10@gmail.com>
2017-05-14 00:56:37 +0200

    Add Manacher

---
+++ content/strings/Manacher.h
@@ -0,0 +1,23 @@
+/**
+ * Author: User adamant on CodeForces
+ * Source: http://codeforces.com/blog/entry/12143
+ * Description: For each position in a string, computes p[0][i] = half length of
+ *  longest even palindrome around pos i, p[1][i] = longest odd (half rounded down).
+ * Time: O(N)
+ * Status: Fuzz-tested
+ */
+#pragma once
+#include <string>
+using namespace std;
+
+void manacher(const string& s) {
+	int n = sz(s);
+	vi p[2] = {vi(n+1), vi(n)};
+	rep(z,0,2) for (int i=0,l=0,r=0; i < n; i++) {
+		int t = r-i+!z;
+		if (i<r) p[z][i] = min(t, p[z][l+t]);
+		int L = i-p[z][i], R = i+p[z][i]-!z;
+		while (L>=1 && R+1<n && s[L-1] == s[R+1])
+			p[z][i]++, L--, R++;
+		if (R>r) l=L, r=R;
+}}
