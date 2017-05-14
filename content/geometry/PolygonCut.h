1088236b692f39a9d5f7d3b0fd1c5363d5f17d27
Simon Lindholm <simon.lindholm10@gmail.com>
2017-05-14 13:42:42 +0200

    More camel-case names in content/geometry/

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -8,7 +8,7 @@
 \end{minipage}
 \begin{minipage}{15mm}
 \vspace{-6mm}
-\includegraphics[width=\textwidth]{../content/geometry/polygonCut}
+\includegraphics[width=\textwidth]{../content/geometry/PolygonCut}
 \vspace{-6mm}
 \end{minipage}
  * Status: tested but not extensively
