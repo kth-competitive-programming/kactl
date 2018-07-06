b48e733f54d49ef0de8f2c8adf956319d95d3616
Simon Lindholm <simon.lindholm10@gmail.com>
2018-07-07 00:51:55 +0200

    Add angleDiff function to Angle.h
    Closes #44.

--- content/geometry/Angle.h
+++ content/geometry/Angle.h
@@ -5,11 +5,11 @@
  * Source:
  * Description: A class for ordering angles (as represented by int points and
  *  a number of rotations around the origin). Useful for rotational sweeping.
+ *  Sometimes also represents points or vectors.
  * Usage:
  *  vector<Angle> v = {w[0], w[0].t360() ...}; // sorted
- *  int j = 0; rep(i,0,n) {
- *    while (v[j] < v[i].t180()) ++j;
- *  } // sweeps j such that (j-i) represents the number of positively oriented triangles with vertices at 0 and i
+ *  int j = 0; rep(i,0,n) { while (v[j] < v[i].t180()) ++j; }
+ *  // sweeps j such that (j-i) represents the number of positively oriented triangles with vertices at 0 and i
  * Status: Used, works well
  */
 #pragma once
@@ -18,7 +18,7 @@ struct Angle {
 	int x, y;
 	int t;
 	Angle(int x, int y, int t=0) : x(x), y(y), t(t) {}
-	Angle operator-(Angle a) const { return {x-a.x, y-a.y, t}; }
+	Angle operator-(Angle b) const { return {x-b.x, y-b.y, t}; }
 	int quad() const {
 		assert(x || y);
 		if (y < 0) return (x >= 0) + 2;
@@ -34,9 +34,6 @@ bool operator<(Angle a, Angle b) {
 	return make_tuple(a.t, a.quad(), a.y * (ll)b.x) <
 	       make_tuple(b.t, b.quad(), a.x * (ll)b.y);
 }
-bool operator>=(Angle a, Angle b) { return !(a < b); }
-bool operator>(Angle a, Angle b) { return b < a; }
-bool operator<=(Angle a, Angle b) { return !(b < a); }
 
 // Given two points, this calculates the smallest angle between
 // them, i.e., the angle that covers the defined line segment.
@@ -45,9 +42,12 @@ pair<Angle, Angle> segmentAngles(Angle a, Angle b) {
 	return (b < a.t180() ?
 	        make_pair(a, b) : make_pair(b, a.t360()));
 }
-
-Angle operator+(Angle a, Angle b) { // where b is a vector
+Angle operator+(Angle a, Angle b) { // point a + vector b
 	Angle r(a.x + b.x, a.y + b.y, a.t);
-	if (r > a.t180()) r.t--;
+	if (a.t180() < r) r.t--;
 	return r.t180() < a ? r.t360() : r;
+}
+Angle angleDiff(Angle a, Angle b) { // angle b - angle a
+	int tu = b.t - a.t; a.t = b.t;
+	return {a.x*b.x + a.y*b.y, a.x*b.y - a.y*b.x, tu - (b < a)};
 }
