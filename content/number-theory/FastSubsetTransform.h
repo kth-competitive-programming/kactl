496bc41afb7c673e5c94b43f7547c6981880359c
Simon Lindholm <simon.lindholm10@gmail.com>
2018-07-07 14:26:19 +0200

    Add fast subset transform

---
+++ content/number-theory/FastSubsetTransform.h
@@ -0,0 +1,29 @@
+/**
+ * Author: Lucian Bicsi
+ * Date: 2015-06-25
+ * License: GNU Free Documentation License 1.2
+ * Source: csacademy
+ * Description: Transform to a basis with fast convolutions of the form
+ * $\displaystyle c[z] = \sum\nolimits_{z = x \oplus y} a[x] \cdot b[y]$,
+ * where $\oplus$ is one of AND, OR, XOR. The size of $a$ must be a power of two.
+ * Time: O(N \log N)
+ * Status: tested
+ */
+#pragma once
+
+void FST(vi& a, bool inv) {
+	for (int n = sz(a), step = 1; step < n; step *= 2) {
+		for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
+			int &u = a[j], &v = a[j + step]; tie(u, v) =
+				inv ? pii(v - u, u) : pii(v, u + v); // AND
+				inv ? pii(v, u - v) : pii(u + v, u); // OR
+				pii(u + v, u - v);                   // XOR
+		}
+	}
+	if (inv) trav(x, a) x /= sz(a); // XOR only
+}
+vi conv(vi a, vi b) {
+	FST(a, 0); FST(b, 0);
+	rep(i,0,sz(a)) a[i] *= b[i];
+	FST(a, 1); return a;
+}
