33da732a60b0c193625eb888d958ffa59b37b9e5
Simon Lindholm <simon.lindholm10@gmail.com>
2018-07-09 01:13:18 +0200

    Fail-safe tridiagonal
    Better safe than sorry, even if most practical cases probably don't need it.

--- content/numerical/Tridiagonal.h
+++ content/numerical/Tridiagonal.h
@@ -1,5 +1,5 @@
 /**
- * Author: Ulf Lundstrom
+ * Author: Ulf Lundstrom, Simon Lindholm
  * Date: 2009-08-15
  * License: CC0
  * Source: Gauss
@@ -24,22 +24,36 @@ where $a_0$, $a_{n+1}$, $b_i$, $c_i$ and $d_i$ are known. $a$ can then be obtain
 \{a_i\}=\textrm{tridiagonal}(&\{1,-1,-1,...,-1,1\}, \{0,c_1,c_2,\dots,c_n\},\\
 &\{b_1,b_2,\dots,b_n,0\}, \{a_0,d_1,d_2,\dots,d_n,a_{n+1}\}).
 \end{align*}
-Warning: might fail if numbers are not nice. If so, perturb them slightly.
+Fails if the solution is not unique.
  * Time: O(N)
- * Status: Usually works...
+ * Status: Fuzz-tested mod 5 and 7.
  */
 #pragma once
 
 typedef double T;
 vector<T> tridiagonal(vector<T> diag, const vector<T>& super,
 		const vector<T>& sub, vector<T> b) {
-	rep(i,0,sz(b)-1) {
-		diag[i+1] -= super[i]*sub[i]/diag[i];
-		b[i+1] -= b[i]*sub[i]/diag[i];
+	int n = sz(b); vi tr(n);
+	rep(i,0,n-1) {
+		if (abs(diag[i]) < 1e-9 * abs(super[i])) { // diag[i] == 0
+			// (For many problems, this can never occur.)
+			b[i+1] -= b[i] * diag[i+1] / super[i];
+			if (i+2 < n) b[i+2] -= b[i] * sub[i+1] / super[i];
+			diag[i+1] = sub[i]; tr[++i] = 1;
+		} else {
+			diag[i+1] -= super[i]*sub[i]/diag[i];
+			b[i+1] -= b[i]*sub[i]/diag[i];
+		}
 	}
-	for (int i = sz(b); i--;) {
-		b[i] /= diag[i];
-		if (i) b[i-1] -= b[i]*super[i-1];
+	for (int i = n; i--;) {
+		if (tr[i]) {
+			swap(b[i], b[i-1]);
+			diag[i-1] = diag[i];
+			b[i] /= super[i-1];
+		} else {
+			b[i] /= diag[i];
+			if (i) b[i-1] -= b[i]*super[i-1];
+		}
 	}
 	return b;
 }
