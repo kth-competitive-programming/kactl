981b23015adc13a59db592a098ba1c81ef8caaa5
Simon Lindholm <simon.lindholm10@gmail.com>
2018-07-20 14:24:11 +0200

    Add FastDelaunay, but don't include it
    It's rather long, and probably not useful enough.

---
+++ content/geometry/FastDelaunay.h
@@ -0,0 +1,106 @@
+/**
+ * Author: Philippe Legault
+ * Date: 2016
+ * License: MIT
+ * Source: https://github.com/Bathlamos/delaunay-triangulation/
+ * Description: Fast Delaunay triangulation. There must be no duplicate points.
+ * If all points are on a line, no triangles will be returned.
+ * Should work for doubles as well, though there may be precision issues in 'circ'.
+ * Returns triangles in order \{t[0][0], t[0][1], t[0][2], t[1][0], \dots\}, all counter-clockwise.
+ * Time: O(n \log n)
+ * Status: fuzz-tested
+ */
+#pragma once
+
+#include "Point.h"
+
+typedef Point<ll> P;
+typedef struct Quad* Q;
+typedef __int128_t lll; // (can be ll if coords are < 2e4)
+P arb(LLONG_MAX,LLONG_MAX); // not equal to any other point
+
+struct Quad {
+	bool mark; Q o, rot; P p;
+	P F() { return r()->p; }
+	Q r() { return rot->rot; }
+	Q prev() { return rot->o->rot; }
+	Q next() { return rot->r()->o->rot; }
+};
+
+bool circ(P p, P a, P b, P c) { // is p in the circumcircle?
+	lll p2 = p.dist2(), A = a.dist2()-p2,
+	    B = b.dist2()-p2, C = c.dist2()-p2;
+	return p.cross(a,b)*C + p.cross(b,c)*A + p.cross(c,a)*B > 0;
+}
+Q makeEdge(P orig, P dest) {
+	Q q0 = new Quad{0,0,0,orig}, q1 = new Quad{0,0,0,arb},
+	  q2 = new Quad{0,0,0,dest}, q3 = new Quad{0,0,0,arb};
+	q0->o = q0; q2->o = q2; // 0-0, 2-2
+	q1->o = q3; q3->o = q1; // 1-3, 3-1
+	q0->rot = q1; q1->rot = q2;
+	q2->rot = q3; q3->rot = q0;
+	return q0;
+}
+void splice(Q a, Q b) {
+	swap(a->o->rot->o, b->o->rot->o); swap(a->o, b->o);
+}
+Q connect(Q a, Q b) {
+	Q q = makeEdge(a->F(), b->p);
+	splice(q, a->next());
+	splice(q->r(), b);
+	return q;
+}
+
+pair<Q,Q> rec(const vector<P>& s) {
+	if (sz(s) <= 3) {
+		Q a = makeEdge(s[0], s[1]), b = makeEdge(s[1], s.back());
+		if (sz(s) == 2) return { a, a->r() };
+		splice(a->r(), b);
+		auto side = s[0].cross(s[1], s[2]);
+		Q c = side ? connect(b, a) : 0;
+		return {side < 0 ? c->r() : a, side < 0 ? c : b->r() };
+	}
+
+#define H(e) e->F(), e->p
+#define valid(e) (e->F().cross(H(base)) > 0)
+	Q A, B, ra, rb;
+	int half = (sz(s) + 1) / 2;
+	tie(ra, A) = rec({s.begin(), s.begin() + half});
+	tie(B, rb) = rec({s.begin() + half, s.end()});
+	while ((B->p.cross(H(A)) < 0 && (A = A->next())) ||
+	       (A->p.cross(H(B)) > 0 && (B = B->r()->o)));
+	Q base = connect(B->r(), A);
+	if (A->p == ra->p) ra = base->r();
+	if (B->p == rb->p) rb = base;
+
+#define DEL(e, init, dir) Q e = init->dir; if (valid(e)) \
+		while (circ(e->dir->F(), H(base), e->F())) { \
+			Q t = e->dir; \
+			splice(e, e->prev()); \
+			splice(e->r(), e->r()->prev()); \
+			e = t; \
+		}
+	for (;;) {
+		DEL(LC, base->r(), o);  DEL(RC, base, prev());
+		if (!valid(LC) && !valid(RC)) break;
+		if (!valid(LC) || (valid(RC) && circ(H(RC), H(LC))))
+			base = connect(RC, base->r());
+		else
+			base = connect(base->r(), LC->r());
+	}
+	return { ra, rb };
+}
+
+vector<P> triangulate(vector<P> pts) {
+	sort(all(pts));  assert(unique(all(pts)) == pts.end());
+	if (sz(pts) < 2) return {};
+	Q e = rec(pts).first;
+	vector<Q> q = {e};
+	int qi = 0;
+	while (e->o->F().cross(e->F(), e->p) < 0) e = e->o;
+#define ADD { Q c = e; do { c->mark = 1; pts.push_back(c->p); \
+	q.push_back(c->r()); c = c->next(); } while (c != e); }
+	ADD; pts.clear();
+	while (qi < sz(q)) if (!(e = q[qi++])->mark) ADD;
+	return pts;
+}
