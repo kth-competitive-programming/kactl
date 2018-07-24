9aa3658eb26abfa7ccc9c0a9081ff63188f0387b
Simon Lindholm <simon.lindholm10@gmail.com>
2018-07-24 22:07:14 +0200

    Notes about numeric stability for Tridiagonal

--- content/numerical/Tridiagonal.h
+++ content/numerical/Tridiagonal.h
@@ -2,7 +2,7 @@
  * Author: Ulf Lundstrom, Simon Lindholm
  * Date: 2009-08-15
  * License: CC0
- * Source: Gauss
+ * Source: https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
  * Description: $x=\textrm{tridiagonal}(d,p,q,b)$ solves the equation system
 \[
 \left(\begin{array}{c}b_0\\b_1\\b_2\\b_3\\\vdots\\b_{n-1}\end{array}\right) =
@@ -25,8 +25,11 @@ where $a_0$, $a_{n+1}$, $b_i$, $c_i$ and $d_i$ are known. $a$ can then be obtain
 &\{b_1,b_2,\dots,b_n,0\}, \{a_0,d_1,d_2,\dots,d_n,a_{n+1}\}).
 \end{align*}
 Fails if the solution is not unique.
+
+If $|d_i| > |p_i| + |q_{i-1}|$ for all $i$, or $|d_i| > |p_{i-1}| + |q_i|$, or the matrix is positive definite,
+the algorithm is numerically stable and neither \texttt{tr} nor the check for \texttt{diag[i] == 0} is needed.
  * Time: O(N)
- * Status: Fuzz-tested mod 5 and 7.
+ * Status: Brute-force tested mod 5 and 7 and fuzz-tested for real matrices obeying the criteria above.
  */
 #pragma once
 
@@ -36,7 +39,6 @@ vector<T> tridiagonal(vector<T> diag, const vector<T>& super,
 	int n = sz(b); vi tr(n);
 	rep(i,0,n-1) {
 		if (abs(diag[i]) < 1e-9 * abs(super[i])) { // diag[i] == 0
-			// (For many problems, this can never occur.)
 			b[i+1] -= b[i] * diag[i+1] / super[i];
 			if (i+2 < n) b[i+2] -= b[i] * sub[i+1] / super[i];
 			diag[i+1] = sub[i]; tr[++i] = 1;
