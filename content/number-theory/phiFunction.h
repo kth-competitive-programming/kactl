2ae8d6339e26126bf604fa31a273ea3d56f1f2f7
Simon Lindholm <simon.lindholm10@gmail.com>
2018-11-12 16:13:18 +0100

    Shorter phiFunction
    (thanks, ludopulles)

--- content/number-theory/phiFunction.h
+++ content/number-theory/phiFunction.h
@@ -22,8 +22,6 @@ int phi[LIM];
 
 void calculatePhi() {
 	rep(i,0,LIM) phi[i] = i&1 ? i : i/2;
-	for(int i = 3; i < LIM; i += 2)
-		if(phi[i] == i)
-			for(int j = i; j < LIM; j += i)
-				(phi[j] /= i) *= i-1;
+	for(int i = 3; i < LIM; i += 2) if(phi[i] == i)
+		for(int j = i; j < LIM; j += i) phi[j] -= phi[j] / i;
 }
