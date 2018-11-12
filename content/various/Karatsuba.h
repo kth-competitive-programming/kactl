951ee421f559f6403ca714ab5e689eb5fa1697e2
Simon Lindholm <simon.lindholm10@gmail.com>
2018-11-12 16:41:17 +0100

    Remove pointless comments

--- content/various/Karatsuba.h
+++ content/various/Karatsuba.h
@@ -5,8 +5,6 @@
  * Source: me
  * Description: Faster-than-naive convolution of two sequences: $c[x] = \sum a[i]b[x-i]$.
    Uses the identity $(aX+b)(cX+d) = acX^2 + bd + ((a+c)(b+d)-ac-bd)X$.
-   Doesn't handle sequences of very different length well.
    See also FFT, under the Numerical chapter.
  * Time: O(N^{1.6})
- * Status: tested
  */
