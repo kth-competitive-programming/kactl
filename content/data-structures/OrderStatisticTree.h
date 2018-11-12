2eafce0ef2d1572b81756ba20e4079d9ee94b944
Simon Lindholm <simon.lindholm10@gmail.com>
2018-11-12 16:24:19 +0100

    Explain how to make the order statistic tree into a map

--- content/data-structures/OrderStatisticTree.h
+++ content/data-structures/OrderStatisticTree.h
@@ -5,6 +5,7 @@
  * Source: hacKIT, NWERC 2015
  * Description: A set (not multiset!) with support for finding the n'th
  * element, and finding the index of an element.
+ * To get a map, change \texttt{null\_type}.
  * Time: O(\log N)
  */
 #pragma once
