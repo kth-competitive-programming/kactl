7152840814fbaa3c885eac37ea3b867a7b5ed032
Simon Lindholm <simon.lindholm10@gmail.com>
2018-11-13 14:07:09 +0100

    Shorten modsqrt

--- content/number-theory/ModSqrt.h
+++ content/number-theory/ModSqrt.h
@@ -17,27 +17,22 @@ ll sqrt(ll a, ll p) {
 	assert(modpow(a, (p-1)/2, p) == 1);
 	if (p % 4 == 3) return modpow(a, (p+1)/4, p);
 	// a^(n+3)/8 or 2^(n+3)/8 * 2^(n-1)/4 works if p % 8 == 5
-	ll s = p - 1;
-	int r = 0;
+	ll s = p - 1, n = 2;
+	int r = 0, m;
 	while (s % 2 == 0)
 		++r, s /= 2;
-	ll n = 2; // find a non-square mod p
+	/// find a non-square mod p
 	while (modpow(n, (p - 1) / 2, p) != p - 1) ++n;
 	ll x = modpow(a, (s + 1) / 2, p);
-	ll b = modpow(a, s, p);
-	ll g = modpow(n, s, p);
-	for (;;) {
+	ll b = modpow(a, s, p), g = modpow(n, s, p);
+	for (;; r = m) {
 		ll t = b;
-		int m = 0;
-		for (; m < r; ++m) {
-			if (t == 1) break;
+		for (m = 0; m < r && t != 1; ++m)
 			t = t * t % p;
-		}
 		if (m == 0) return x;
-		ll gs = modpow(g, 1 << (r - m - 1), p);
+		ll gs = modpow(g, 1LL << (r - m - 1), p);
 		g = gs * gs % p;
 		x = x * gs % p;
 		b = b * g % p;
-		r = m;
 	}
 }
