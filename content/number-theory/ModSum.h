0641d9a286ff04ee25756dd78b5a98e9fc0bbc96
Simon Lindholm <simon.lindholm10@gmail.com>
2018-11-13 13:36:29 +0100

    Shorten modsum
    (thanks to ludopulles)

--- content/number-theory/ModSum.h
+++ content/number-theory/ModSum.h
@@ -5,7 +5,7 @@
  * Source: own work
  * Description: Sums of mod'ed arithmetic progressions.
  *
- * \texttt{modsum(to, c, k, m)} = $\sum_{i=0}^{to-1}{(ki+c) \% m}$.
+ * \texttt{modsum(to, c, k, m)} = $\sum_{i=0}^{\mathrm{to}-1}{(ki+c) \% m}$.
  * \texttt{divsum} is similar but for floored division.
  * Status: Tested for |k|,|c|,to,m <= 50, and on "aladin" on kattis.
  * Time: $\log(m)$, with a large constant.
@@ -18,12 +18,9 @@ ull sumsq(ull to) { return to / 2 * ((to-1) | 1); }
 ull divsum(ull to, ull c, ull k, ull m) {
 	ull res = k / m * sumsq(to) + c / m * to;
 	k %= m; c %= m;
-	if (k) {
-		ull to2 = (to * k + c) / m;
-		res += to * to2;
-		res -= divsum(to2, m-1 - c, m, k) + to2;
-	}
-	return res;
+	if (!k) return res;
+	ull to2 = (to * k + c) / m;
+	return res + (to - 1) * to2 - divsum(to2, m-1 - c, m, k);
 }
 
 ll modsum(ull to, ll c, ll k, ll m) {
