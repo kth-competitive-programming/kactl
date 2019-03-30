9c0d98742306d06f62453faa2db527f6b2b34b87
Horace He <horacehe2007@yahoo.com>
2019-03-29 22:37:55 -0400

    Added explanation

--- content/number-theory/MillerRabin.h
+++ content/number-theory/MillerRabin.h
@@ -2,7 +2,8 @@
  * Author: chilli, c1729
  * Date: 2019-03-28
  * License: CC0
- * Description: Deterministic Miller-Rabin primality test. Works for numbers up to 2^64
+ * Description: Deterministic Miller-Rabin primality test. Guaranteed to work for numbers up to 2^64.
+ * Can test 15 random numbers for $a$ for probabilistic test on values higher than 2^64.
  * Time: 7 times the complexity of $a^b \mod c$.
  */
 #pragma once
@@ -15,7 +16,7 @@ bool isPrime(ull n) {
         if (n % p == 0) return (n == p);
     ull d = n - 1;
     while (!(d & 1)) d >>= 1;
-    for (ull a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+    for (ull a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) { 
         ull p = mod_pow(a, d, n), i = d;
         while (p != n - 1 && i != n - 1 && p != 1)
             i <<= 1, p = mod_mul(p, p, n);
