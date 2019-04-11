41a7ed44ef95d273b877586d13b957163a6c3220
Horace He <horacehe2007@yahoo.com>
2019-04-11 19:02:09 -0400

    Formatted to be within 60 characters

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -4,16 +4,15 @@
  * License: CC0
  * Source: MIT Notebook
  * computation in suffix arrays and its applications (2001).
- * Description: Builds suffix array for a string. $a[i]$ is
+ * Description: Builds suffix array for a string. $sa[i]$ is
  * the starting index of the suffix which is $i$-th in the
- * sorted suffix array. The returned vector is of size $n+1$,
- * and $a[0] = n$. The {\tt lcp} function calculates longest
- * common prefixes for neighbouring strings in suffix array.
- * The returned vector is of size $n+1$, and $ret[0] = 0$.
- * Time: $O(N)$ where $N$ is the length of the string
- * for creation of the SA. $O(N)$ for longest common prefixes.
- * Memory: $O(N)$
- * Status: Tested on UVa Live 4513
+ * sorted suffix array. The returned vector is of size
+ * $n+1$, and $sa[0] = n$. The {\tt lcp} function calculates
+ * longest common prefixes for neighbouring strings in
+ * suffix array. The returned vector is of size $n+1$, and
+ * $ret[0] = 0$. Time: $O(N)$ where $N$ is the length of the
+ * string for creation of the SA. $O(N)$ for longest common
+ * prefixes. Memory: $O(N)$ Status: Tested on UVa Live 4513
  */
 #pragma once
 
@@ -38,17 +37,21 @@ struct SuffixArray {
             rep(i, 0, lim) { ws[i] = 0; }
             rep(i, 0, n) ws[wv[i]]++;
             rep(i, 1, lim) ws[i] += ws[i - 1];
-            for (int i = n - 1; i >= 0; i--) sa[--ws[wv[i]]] = y[i];
+            for (int i = n - 1; i >= 0; i--)
+                sa[--ws[wv[i]]] = y[i];
             swap(x, y), p = 1, x[sa[0]] = 0;
-            rep(i, 1, n) x[sa[i]] = cmp(y, sa[i - 1], sa[i], j) ? p - 1 : p++;
+            rep(i, 1, n) x[sa[i]] =
+                cmp(y, sa[i - 1], sa[i], j) ? p - 1 : p++;
         }
-        // LCP
+        // longest common prefixes: res[i] = lcp(a[i],
+        // a[i-1])
         lcp.resize(n - 1);
         vi rank(n - 1);
         int j, k = 0;
         rep(i, 1, n) rank[sa[i]] = i;
         for (int i = 0; i < n; lcp[rank[i++]] = k)
-            for (k ? k-- : 0, j = sa[rank[i] - 1]; s[i + k] == s[j + k]; k++)
+            for (k ? k-- : 0, j = sa[rank[i] - 1];
+                 s[i + k] == s[j + k]; k++)
                 ;
     }
 };
