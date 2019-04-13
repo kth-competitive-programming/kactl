88388636c4c10fcebca2e549b8dc141f5b7f69ca
Horace He <horacehe2007@yahoo.com>
2019-04-13 17:33:46 -0400

    New suffix array implementation (#58)

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -1,80 +1,42 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-27
- * License: CC0
- * Source: folklore and Linear-time longest-common-prefix
- * computation in suffix arrays and its applications (2001).
- * Description: Builds suffix array for a string. $a[i]$ is
- * the starting index of the suffix which is $i$-th in the
- * sorted suffix array. The returned vector is of size $n+1$,
- * and $a[0] = n$. The {\tt lcp} function calculates longest
- * common prefixes for neighbouring strings in suffix array.
- * The returned vector is of size $n+1$, and $ret[0] = 0$.
- * Time: $O(N \log^2 N)$ where $N$ is the length of the string
- * for creation of the SA. $O(N)$ for longest common prefixes.
- * Memory: $O(N)$
- * Status: Tested on UVa Live 4513
+ * Author: chilli
+ * Date: 2019-04-11
+ * License: Unknown
+ * Source: Chinese folklore
+ * Description: Builds suffix array for a string.
+ * \texttt{sa[i]} is the starting index of the suffix which
+ * is $i$'th in the sorted suffix array.
+ * The returned vector is of size $n+1$, and \texttt{sa[0] = n}.
+ * The \texttt{lcp} array contains longest common prefixes for
+ * neighbouring strings in the suffix array:
+ * \texttt{lcp[i] = lcp(sa[i], sa[i-1])}, \texttt{lcp[0] = 0}.
+ * The input string must not contain any zero bytes.
+ * Time: O(n \log n)
+ * Status: fuzz-tested
  */
 #pragma once
 
-typedef pair<ll, int> pli;
-void count_sort(vector<pli> &b, int bits) { // (optional)
-	//this is just 3 times faster than stl sort for N=10^6
-	int mask = (1 << bits) - 1;
-	rep(it,0,2) {
-		int move = it * bits;
-		vi q(1 << bits), w(sz(q) + 1);
-		rep(i,0,sz(b))
-			q[(b[i].first >> move) & mask]++;
-		partial_sum(q.begin(), q.end(), w.begin() + 1);
-		vector<pli> res(b.size());
-		rep(i,0,sz(b))
-			res[w[(b[i].first >> move) & mask]++] = b[i];
-		swap(b, res);
-	}
-}
 struct SuffixArray {
-	vi a;
-	string s;
-	SuffixArray(const string& _s) : s(_s + '\0') {
-		int N = sz(s);
-		vector<pli> b(N);
-		a.resize(N);
-		rep(i,0,N) {
-			b[i].first = s[i];
-			b[i].second = i;
+	vi sa, lcp;
+	SuffixArray(string& s, int lim=256) { // or basic_string<int>
+		int n = sz(s) + 1, k = 0, a, b;
+		vi x(all(s)+1), y(n), ind(n), ws(max(n, lim)), rank(n);
+		sa = lcp = y, iota(all(sa), 0);
+		for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
+			p = j, iota(all(y), n - j);
+			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
+			rep(i,0,n) ind[i] = x[y[i]];
+			fill(all(ws), 0);
+			rep(i,0,n) ws[ind[i]]++;
+			rep(i,1,lim) ws[i] += ws[i - 1];
+			for (int i = n; i--;) sa[--ws[ind[i]]] = y[i];
+			swap(x, y), p = 1, x[sa[0]] = 0;
+			rep(i,1,n) a = sa[i - 1], b = sa[i], x[b] =
+				(y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
 		}
-
-		int q = 8;
-		while ((1 << q) < N) q++;
-		for (int moc = 0;; moc++) {
-			count_sort(b, q); // sort(all(b)) can be used as well
-			a[b[0].second] = 0;
-			rep(i,1,N)
-				a[b[i].second] = a[b[i - 1].second] +
-					(b[i - 1].first != b[i].first);
-
-			if ((1 << moc) >= N) break;
-			rep(i,0,N) {
-				b[i].first = (ll)a[i] << q;
-				if (i + (1 << moc) < N)
-					b[i].first += a[i + (1 << moc)];
-				b[i].second = i;
-			}
-		}
-		rep(i,0,sz(a)) a[i] = b[i].second;
-	}
-	vi lcp() {
-		// longest common prefixes: res[i] = lcp(a[i], a[i-1])
-		int n = sz(a), h = 0;
-		vi inv(n), res(n);
-		rep(i,0,n) inv[a[i]] = i;
-		rep(i,0,n) if (inv[i] > 0) {
-			int p0 = a[inv[i] - 1];
-			while (s[i + h] == s[p0 + h]) h++;
-			res[inv[i]] = h;
-			if(h > 0) h--;
-		}
-		return res;
+		rep(i,1,n) rank[sa[i]] = i;
+		for (int i = 0, j; i < n - 1; lcp[rank[i++]] = k)
+			for (k && k--, j = sa[rank[i] - 1];
+					s[i + k] == s[j + k]; k++);
 	}
 };
