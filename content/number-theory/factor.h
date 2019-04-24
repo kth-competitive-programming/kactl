e71e6fb7fff3eaec7c34e3e0259f5fb6636905e1
Horace He <horacehe2007@yahoo.com>
2019-04-24 13:53:51 -0400

    Responded to most comments

--- content/number-theory/factor.h
+++ content/number-theory/factor.h
@@ -14,22 +14,22 @@
 
 ull f(ull x, ull n) { return (mod_mul(x, x, n) + 1) % n; }
 ull Pollard(ull n) {
+	auto f = [&n](ull x) { return (mod_mul(x, x, n) + 1) % n; };
 	if (isPrime(n)) return n;
 	if (!(n & 1)) return 2;
-	rep(i,2,50) {
-		ull x = i, y = f(x, n), p = __gcd(n + y - x, n);
+	for (ull i=2;;i++){
+		ull x = i, y = f(x), p = __gcd(n + y - x, n);
 		while (p == 1)
-			x = f(x, n), y = f(f(y, n), n), p = __gcd(n + y - x, n);
-		if (p == n) continue;
-		return p;
+			x = f(x), y = f(f(y)), p = __gcd(n + y - x, n);
+		if (p != n) return p;
 	}
 	return 0;
 }
 vector<ull> factor(ull n) {
 	if (n==1) return {};
-    ull x = Pollard(n);
-    if (x == n) return {x};
-    auto l = factor(x), r = factor(n / x);
-    l.insert(l.end(), all(r));
-    return l;
+	ull x = Pollard(n);
+	if (x == n) return {x};
+	auto l = factor(x), r = factor(n / x);
+	l.insert(l.end(), all(r));
+	return l;
 }
