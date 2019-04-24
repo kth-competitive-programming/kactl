5e1707eaccad45c0024f81caf29523b556f599fe
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-24 02:21:01 +0200

    Formatting

--- content/numerical/NumberTheoreticTransform.h
+++ content/numerical/NumberTheoreticTransform.h
@@ -19,32 +19,31 @@ const ll mod = (119 << 23) + 1, root = 62; // = 998244353
 // and 483 << 21 (same root). The last two are > 10^9.
 
 typedef vector<ll> vl;
-void ntt(vl &a, vl &rt, vl &rev, int n) {
+void ntt(vl& a, vl& rt, vl& rev, int n) {
 	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
 	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k)
-			rep(j,0,k) {
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
 				ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
-				a[i + j + k] = z > ai ? ai - z + mod : ai - z;
-				ai += ai + z >= mod ? z - mod : z;
-			}
+				a[i + j + k] = (z > ai ? ai - z + mod : ai - z);
+				ai += (ai + z >= mod ? z - mod : z);
+	}
 }
 
-vl conv(const vl &a, const vl &b) {
+vl conv(const vl& a, const vl& b) {
 	if (a.empty() || b.empty())
 		return {};
-	int s = sz(a) + sz(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+	int s = sz(a)+sz(b)-1, B = 32 - __builtin_clz(s), n = 1 << B;
 	vl L(a), R(b), out(n), rt(n, 1), rev(n);
 	L.resize(n), R.resize(n);
 	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << B) / 2;
 	int curL = mod / 2;
 	for (int k = 2; k < n; k *= 2) {
 		ll z[] = {1, modpow(root, curL /= 2)};
-		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1 ] %mod;
+		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1] % mod;
 	}
 	ntt(L, rt, rev, n), ntt(R, rt, rev, n);
 	ll inv = modpow(n, mod - 2);
-	rep(i,0,n) out[-i&(n-1)] = L[i] * R[i] % mod * inv % mod;
+	rep(i,0,n) out[-i & (n-1)] = L[i] * R[i] % mod * inv % mod;
 	ntt(out, rt, rev, n);
 	return {out.begin(), out.begin() + s};
 }
