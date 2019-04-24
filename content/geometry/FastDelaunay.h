5213474332c6585d615713636400717b15b83204
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-24 03:16:09 +0200

    Update FastDelaunay description

--- content/geometry/FastDelaunay.h
+++ content/geometry/FastDelaunay.h
@@ -3,7 +3,9 @@
  * Date: 2016
  * License: MIT
  * Source: https://github.com/Bathlamos/delaunay-triangulation/
- * Description: Fast Delaunay triangulation. There must be no duplicate points.
+ * Description: Fast Delaunay triangulation.
+ * Each circumcircle contains none of the input points.
+ * There must be no duplicate points.
  * If all points are on a line, no triangles will be returned.
  * Should work for doubles as well, though there may be precision issues in 'circ'.
  * Returns triangles in order \{t[0][0], t[0][1], t[0][2], t[1][0], \dots\}, all counter-clockwise.
