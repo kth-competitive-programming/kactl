d3635fcd306aa6395a1355556fe1ad1edeaa95ca
Horace He <horacehe2007@yahoo.com>
2019-04-24 14:37:55 -0400

    Updated in response to comments

--- content/number-theory/factor.h
+++ content/number-theory/factor.h
@@ -3,7 +3,7 @@
  * Date: 2019-04-24
  * License: CC0
  * Source: https://github.com/FTRobbin/Dreadnought-Standard-Code-Library
- * Description: Pollard's rho algorithm: a probabilistic factorisation
+ * Description: Pollard's rho algorithm. A probabilistic factorisation
  * algorithm whose expected time complexity is good.
  * Time: On worst case numbers, $O(n^{1/4})$. Faster than that for most numbers.
  */
@@ -12,10 +12,8 @@
 #include "ModMulLL.h"
 #include "MillerRabin.h"
 
-ull f(ull x, ull n) { return (mod_mul(x, x, n) + 1) % n; }
 ull Pollard(ull n) {
-	auto f = [&n](ull x) { return (mod_mul(x, x, n) + 1) % n; };
-	if (isPrime(n)) return n;
+	auto f = [n](ull x) { return (mod_mul(x, x, n) + 1) % n; };
 	if (!(n & 1)) return 2;
 	for (ull i = 2;; i++) {
 		ull x = i, y = f(x), p = __gcd(n + y - x, n);
@@ -23,12 +21,11 @@ ull Pollard(ull n) {
 			x = f(x), y = f(f(y)), p = __gcd(n + y - x, n);
 		if (p != n) return p;
 	}
-	return 0;
 }
 vector<ull> factor(ull n) {
 	if (n==1) return {};
+	if (isPrime(n)) return {n};
 	ull x = Pollard(n);
-	if (x == n) return {x};
 	auto l = factor(x), r = factor(n / x);
 	l.insert(l.end(), all(r));
 	return l;
