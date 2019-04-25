b83e867a29a55e5d75c17997796bec7fce06ad7b
Horace He <horacehe2007@yahoo.com>
2019-04-25 07:25:44 -0400

    Fixed some nitpicky formatting things

--- content/numerical/IntegrateAdaptive.h
+++ content/numerical/IntegrateAdaptive.h
@@ -18,14 +18,13 @@
 typedef double d;
 #define S(a,b) (f(a) + 4*f((a+b) / 2) + f(b)) * (b-a) / 6
 
-template<class F>
-d rec(F f, d a, d b, d eps, d S) {
-	d c = (a+b) / 2;
-	d S1 = S(a, c);
-	d S2 = S(c, b), T = S1 + S2;
-	if (abs (T - S) <= 15*eps || b-a < 1e-10)
-		return T + (T - S) / 15;
-	return rec(f, a, c, eps/2, S1) + rec(f, c, b, eps/2, S2);
+template <class F> d rec(F f, d a, d b, d eps, d S) {
+    d c = (a + b) / 2;
+    d S1 = S(a, c);
+    d S2 = S(c, b), T = S1 + S2;
+    if (abs(T - S) <= 15 * eps || b - a < 1e-10)
+        return T + (T - S) / 15;
+    return rec(f, a, c, eps / 2, S1) + rec(f, c, b, eps / 2, S2);
 }
 template<class F>
 d quad(d a, d b, F f, d eps = 1e-8) {
