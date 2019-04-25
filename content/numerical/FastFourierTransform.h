e83e65b16ee9d89653da4d383b567347669f5f2a
Horace He <horacehe2007@yahoo.com>
2019-04-25 03:26:28 -0400

    removed another 2 lines

--- content/numerical/FastFourierTransform.h
+++ content/numerical/FastFourierTransform.h
@@ -19,58 +19,56 @@ typedef complex<double> C;
 typedef vector<double> vd;
 
 void fft(vector<C> &a, int n, int L) {
-    vector<C> rt(n, 1); vi rev(n);
-    rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-    for (int k = 2; k < n; k *= 2) {
-        C z[] = {1, polar(1.0, M_PI / k)};
-        rep(i, k, 2 * k) rt[i] = rt[i / 2] * z[i & 1];
-    }
-    rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-    for (int k = 1; k < n; k *= 2)
-        for (int i = 0; i < n; i += 2 * k) rep(j, 0, k) {
-                // C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
-                auto x = (double *)&rt[j + k], y = (double *)&a[i + j + k]; /// exclude-line
-                C z(x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]);  /// exclude-line
-                a[i + j + k] = a[i + j] - z;
-                a[i + j] += z;
-            }
+	vector<C> rt(n, 1); vi rev(n);
+	rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	for (int k = 2; k < n; k *= 2) {
+		C z[] = {1, polar(1.0, M_PI / k)};
+		rep(i, k, 2 * k) rt[i] = rt[i / 2] * z[i & 1];
+	}
+	rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	for (int k = 1; k < n; k *= 2)
+		for (int i = 0; i < n; i += 2 * k) rep(j, 0, k) {
+				// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+				auto x = (double *)&rt[j + k], y = (double *)&a[i + j + k]; /// exclude-line
+				C z(x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]);  /// exclude-line
+				a[i + j + k] = a[i + j] - z;
+				a[i + j] += z;
+			}
 }
 
 vd conv(const vd &a, const vd &b) {
-    if (a.empty() || b.empty()) return {};
-    vd res(sz(a) + sz(b) - 1);
-    int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
-    vector<C> in(n), out(n);
-    copy(all(a), begin(in));
-    rep(i, 0, sz(b)) in[i].imag(b[i]);
-    fft(in, n, L);
-    trav(x, in) x *= x;
-    rep(i, 0, n) out[i] = in[(n - i) & (n - 1)] - conj(in[i]);
-    fft(out, n, L);
-    rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
-    return res;
+	if (a.empty() || b.empty()) return {};
+	vd res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+	vector<C> in(n), out(n);
+	copy(all(a), begin(in));
+	rep(i, 0, sz(b)) in[i].imag(b[i]);
+	fft(in, n, L);
+	trav(x, in) x *= x;
+	rep(i, 0, n) out[i] = in[(n - i) & (n - 1)] - conj(in[i]);
+	fft(out, n, L);
+	rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
+	return res;
 }
 
 template <int M> vector<ll> convMod(const vi &a, const vi &b) {
-    if (a.empty() || b.empty()) return {};
-    vector<ll> res(sz(a) + sz(b) - 1);
-    int L = 32 - __builtin_clz(sz(res)), n = 1 << L, cut = sqrt(M);
-    vector<C> inl(n), inr(n), outs(n), outl(n);
-    rep(i, 0, a.size()) inl[i] = {a[i] / cut, a[i] % cut};
-    rep(i, 0, b.size()) inr[i] = {b[i] / cut, b[i] % cut};
-    fft(inl, n, L), fft(inr, n, L);
-    rep(i, 0, n) {
-        int j = (n - i) & (n - 1);
-        outl[-i & (n - i)] = (inl[i] + conj(inl[j])) * 0.5 * inr[i];
-        outs[-i & (n - i)] = (inl[i] - conj(inl[j])) * -.5i * inr[i];
-    }
-    fft(outl, n, L), fft(outs, n, L);
-    rep(i, 0, sz(res)) {
-        outl[i] /= n, outs[i] /= n;
-        ll av = ll(round(outl[i].real())) % M;
-        ll bv = ll(round(outl[i].imag()) + round(outs[i].real())) % MOD;
-        ll cv = ll(round(outs[i].imag())) % M;
-        res[i] = ((av * cut + bv) * cut + cv) % MOD;
-    }
-    return res;
+	if (a.empty() || b.empty()) return {};
+	vector<ll> res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L, cut = sqrt(M);
+	vector<C> inl(n), inr(n), outs(n), outl(n);
+	rep(i, 0, a.size()) inl[i] = {a[i] / cut, a[i] % cut};
+	rep(i, 0, b.size()) inr[i] = {b[i] / cut, b[i] % cut};
+	fft(inl, n, L), fft(inr, n, L);
+	rep(i, 0, n) {
+		int j = -i & (n - 1);
+		outl[j] = (inl[i] + conj(inl[j])) * inr[i] / (2.0 * n);
+		outs[j] = (inl[i] - conj(inl[j])) * inr[i] / (2.0 * n) / 1i;
+	}
+	fft(outl, n, L), fft(outs, n, L);
+	rep(i, 0, sz(res)) {
+		ll av = round(outl[i].real()), cv = round(outs[i].imag());
+		ll bv = round(outl[i].imag()) + round(outs[i].real());
+		res[i] = ((av % M * cut + bv % M) * cut + cv % M) % M;
+	}
+	return res;
 }
