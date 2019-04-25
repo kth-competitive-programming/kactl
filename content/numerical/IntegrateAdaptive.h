acac52a5955509c884169763b4405989d2c2de08
Horace He <horacehe2007@yahoo.com>
2019-04-25 07:27:43 -0400

    Aligned example for integrateadaptive

--- content/numerical/IntegrateAdaptive.h
+++ content/numerical/IntegrateAdaptive.h
@@ -8,9 +8,9 @@
  * Usage:
 	double sphereVol =
 	quad(-1, 1, [](double x) {
-		return quad(-1, 1, [x](double y) {
-			return quad(-1, 1, [x, y](double z) {
-				return x * x + y * y + z * z <= 1; });});});
+	return quad(-1, 1, [x](double y) {
+	return quad(-1, 1, [x, y](double z) {
+	return x * x + y * y + z * z <= 1; });});});
 	double pi = sphereVol * 3 / 4;
  */
 #pragma once
@@ -18,13 +18,14 @@
 typedef double d;
 #define S(a,b) (f(a) + 4*f((a+b) / 2) + f(b)) * (b-a) / 6
 
-template <class F> d rec(F f, d a, d b, d eps, d S) {
-    d c = (a + b) / 2;
-    d S1 = S(a, c);
-    d S2 = S(c, b), T = S1 + S2;
-    if (abs(T - S) <= 15 * eps || b - a < 1e-10)
-        return T + (T - S) / 15;
-    return rec(f, a, c, eps / 2, S1) + rec(f, c, b, eps / 2, S2);
+template <class F>
+d rec(F f, d a, d b, d eps, d S) {
+		d c = (a + b) / 2;
+		d S1 = S(a, c);
+		d S2 = S(c, b), T = S1 + S2;
+		if (abs(T - S) <= 15 * eps || b - a < 1e-10)
+				return T + (T - S) / 15;
+		return rec(f, a, c, eps / 2, S1) + rec(f, c, b, eps / 2, S2);
 }
 template<class F>
 d quad(d a, d b, F f, d eps = 1e-8) {
