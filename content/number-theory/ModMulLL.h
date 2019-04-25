93af2c026ce835cf96dcd324dfccf2012b4385a0
Horace He <horacehe2007@yahoo.com>
2019-04-25 14:44:16 -0400

    Switched to iterative modpow

--- content/number-theory/ModMulLL.h
+++ content/number-theory/ModMulLL.h
@@ -1,7 +1,7 @@
 /**
- * Author: chilli, Ramchandra Apte
+ * Author: chilli, Ramchandra Apte, Noam
  * Date: 2019-04-24
- * License: MIT
+ * License: CC0
  * Source: https://github.com/RamchandraApte/OmniTemplate/blob/master/modulo.h
  * Description: Calculate $a\cdot b\bmod c$ (or $a^b \bmod c$) for $0 \le a, b < c < 2^{63}$.
  * Time: O(1) for \texttt{mod\_mul}, O(\log b) for \texttt{mod\_pow}
@@ -14,9 +14,9 @@ ull mod_mul(ull a, ull b, ull M) {
 	ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
 	return ret + M * ((ret < 0) - (ret >= (ll)M));
 }
-ull mod_pow(ull a, ull b, ull mod) {
-	if (b == 0) return 1;
-	ull res = mod_pow(a, b / 2, mod);
-	res = mod_mul(res, res, mod);
-	return b & 1? mod_mul(res, a, mod) : res;
+ull mod_pow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = mod_mul(b, b, mod), e /= 2)
+		if (e & 1) ans = mod_mul(ans, b, mod);
+	return ans;
 }
