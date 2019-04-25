2af7fb2ca8f80a80adb5a9870d87b58e5ecbdffe
Horace He <horacehe2007@yahoo.com>
2019-04-24 21:24:48 -0400

    Updated descrpition

--- content/number-theory/ModMulLL.h
+++ content/number-theory/ModMulLL.h
@@ -3,8 +3,8 @@
  * Date: 2019-04-24
  * License: MIT
  * Source: https://github.com/RamchandraApte/OmniTemplate/blob/master/modulo.h
- * Works for numbers < 2^63.
- * Description: Calculate $a\cdot b\bmod c$ (or $a^b \bmod c$) for large $c$.
+ * Description: Calculate $a\cdot b\bmod c$ (or $a^b \bmod c$) for $c < 2^63$.
+ * a and b must lie in the range [0, mod) before input.
  * Time: O(1) for mod_mul, O(log b) for mod_pow
  */
 #pragma once
@@ -13,7 +13,7 @@ typedef unsigned long long ull;
 typedef long double ld;
 ull mod_mul(ull a, ull b, ull M) {
 	ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
-	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+	return ret + M * ((ret < 0) - (ret >= (ll)M));
 }
 ull mod_pow(ull a, ull b, ull mod) {
 	if (b == 0) return 1;
