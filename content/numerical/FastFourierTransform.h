7c16aeb886fe91b8b770bd7d75d817f44c9db6ed
Horace He <horacehe2007@yahoo.com>
2019-04-26 14:22:34 -0400

    Fixed rep space issues

--- content/numerical/FastFourierTransform.h
+++ content/numerical/FastFourierTransform.h
@@ -20,14 +20,14 @@ typedef vector<double> vd;
 
 void fft(vector<C> &a, int n, int L) {
 	vector<C> rt(n, 1); vi rev(n);
-	rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
 	for (int k = 2; k < n; k *= 2) {
 		C z[] = {1, polar(1.0, M_PI / k)};
-		rep(i, k, 2 * k) rt[i] = rt[i / 2] * z[i & 1];
+		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1];
 	}
-	rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
 	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j, 0, k) {
+		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
 				// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
 				auto x = (double *)&rt[j + k], y = (double *)&a[i + j + k]; /// exclude-line
 				C z(x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]);  /// exclude-line
@@ -42,12 +42,12 @@ vd conv(const vd &a, const vd &b) {
 	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
 	vector<C> in(n), out(n);
 	copy(all(a), begin(in));
-	rep(i, 0, sz(b)) in[i].imag(b[i]);
+	rep(i,0,sz(b)) in[i].imag(b[i]);
 	fft(in, n, L);
 	trav(x, in) x *= x;
-	rep(i, 0, n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
 	fft(out, n, L);
-	rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
+	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
 
 	return res;
 }
