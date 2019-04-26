ace08abde3afc942d3e989eeb5ef432286ff7392
Horace He <horacehe2007@yahoo.com>
2019-04-26 14:16:56 -0400

    Made things fit within 63 columns

--- content/numerical/FastFourierTransformMod.h
+++ content/numerical/FastFourierTransformMod.h
@@ -13,21 +13,20 @@
 
 #include "FastFourierTransform.h"
 
-template <int M>
-vi convMod(const vi &a, const vi &b) {
+template <int M> vi convMod(const vi &a, const vi &b) {
 	if (a.empty() || b.empty()) return {};
 	vi res(sz(a) + sz(b) - 1);
-	int L = 32 - __builtin_clz(sz(res)), n = 1 << L, cut = sqrt(M);
-	vector<C> inl(n), inr(n), outs(n), outl(n);
-	rep(i, 0, a.size()) inl[i] = {a[i] / cut, a[i] % cut};
-	rep(i, 0, b.size()) inr[i] = {b[i] / cut, b[i] % cut};
-	fft(inl, n, L), fft(inr, n, L);
+	int B = 32 - __builtin_clz(sz(res)), n = 1<<B, cut = sqrt(M);
+	vector<C> L(n), R(n), outs(n), outl(n);
+	rep(i, 0, a.size()) L[i] = {a[i] / cut, a[i] % cut};
+	rep(i, 0, b.size()) R[i] = {b[i] / cut, b[i] % cut};
+	fft(L, n, B), fft(R, n, B);
 	rep(i, 0, n) {
 		int j = -i & (n - 1);
-		outl[j] = (inl[i] + conj(inl[j])) * inr[i] / (2.0 * n);
-		outs[j] = (inl[i] - conj(inl[j])) * inr[i] / (2.0 * n) / 1i;
+		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
 	}
-	fft(outl, n, L), fft(outs, n, L);
+	fft(outl, n, B), fft(outs, n, B);
 	rep(i, 0, sz(res)) {
 		ll av = round(outl[i].real()), cv = round(outs[i].imag());
 		ll bv = round(outl[i].imag()) + round(outs[i].real());
