dcdc34aeb59dc8e52eafdf20f4b6f6926078d378
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-26 20:02:30 +0200

    Add -dD to hashing

--- content/contest/hash.sh
+++ content/contest/hash.sh
@@ -1,3 +1,3 @@
 # Hashes a file, ignoring all whitespace and comments. Use for
 # verifying that code was correctly typed.
-cpp -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c1-6
+cpp -dD -P -fpreprocessed | tr -d '[:space:]'| md5sum |cut -c-6
