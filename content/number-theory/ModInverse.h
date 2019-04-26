ce986e79a1c9bd638fa6052819178309533d6289
Horace He <horacehe2007@yahoo.com>
2019-04-26 18:01:52 -0400

    more stylistic changes to tests

--- content/number-theory/ModInverse.h
+++ content/number-theory/ModInverse.h
@@ -9,5 +9,5 @@
 #pragma once
 
 const ll mod = 1000000007, LIM = 200000;
-ll* inv = new ll[LIM] - 1; inv[1] = 1;
+vector<ll> inv(LIM); inv[1] = 1;
 rep(i,2,LIM) inv[i] = mod - (mod / i) * inv[mod % i] % mod;
