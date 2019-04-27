8fc3f3281fb472948d3c3c4c5b6c96680ad51323
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-28 01:56:24 +0200

    Add proof

--- content/number-theory/ModMulLL.h
+++ content/number-theory/ModMulLL.h
@@ -3,8 +3,10 @@
  * Date: 2019-04-24
  * License: CC0
  * Source: https://github.com/RamchandraApte/OmniTemplate/blob/master/modulo.h
+ * Proof of correctness is in doc/modmul-proof.md.
  * Description: Calculate $a\cdot b\bmod c$ (or $a^b \bmod c$) for $0 \le a, b < c < 2^{63}$.
  * Time: O(1) for \texttt{mod\_mul}, O(\log b) for \texttt{mod\_pow}
+ * Status: fuzz-tested, proven correct
  */
 #pragma once
 
