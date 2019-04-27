8f58fbfea4743112e13e64fed73c9ecda499f982
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-27 12:30:42 +0200

    Codegolf

--- content/graph/Dinic.h
+++ content/graph/Dinic.h
@@ -7,45 +7,43 @@
  * On bipartite graphs the complexity is $O(\sqrt{V}E)$, and on unit graphs $O(\min(V^{1/2}, E^{2/3})E)).
  * Status: Tested on SPOJ FASTFLOW and SPOJ MATCHING
  */
-template<class T=int> struct Dinic {
-	const T INF = numeric_limits<T>::max();
-	struct edge {
+struct Dinic {
+	struct Edge {
 		int to, rev;
-		T c, f;
+		ll c, f;
 	};
-	vi lvl, ptr;
-	vector<vector<edge>> adj;
-	Dinic(int n) : lvl(n), ptr(n), adj(n) {}
-	void addEdge(int a, int b, T c, int rcap = 0) {
+	vi lvl, ptr, q;
+	vector<vector<Edge>> adj;
+	Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
+	void addEdge(int a, int b, ll c, int rcap = 0) {
 		adj[a].push_back({b, sz(adj[b]), c, 0});
 		adj[b].push_back({a, sz(adj[a]) - 1, rcap, 0});
 	}
-	T dfs(int v, T f, int t) {
+	ll dfs(int v, int t, ll f) {
 		if (v == t || !f) return f;
-		for (; ptr[v] < sz(adj[v]); ptr[v]++) {
-			edge &e = adj[v][ptr[v]];
-			if (lvl[e.to] != lvl[v] + 1) continue;
-			if (T p = dfs(e.to, min(f, e.c - e.f), t)) {
-				e.f += p, adj[e.to][e.rev].f -= p;
-				return p;
-			}
+		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+			Edge& e = adj[v][i];
+			if (lvl[e.to] == lvl[v] + 1)
+				if (ll p = dfs(e.to, t, min(f, e.c - e.f))) {
+					e.f += p, adj[e.to][e.rev].f -= p;
+					return p;
+				}
 		}
 		return 0;
 	}
 	ll calc(int s, int t) {
-		ll flow = 0;
-		while(true) {
-			fill(all(ptr), 0), fill(all(lvl), -1), lvl[s] = 0;
-			queue<int> q({s});
-			while (!q.empty() && lvl[t] == -1) {
-				int v = q.front();
-				q.pop();
+		ll flow = 0; q[0] = s;
+		do {
+			lvl = ptr = vi(sz(q));
+			int qi = 0, qe = lvl[s] = 1;
+			while (qi < qe && !lvl[t]) {
+				int v = q[qi++];
 				trav(e, adj[v])
-					if (lvl[e.to] == -1 && e.f < e.c)
-						q.push(e.to), lvl[e.to] = lvl[v] + 1;
+					if (!lvl[e.to] && e.f < e.c)
+						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
 			}
-			if(lvl[t] == -1) return flow;
-			while (T p = dfs(s, INF, t)) flow += p;
-		}
+			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+		} while (lvl[t]);
+		return flow;
 	}
 };
