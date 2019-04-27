4a6c2ccf9c46f02e0e2296c1d8b8da3da5347a25
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-28 01:55:56 +0200

    Slight optimization

--- content/number-theory/ModMulLL.h
+++ content/number-theory/ModMulLL.h
@@ -12,7 +12,7 @@ typedef unsigned long long ull;
 typedef long double ld;
 ull mod_mul(ull a, ull b, ull M) {
 	ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
-	return ret + M * ((ret < 0) - (ret >= (ll)M));
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
 }
 ull mod_pow(ull b, ull e, ull mod) {
 	ull ans = 1;
