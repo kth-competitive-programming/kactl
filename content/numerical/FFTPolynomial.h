5ea7caae2aab045ac410e6cb76f40eea40581a34
Horace He <horacehe2007@yahoo.com>
2019-04-27 17:54:12 -0400

    Added log/exp

--- content/numerical/FFTPolynomial.h
+++ content/numerical/FFTPolynomial.h
@@ -68,8 +68,20 @@ poly deriv(poly a) {
 }
 poly integr(poly a) {
     if (a.empty())
-        return {};
+        return {0};
     poly b(sz(a) + 1);
-    rep(i, 0, sz(a) + 1) b[i + 1] = a[i] / num(i + 1);
+    rep(i,1,sz(b)) b[i] = a[i - 1] / num(i);
     return b;
+}
+poly log(poly a) { return modK(integr(deriv(a) * inverse(a)), sz(a)); }
+poly exp(poly a) {
+    poly b(1, num(1));
+    if (a.empty())
+        return b;
+    while (sz(b) < sz(a)) {
+        b.resize(sz(b)*2);
+        b *= (poly({num(1)}) + modK(a, sz(b)) - log(b));
+        b.resize(sz(b)/2 + 1);
+    }
+    return modK(b, sz(a));
 }
