780f2dbb386797bcffd50b197ef1199a82a66ae9
Horace He <horacehe2007@yahoo.com>
2019-04-27 12:16:11 -0400

    Put it on one line now

--- content/geometry/onSegment.h
+++ content/geometry/onSegment.h
@@ -11,6 +11,5 @@
 #include "Point.h"
 
 template <class P> bool onSegment(P s, P e, P p) {
-	return s.cross(e, p) == 0 &&
-			(s - p).dot(e - p) <= 0;
+	return s.cross(e, p) == 0 && (s - p).dot(e - p) <= 0;
 }
