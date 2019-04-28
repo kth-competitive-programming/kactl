e18a1e9121d3885027dfe9774c88de44f2b4f3ec
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-28 14:35:07 +0200

    Remove dead code

--- fuzz-tests/data-structures/LazySegmentTree.cpp
+++ fuzz-tests/data-structures/LazySegmentTree.cpp
@@ -11,7 +11,6 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 #include "../../content/data-structures/LazySegmentTree.h"
-// #include "../../content/data-structures/old.h"
 
 static unsigned R;
 int ra() {
