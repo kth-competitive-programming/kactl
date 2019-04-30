090ecdb8d6a1f877a7c03bf45e7d6a23c5fb5dc6
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-30 16:09:02 +0200

    Make Bellman-Ford 2x faster
    If we have it we might as well make it fast...

--- content/graph/BellmanFord.h
+++ content/graph/BellmanFord.h
@@ -5,30 +5,32 @@
  * Source: http://en.wikipedia.org/wiki/Bellman-Ford_algorithm
  * Description: Calculates shortest paths from $s$ in a graph that might have negative edge weights.
  * Unreachable nodes get dist = inf; nodes reachable through negative-weight cycles get dist = -inf.
- * Assumes $VE \cdot \max |w_i| < 2^{63}$.
+ * Assumes $V^2 \max |w_i| < \tilde{} 2^{63}$.
  * Time: O(EV)
  * Status: Tested on kattis:shortestpath3
  */
 #pragma once
 
 const ll inf = LLONG_MAX;
-struct Ed { int src, dest, w; };
+struct Ed { int a, b, w, s() { return a < b ? a : -a; }};
 struct Node { ll dist = inf; int prev = -1; };
 
 void bellmanFord(vector<Node>& nodes, vector<Ed>& eds, int s) {
 	nodes[s].dist = 0;
+	sort(all(eds), [](Ed a, Ed b) { return a.s() < b.s(); });
 
-	rep(i,0,sz(nodes)) trav(e, eds) {
-		Node cur = nodes[e.src], &dest = nodes[e.dest];
+	int lim = sz(nodes) / 2 + 2; // /3+100 with shuffled vertices
+	rep(i,0,lim) trav(ed, eds) {
+		Node cur = nodes[ed.a], &dest = nodes[ed.b];
 		if (abs(cur.dist) == inf) continue;
-		ll d = cur.dist + e.w;
+		ll d = cur.dist + ed.w;
 		if (d < dest.dist) {
-			dest.prev = e.src;
-			dest.dist = (i < sz(nodes)-1 ? d : -inf);
+			dest.prev = ed.a;
+			dest.dist = (i < lim-1 ? d : -inf);
 		}
 	}
-	rep(i,0,sz(nodes)) trav(e, eds) {
-		if (nodes[e.src].dist == -inf)
-			nodes[e.dest].dist = -inf;
+	rep(i,0,lim) trav(e, eds) {
+		if (nodes[e.a].dist == -inf)
+			nodes[e.b].dist = -inf;
 	}
 }
--- content/graph/FloydWarshall.h
+++ content/graph/FloydWarshall.h
@@ -14,7 +14,7 @@
 const ll inf = 1LL << 62;
 void floydWarshall(vector<vector<ll>>& m) {
 	int n = sz(m);
-	rep(i,0,n) m[i][i] = min(m[i][i], {});
+	rep(i,0,n) m[i][i] = min(m[i][i], 0LL);
 	rep(k,0,n) rep(i,0,n) rep(j,0,n)
 		if (m[i][k] != inf && m[k][j] != inf) {
 			auto newDist = max(m[i][k] + m[k][j], -inf);
