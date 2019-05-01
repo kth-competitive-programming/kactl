ba04224628a78b0b084cf90a4676bedc2d6c6cc6
Horace He <horacehe2007@yahoo.com>
2019-04-30 23:39:17 -0400

    Updated hashmap with custom hash

--- content/data-structures/HashMap.h
+++ content/data-structures/HashMap.h
@@ -9,7 +9,16 @@
 #pragma once
 
 #include <bits/extc++.h> /** keep-include */
-__gnu_pbds::gp_hash_table<ll, int> h({},{},{},{}, {1 << 16});
+typedef unsigned long long ull;
+struct chash {
+    ull hash_f(ull x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+};
+__gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
 
 /** For CodeForces, or other places where hacking might be a problem:
 const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
