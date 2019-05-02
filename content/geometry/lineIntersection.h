fab7b2eb488fa1e5dcfbb44395e7d661590be4e1
Horace He <horacehe2007@yahoo.com>
2019-05-02 01:35:12 -0400

    made change

--- content/geometry/lineIntersection.h
+++ content/geometry/lineIntersection.h
@@ -5,13 +5,13 @@
  * Source:
  * Description:\\
 \begin{minipage}{75mm}
-If a unique intersetion point of the lines going through s1,e1 and s2,e2 exists r is set to this point and 1 is returned. If no intersection point exists 0 is returned and if infinitely many exists -1 is returned. If s1==e1 or s2==e2 -1 is returned. The wrong position will be returned if P is Point<int> and the intersection point does not have integer coordinates. Products of three coordinates are used in intermediate steps so watch out for overflow if using int or long long.
+If a unique intersection point of the lines going through s1,e1 and s2,e2 exists r is set to this point and 1 is returned. If no intersection point exists 0 is returned and if infinitely many exists -1 is returned. If s1==e1 or s2==e2 -1 is returned. The wrong position will be returned if P is Point<int> and the intersection point does not have integer coordinates. Products of three coordinates are used in intermediate steps so watch out for overflow if using int or long long.
 \end{minipage}
 \begin{minipage}{15mm}
 \includegraphics[width=\textwidth]{content/geometry/lineIntersection}
 \end{minipage}
  * Status: tested
- * Usage: 
+ * Usage:
  * 	point<double> intersection;
  * 	if (1 == LineIntersection(s1,e1,s2,e2,intersection))
  * 		cout << "intersection point at " << intersection << endl;
@@ -21,11 +21,10 @@ If a unique intersetion point of the lines going through s1,e1 and s2,e2 exists 
 #include "Point.h"
 
 template<class P>
-int lineIntersection(const P& s1, const P& e1, const P& s2,
-		const P& e2, P& r) {
-	if ((e1-s1).cross(e2-s2)) { //if not parallell
-		r = s2-(e2-s2)*(e1-s1).cross(s2-s1)/(e1-s1).cross(e2-s2);
-		return 1;
-	} else
-		return -((e1-s1).cross(s2-s1)==0 || s2==e2);
+pair<int, P> lineIntersection(P s1, P e1, P s2, P e2) {
+	auto d = (e1-s1).cross(e2-s2);
+	if (d == 0)  //if parallel
+		return {-((e1-s1).cross(s2-s1)==0 || s2==e2), P(0,0)};
+	else
+		return {1, s2-(e2-s2)*(e1-s1).cross(s2-s1)/d};
 }
