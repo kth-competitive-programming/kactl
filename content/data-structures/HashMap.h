e943756fac5caa18792e82f18a1d2334503d149a
Horace He <horacehe2007@yahoo.com>
2019-05-02 13:53:57 -0400

    Fixed column limits and CF hack-proof version

--- content/data-structures/HashMap.h
+++ content/data-structures/HashMap.h
@@ -9,16 +9,19 @@
 #pragma once
 
 #include <bits/extc++.h> /** keep-include */
-struct chash { // To use most bits rather than just the lowest ones:
-    const uint64_t C = ll(2e18 * M_PI) + 71; // large odd number
-    ll operator()(ll x) const { return __builtin_bswap64(x*C); }
+// To use most bits rather than just the lowest ones:
+struct chash {
+	const uint64_t C = ll(2e18 * M_PI) + 71; // large odd number
+	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
 };
-__gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
+__gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
 
 /** For CodeForces, or other places where hacking might be a problem:
+
 const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(ll x) const { return x + RANDOM; }
+struct chash { // To use most bits rather than just the lowest ones:
+	const uint64_t C = ll(2e18 * M_PI) + 71; // large odd number
+	ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
 };
 __gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
 */
