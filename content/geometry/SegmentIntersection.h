37bbb38ad7eb3986e8a2aa684e5d153a021d7a62
Horace He <horacehe2007@yahoo.com>
2019-05-04 16:31:39 -0400

    Updated description of condition in segmentintersection

--- content/geometry/SegmentIntersection.h
+++ content/geometry/SegmentIntersection.h
@@ -27,7 +27,7 @@ Products of three coordinates are used in intermediate steps so watch out for ov
 template<class P> vector<P> segInter(P a, P b, P c, P d) {
 	auto oa = c.cross(d, a), ob = c.cross(d, b),
 	     oc = a.cross(b, c), od = a.cross(b, d);
-	// Returns if only one intersection exists
+	// Checks if intersection is single non-endpoint point.
 	if (sgn(oa) * sgn(ob) < 0 && sgn(oc) * sgn(od) < 0)
 		return {(a * ob - b * oa) / (ob - oa)};
 	set<P> s;
