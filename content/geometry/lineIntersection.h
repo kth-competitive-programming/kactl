b5fb51f4bf2b20adfefd71797e2ef5bfcfd991c1
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-04 23:01:35 +0200

    Tiny description fixes

--- content/geometry/lineIntersection.h
+++ content/geometry/lineIntersection.h
@@ -5,7 +5,7 @@
  * Source:
  * Description:\\
 \begin{minipage}{75mm}
-If a unique intersetion point of the lines going through s1,e1 and s2,e2 exists r is set to this point and 1 is returned. If no intersection point exists 0 is returned and if infinitely many exists -1 is returned. If s1==e1 or s2==e2 -1 is returned. The wrong position will be returned if P is Point<int> and the intersection point does not have integer coordinates. Products of three coordinates are used in intermediate steps so watch out for overflow if using int or long long.
+If a unique intersection point of the lines going through s1,e1 and s2,e2 exists r is set to this point and 1 is returned. If no intersection point exists 0 is returned and if infinitely many exists -1 is returned. If s1==e1 or s2==e2 -1 is returned. The wrong position will be returned if P is Point<ll> and the intersection point does not have integer coordinates. Products of three coordinates are used in intermediate steps so watch out for overflow if using int or long long.
 \end{minipage}
 \begin{minipage}{15mm}
 \includegraphics[width=\textwidth]{content/geometry/lineIntersection}
@@ -23,7 +23,7 @@ If a unique intersetion point of the lines going through s1,e1 and s2,e2 exists 
 template<class P>
 int lineIntersection(const P& s1, const P& e1, const P& s2,
 		const P& e2, P& r) {
-	if ((e1-s1).cross(e2-s2)) { //if not parallell
+	if ((e1-s1).cross(e2-s2)) { // if not parallel
 		r = s2-(e2-s2)*(e1-s1).cross(s2-s1)/(e1-s1).cross(e2-s2);
 		return 1;
 	} else
