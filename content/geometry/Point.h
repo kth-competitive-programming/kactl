2aad7c9396a212deb13f2a33205b80846514a6d7
Horace He <horacehe2007@yahoo.com>
2019-05-04 16:46:32 -0400

    Updated onSegment.h and both segment intersections (#78)

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -9,6 +9,7 @@
  */
 #pragma once
 
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
 template<class T>
 struct Point {
 	typedef Point P;
--- content/geometry/onSegment.h
+++ content/geometry/onSegment.h
@@ -1,17 +1,15 @@
 /**
- * Author: Ulf Lundstrom
- * Date: 2009-04-09
+ * Author: Victor Lecomte, chilli
+ * Date: 2019-04-26
  * License: CC0
- * Source: Basic geometry
- * Description: Returns true iff p lies on the line segment from s to e. Intended for use with e.g. Point<long long> where overflow is an issue. Use (segDist(s,e,p)<=epsilon) instead when using Point<double>.
+ * Source: https://vlecomte.github.io/cp-geo.pdf
+ * Description: Returns true iff p lies on the line segment from s to e. Intended for use with e.g. Point<long long> where overflow is an issue.  Use (segDist(s,e,p)<=epsilon) instead when using Point<double>.
  * Status:
  */
 #pragma once
 
 #include "Point.h"
 
-template<class P>
-bool onSegment(const P& s, const P& e, const P& p) {
-	P ds = p-s, de = p-e;
-	return ds.cross(de) == 0 && ds.dot(de) <= 0;
+template <class P> bool onSegment(P s, P e, P p) {
+	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
 }
