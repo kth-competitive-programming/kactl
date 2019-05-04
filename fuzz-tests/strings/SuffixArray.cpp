182725454a9747aee045772302fd60d7da5d7a21
Horace He <horacehe2007@yahoo.com>
2019-05-04 16:57:28 -0400

    Fixed some extraneous commits

--- fuzz-tests/strings/SuffixArray.cpp
+++ fuzz-tests/strings/SuffixArray.cpp
@@ -13,7 +13,7 @@ typedef vector<int> vi;
 #include "../../content/strings/SuffixArray.h"
 
 struct VecSuffixArray {
-    vi sa, lcp;
+	vi sa, lcp;
 	VecSuffixArray(vi &s, int lim = 256) {
 		int n = sz(s), k = 0;
 		vi x(2 * n), y(2 * n), wv(n), ws(max(n, lim)), rank(n);
@@ -72,7 +72,7 @@ void test(const string& s, int alpha) {
 
 	if (suffixes != sa.sa) {
 		cout << "sa fails for " << display(s) << ' ' << alpha << endl;
-		exit(1);
+		assert(suffixes == sa.sa);
 	}
 
 	rep(i,0,sz(s)) {
@@ -85,7 +85,7 @@ void test(const string& s, int alpha) {
 
 	if (lcp != sa.lcp) {
 		cout << "lcp fails for " << display(s) << ' ' << alpha << endl;
-		exit(1);
+		assert(lcp == sa.lcp);
 	}
 }
 
@@ -94,134 +94,131 @@ namespace old {
 typedef long long ll;
 typedef pair<ll, int> pli;
 void count_sort(vector<pli> &b, int bits) { // (optional)
-    // this is just 3 times faster than stl sort for N=10^6
-    int mask = (1 << bits) - 1;
-    for (int it = 0; it < 2; it++) {
-        int move = it * bits;
-        vector<int> q(1 << bits), w((q).size() + 1);
-        for (int i = 0; i < sz(b); i++)
-            q[(b[i].first >> move) & mask]++;
-        partial_sum(q.begin(), q.end(), w.begin() + 1);
-        vector<pli> res(b.size());
-        for (int i = 0; i < sz(b); i++)
-            res[w[(b[i].first >> move) & mask]++] = b[i];
-        swap(b, res);
-    }
+	// this is just 3 times faster than stl sort for N=10^6
+	int mask = (1 << bits) - 1;
+	for (int it = 0; it < 2; it++) {
+		int move = it * bits;
+		vector<int> q(1 << bits), w((q).size() + 1);
+		for (int i = 0; i < sz(b); i++)
+			q[(b[i].first >> move) & mask]++;
+		partial_sum(q.begin(), q.end(), w.begin() + 1);
+		vector<pli> res(b.size());
+		for (int i = 0; i < sz(b); i++)
+			res[w[(b[i].first >> move) & mask]++] = b[i];
+		swap(b, res);
+	}
 }
 struct SuffixArray {
-    vector<int> a;
-    string s;
-    SuffixArray(const string &_s) : s(_s + '\0') {
-        int N = sz(s);
-        vector<pli> b(N);
-        a.resize(N);
-        for (int i = 0; i < N; i++) {
-            b[i].first = s[i];
-            b[i].second = i;
-        }
+	vector<int> a;
+	string s;
+	SuffixArray(const string &_s) : s(_s + '\0') {
+		int N = sz(s);
+		vector<pli> b(N);
+		a.resize(N);
+		for (int i = 0; i < N; i++) {
+			b[i].first = s[i];
+			b[i].second = i;
+		}
 
-        int q = 8;
-        while ((1 << q) < N)
-            q++;
-        for (int moc = 0;; moc++) {
-            count_sort(b, q); // sort(all(b)) can be used as well
-            a[b[0].second] = 0;
-            for (int i = 1; i < N; i++)
-                a[b[i].second] = a[b[i - 1].second] + (b[i - 1].first != b[i].first);
+		int q = 8;
+		while ((1 << q) < N)
+			q++;
+		for (int moc = 0;; moc++) {
+			count_sort(b, q); // sort(all(b)) can be used as well
+			a[b[0].second] = 0;
+			for (int i = 1; i < N; i++)
+				a[b[i].second] = a[b[i - 1].second] + (b[i - 1].first != b[i].first);
 
-            if ((1 << moc) >= N)
-                break;
-            for (int i = 0; i < N; i++) {
-                b[i].first = (ll)a[i] << q;
-                if (i + (1 << moc) < N)
-                    b[i].first += a[i + (1 << moc)];
-                b[i].second = i;
-            }
-        }
-        for (int i = 0; i < sz(a); i++)
-            a[i] = b[i].second;
-    }
-    vector<int> lcp() {
-        // longest common prefixes: res[i] = lcp(a[i],
-        // a[i-1])
-        int n = sz(a), h = 0;
-        vector<int> inv(n), res(n);
-        for (int i = 0; i < n; i++)
-            inv[a[i]] = i;
-        for (int i = 0; i < n; i++)
-            if (inv[i] > 0) {
-                int p0 = a[inv[i] - 1];
-                while (s[i + h] == s[p0 + h])
-                    h++;
-                res[inv[i]] = h;
-                if (h > 0)
-                    h--;
-            }
-        return res;
-    }
+			if ((1 << moc) >= N)
+				break;
+			for (int i = 0; i < N; i++) {
+				b[i].first = (ll)a[i] << q;
+				if (i + (1 << moc) < N)
+					b[i].first += a[i + (1 << moc)];
+				b[i].second = i;
+			}
+		}
+		for (int i = 0; i < sz(a); i++)
+			a[i] = b[i].second;
+	}
+	vector<int> lcp() {
+		// longest common prefixes: res[i] = lcp(a[i],
+		// a[i-1])
+		int n = sz(a), h = 0;
+		vector<int> inv(n), res(n);
+		for (int i = 0; i < n; i++)
+			inv[a[i]] = i;
+		for (int i = 0; i < n; i++)
+			if (inv[i] > 0) {
+				int p0 = a[inv[i] - 1];
+				while (s[i + h] == s[p0 + h])
+					h++;
+				res[inv[i]] = h;
+				if (h > 0)
+					h--;
+			}
+		return res;
+	}
 };
 } // namespace kactl
 
 struct timeit {
-    decltype(chrono::high_resolution_clock::now()) begin;
-    const string label;
-    timeit(string label = "???") : label(label) { begin = chrono::high_resolution_clock::now(); }
-    ~timeit() {
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-        cerr << duration << "ms elapsed [" << label << "]" << endl;
-    }
+	decltype(chrono::high_resolution_clock::now()) begin;
+	const string label;
+	timeit(string label = "???") : label(label) { begin = chrono::high_resolution_clock::now(); }
+	~timeit() {
+		auto end = chrono::high_resolution_clock::now();
+		auto duration = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+		cerr << duration << "ms elapsed [" << label << "]" << endl;
+	}
 };
 
 signed compare() {
-    srand(0);
-    vi vS;
-    string S;
-    for (int iter = 0; iter < 5; iter++) {
+	srand(0);
+	vi vS;
+	string S;
+	for (int iter = 0; iter < 5; iter++) {
 
-        for (int i = 0; i < MAXN; i++) {
-            int t = rand() % 2;
-            vS.push_back(t + 1);
-            S.push_back((char)(t + 'a'));
-        }
+		for (int i = 0; i < MAXN; i++) {
+			int t = rand() % 2;
+			vS.push_back(t + 1);
+			S.push_back((char)(t + 'a'));
+		}
 
-        // cout << S << endl;
-        vector<array<int, 2>> res;
-        {
-            timeit x("kactl");
-            old::SuffixArray kactl(S);
-            // cout << kactl.a[100] << endl;
-            auto lcp = kactl.lcp();
+		// cout << S << endl;
+		vector<array<int, 2>> res;
+		{
+			timeit x("kactl");
+			old::SuffixArray kactl(S);
+			// cout << kactl.a[100] << endl;
+			auto lcp = kactl.lcp();
 			rep(i,0,sz(S)+1)
-                res.push_back({kactl.a[i], lcp[i]});
-        }
-        {
-            timeit x("MIT");
-            SuffixArray sa(S);
-            // cout << sa.sa[100] << endl;
+				res.push_back({kactl.a[i], lcp[i]});
+		}
+		{
+			timeit x("MIT");
+			SuffixArray sa(S);
+			// cout << sa.sa[100] << endl;
 			rep(i,0,sz(S)+1) {
-                assert((res[i] == array<int, 2>{sa.sa[i], sa.lcp[i]}));
-            }
-        }
-    }
+				assert((res[i] == array<int, 2>{sa.sa[i], sa.lcp[i]}));
+			}
+		}
+	}
 	return 0;
 }
 
 void fuzz(bool onlySmall = false) {
 	rep(large,0,2) {
 		if (onlySmall && large) break;
-		double work = large ? 1e8 : 5e5;
+		double work = large ? 1e7 : 5e5;
 		rep(alpha,1,27) {
 			rep(n,0,100) {
 				if (n * n * pow(alpha, n) > work) break;
-				cout << alpha << ' ' << n << ": " << flush;
+				// cout << alpha << ' ' << n << ": " << flush;
 				string s(n, 'x');
-				int cnt = 0;
 				gen(s, 0, alpha, [&]() {
 					test(s, alpha);
-					cnt++;
 				});
-				cout << cnt << endl;
 			}
 		}
 	}
@@ -255,6 +252,7 @@ void perf2() {
 int main() {
 	// compare();
 	fuzz(0);
+	cout<<"Tests passed!"<<endl;
 	// perf();
 	// perf2();
 }
