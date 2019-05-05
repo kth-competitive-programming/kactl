c92382758e36d9ff97803179e9194d9431ba9143
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-06 00:31:18 +0200

    Fix indent

--- content/geometry/insidePolygon.h
+++ content/geometry/insidePolygon.h
@@ -22,12 +22,12 @@
 
 template<class P>
 bool inPolygon(vector<P> &p, P a, bool strict = true) {
-		int cnt = 0, n = sz(p);
-		rep(i,0,n) {
-			P q = p[(i + 1) % n];
-			if (onSegment(p[i], q, a)) return !strict;
-			//or: if (segDist(p[i], q, a) <= eps) return !strict;
-			cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i],q) > 0;
-		}
-		return cnt;
+	int cnt = 0, n = sz(p);
+	rep(i,0,n) {
+		P q = p[(i + 1) % n];
+		if (onSegment(p[i], q, a)) return !strict;
+		//or: if (segDist(p[i], q, a) <= eps) return !strict;
+		cnt ^= ((a.y<p[i].y) - (a.y<q.y)) * a.cross(p[i], q) > 0;
+	}
+	return cnt;
 }
