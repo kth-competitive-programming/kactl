63425c3bcecfd5a15c537bcbced865542f8a6e81
Horace He <horacehe2007@yahoo.com>
2019-05-04 22:17:20 -0400

    Simplified/shortened the code

--- content/geometry/halfPlane.h
+++ content/geometry/halfPlane.h
@@ -16,35 +16,26 @@
 
 typedef Point<double> P;
 typedef array<P, 2> Line;
-#define v(a) (a[1] - a[0])
-#define c(a) (v(a).cross(a[0]))
-pair<int, P> lineInter(Line a, Line b) {
-	auto d = v(a).cross(v(b));
-	if (d == 0) return {-(c(a) == c(b)), P(0,0)};
-	else return {1, (v(b)*c(a) - v(a)*c(b))/d};
-}
+#define sp(a) a[0], a[1]
+#define ang(a) atan2((a[1] - a[0]).y, (a[1] - a[0]).x)
 
-#define ang(a) atan2(v(a).y, v(a).x)
-int sideOf(Line l, P p) {return sideOf(l[0], l[1], p);}
 vector<P> halfPlaneIntersection(vector<Line> vs) {
     sort(all(vs), [](auto a, auto b) { return ang(a) < ang(b);});
     vs.resize(unique(all(vs), [](auto a, auto b){ return ang(a) == ang(b);}) - vs.begin());
-    deque<Line> deq({vs[0]});
-    deque<P> ans;
+    vector<Line> deq(sz(vs)+5);
+    vector<P> ans(sz(vs)+5);
+    int dh = 0, dt = 1, ah=0, at=0;
+    deq[0] = vs[0];
     for (int i = 1; i < sz(vs); ++i) {
-        if (sgn(ang(vs[i]) - ang(vs[i-1])) == 0) continue;
-        while (sz(ans) && sideOf(vs[i], ans.back())< 0)
-            ans.pop_back(), deq.pop_back();
-        while(sz(ans) && sideOf(vs[i], ans.front()) < 0)
-            ans.pop_front(), deq.pop_front();
-        ans.push_back(lineInter(deq.back(), vs[i]).second);
-        deq.push_back(vs[i]);
+        if (ang(vs[i]) - ang(vs[i-1]) == 0) continue;
+        while (ah < at && sideOf(sp(vs[i]), ans[at-1])< 0) at--, dt--;
+        while (ah < at && sideOf(sp(vs[i]), ans[ah]) < 0) ah++, dh++;
+        ans[at++] = lineInter(sp(deq[dt-1]), sp(vs[i])).second;
+        deq[dt++] = vs[i];
     }
-    while (sz(ans) && sideOf(deq.front(), ans.back()) < 0)
-        ans.pop_back(), deq.pop_back();
-    while (sz(ans) && sideOf(deq.back(), ans.front()) < 0)
-        ans.pop_front(), deq.pop_front();
-    if (sz(deq) <= 2) return {};
-    ans.push_back(lineInter(deq.front(), deq.back()).second);
-    return vector<P>(all(ans));
+    while (ah < at && sideOf(sp(deq[dh]), ans[at-1]) < 0) at--, dt--;
+    while (ah < at && sideOf(sp(deq[dt]), ans[ah]) < 0) ah++, dh++;
+    if (dt-dh <= 2) return {};
+    ans[at++] = lineInter(sp(deq[dh]), sp(deq[dt-1])).second;
+    return {ans.begin()+ah, ans.begin()+at};
 }
