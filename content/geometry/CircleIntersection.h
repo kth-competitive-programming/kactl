b6ddd4e17db154af9afb96c5f7ea6d841ad75720
Horace He <horacehe2007@yahoo.com>
2019-05-05 02:43:30 -0400

    Updated CircleIntersection.h

--- content/geometry/CircleIntersection.h
+++ content/geometry/CircleIntersection.h
@@ -1,8 +1,8 @@
 /**
- * Author: Simon Lindholm
+ * Author: Simon Lindholm, Victor Lecomte, chilli
  * Date: 2015-09-01
  * License: CC0
- * Description: Computes a pair of points at which two circles intersect. Returns false in case of no intersection.
+ * Description: Computes a pair of points at which two circles intersect. Returns number of intersections. If intersection is only at one point then that point is returned twice.
  * Status: somewhat tested
  */
 #pragma once
@@ -10,16 +10,13 @@
 #include "Point.h"
 
 typedef Point<double> P;
-bool circleIntersection(P a, P b, double r1, double r2,
-		pair<P, P>* out) {
-	P delta = b - a;
-	assert(delta.x || delta.y || r1 != r2);
-	if (!delta.x && !delta.y) return false;
-	double r = r1 + r2, d2 = delta.dist2();
+int circleInter(P a,P b,double r1,double r2,pair<P, P>& out){
+	P delta = b - a; double d2 = delta.dist2();
+	if (d2 == 0) { assert(r1 != r2); return 0; }
 	double p = (d2 + r1*r1 - r2*r2) / (2.0 * d2);
 	double h2 = r1*r1 - p*p*d2;
-	if (d2 > r*r || h2 < 0) return false;
+	if (h2 < 0) return 0;
 	P mid = a + delta*p, per = delta.perp() * sqrt(h2 / d2);
-	*out = {mid + per, mid - per};
-	return true;
+	out = {mid + per, mid - per};
+	return 1 + sgn(h2);
 }
