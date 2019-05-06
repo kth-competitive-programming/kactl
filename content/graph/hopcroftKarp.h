eb37dce182e0ef4694d3c7cf8b2f9b353513bf7b
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-06 16:02:44 +0200

    Tiny bit shorter hopcroftKarp

--- content/graph/hopcroftKarp.h
+++ content/graph/hopcroftKarp.h
@@ -18,8 +18,8 @@ bool dfs(int a, int L, vector<vi>& g, vi& btoa, vi& A, vi& B) {
 	if (A[a] != L) return 0;
 	A[a] = -1;
 	trav(b, g[a]) if (B[b] == L + 1) {
-		B[b] = -1;
-		if (btoa[b] == -1 || dfs(btoa[b], L+2, g, btoa, A, B))
+		B[b] = 0;
+		if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B))
 			return btoa[b] = a, 1;
 	}
 	return 0;
@@ -30,13 +30,13 @@ int hopcroftKarp(vector<vi>& g, vi& btoa) {
 	vi A(g.size()), B(btoa.size()), cur, next;
 	for (;;) {
 		fill(all(A), 0);
-		fill(all(B), -1);
+		fill(all(B), 0);
 		/// Find the starting nodes for BFS (i.e. layer 0).
 		cur.clear();
 		trav(a, btoa) if(a != -1) A[a] = -1;
 		rep(a,0,sz(g)) if(A[a] == 0) cur.push_back(a);
 		/// Find all layers using bfs.
-		for (int lay = 1;; lay += 2) {
+		for (int lay = 1;; lay++) {
 			bool islast = 0;
 			next.clear();
 			trav(a, cur) trav(b, g[a]) {
@@ -44,20 +44,18 @@ int hopcroftKarp(vector<vi>& g, vi& btoa) {
 					B[b] = lay;
 					islast = 1;
 				}
-				else if (btoa[b] != a && B[b] == -1) {
+				else if (btoa[b] != a && !B[b]) {
 					B[b] = lay;
 					next.push_back(btoa[b]);
 				}
 			}
 			if (islast) break;
 			if (next.empty()) return res;
-			trav(a, next) A[a] = lay+1;
+			trav(a, next) A[a] = lay;
 			cur.swap(next);
 		}
 		/// Use DFS to scan for augmenting paths.
-		rep(a,0,sz(g)) {
-			if(dfs(a, 0, g, btoa, A, B))
-				++res;
-		}
+		rep(a,0,sz(g))
+			res += dfs(a, 0, g, btoa, A, B);
 	}
 }
