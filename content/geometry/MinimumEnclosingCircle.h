716a12d4fe327fc916bc8415fc10a2f533ba5a50
Horace He <horacehe2007@yahoo.com>
2019-05-07 13:01:13 -0400

    Converted to spaces

--- content/geometry/MinimumEnclosingCircle.h
+++ content/geometry/MinimumEnclosingCircle.h
@@ -2,7 +2,7 @@
  * Author: Andrew He, chilli
  * Date: 2019-05-07
  * License: CC0
- * Source: NAPC 2017 solution presentation
+ * Source: folklore
  * Description: Computes the minimum circle that encloses a set of points.
  * Time: expected O(n)
  * Status: fuzz-tested
@@ -12,19 +12,19 @@
 #include "circumcircle.h"
 
 pair<double, P> mec(vector<P> ps) {
-    shuffle(all(ps), mt19937(time(0)));
-    P o = ps[0];
-    double r = 0, EPS = (1 + 1e-8);
-    rep(i, 1, sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
-        o = ps[i], r = 0;
-        rep(j, 0, i) if ((o - ps[j]).dist() > r * EPS) {
-            o = (ps[i] + ps[j]) / 2;
-            r = (o - ps[i]).dist();
-            rep(k, 0, j) if ((o - ps[k]).dist() > r * EPS) {
-                o = ccCenter(ps[i], ps[j], ps[k]);
-                r = (o - ps[i]).dist();
-            }
-        }
-    }
+	shuffle(all(ps), mt19937(time(0)));
+	P o = ps[0];
+	double r = 0, EPS = (1 + 1e-8);
+	rep(i, 1, sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
+		o = ps[i], r = 0;
+		rep(j, 0, i) if ((o - ps[j]).dist() > r * EPS) {
+			o = (ps[i] + ps[j]) / 2;
+			r = (o - ps[i]).dist();
+			rep(k, 0, j) if ((o - ps[k]).dist() > r * EPS) {
+				o = ccCenter(ps[i], ps[j], ps[k]);
+				r = (o - ps[i]).dist();
+			}
+		}
+	}
 	return {r, o};
 }
