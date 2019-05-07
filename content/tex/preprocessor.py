f388eb2a8372006e7a4bc99b9fac1c094dbdbef9
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-05-07 15:38:42 +0200

    Always look for matching end comment

--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -53,20 +53,20 @@ def addref(caption, outstream):
     with open('header.tmp', 'a') as f:
         f.write(caption + "\n")
 
-def find_helper(source, start, subs):
-    first = (-1, -1)
-    for s in subs:
-        i = source.find(s, start)
-        if i != -1 and (i < first[0] or first[0] == -1):
-            first = (i, i + len(s))
-
-    return first
+COMMENT_TYPES = [
+    ('/**', '*/'),
+    ("'''", "'''"),
+    ('"""', '"""'),
+]
 
 def find_start_comment(source, start=None):
-    return find_helper(source, start, ['/**', "'''"])
+    first = (-1, -1, None)
+    for s, e in COMMENT_TYPES:
+        i = source.find(s, start)
+        if i != -1 and (i < first[0] or first[0] == -1):
+            first = (i, i + len(s), e)
 
-def find_end_comment(source, start=None):
-    return find_helper(source, start, ['*/', "'''"])
+    return first
 
 def processwithcomments(caption, instream, outstream, listingslang = None):
     knowncommands = ['Author', 'Date', 'Description', 'Source', 'Time', 'Memory', 'License', 'Status', 'Usage']
@@ -99,21 +99,21 @@ def processwithcomments(caption, instream, outstream, listingslang = None):
             includelist.append(include)
             continue
         nlines.append(line)
-    # Remove and process /** */ comments
+    # Remove and process multiline comments
     source = '\n'.join(nlines)
     nsource = ''
-    start, start2 = find_start_comment(source)
+    start, start2, end_str = find_start_comment(source)
     end = 0
     commands = {}
     while start >= 0 and not error:
         nsource = nsource.rstrip() + source[end:start]
-        end, end2 = find_end_comment(source, start2)
+        end = source.find(end_str, start2)
         if end<start:
-            error = "Invalid /** */ comments."
+            error = "Invalid %s %s comments." % (source[start:start2], end_str)
             break
         comment = source[start2:end].strip()
-        end = end2
-        start, start2 = find_start_comment(source, end)
+        end += len(end_str)
+        start, start2, end_str = find_start_comment(source, end)
 
         commentlines = comment.split('\n')
         command = None
