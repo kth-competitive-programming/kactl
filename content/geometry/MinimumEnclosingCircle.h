ecb8abc8e72253835ac174a0c1119bc2d4838f6c
Horace He <horacehe2007@yahoo.com>
2019-05-07 13:08:03 -0400

    Responded to review

--- content/geometry/MinimumEnclosingCircle.h
+++ content/geometry/MinimumEnclosingCircle.h
@@ -14,13 +14,13 @@
 pair<P, double> mec(vector<P> ps) {
 	shuffle(all(ps), mt19937(time(0)));
 	P o = ps[0];
-	double r = 0, EPS = (1 + 1e-8);
-	rep(i, 1, sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
+	double r = 0, EPS = 1 + 1e-8;
+	rep(i,0,sz(ps)) if ((o - ps[i]).dist() > r * EPS) {
 		o = ps[i], r = 0;
-		rep(j, 0, i) if ((o - ps[j]).dist() > r * EPS) {
+		rep(j,0,i) if ((o - ps[j]).dist() > r * EPS) {
 			o = (ps[i] + ps[j]) / 2;
 			r = (o - ps[i]).dist();
-			rep(k, 0, j) if ((o - ps[k]).dist() > r * EPS) {
+			rep(k,0,j) if ((o - ps[k]).dist() > r * EPS) {
 				o = ccCenter(ps[i], ps[j], ps[k]);
 				r = (o - ps[i]).dist();
 			}
