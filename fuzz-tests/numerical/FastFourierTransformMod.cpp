6e161a993098be429835027530606b7db2305235
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-08 02:45:39 +0200

    Naive FFTMod fuzz-test

---
+++ fuzz-tests/numerical/FastFourierTransformMod.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < int(b); ++i)
+#define trav(a, v) for(auto& a : v)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const ll mod = 1000000007;
+
+#include "../../content/numerical/FastFourierTransformMod.h"
+
+vl simpleConv(vl a, vl b) {
+	int s = sz(a) + sz(b) - 1;
+	if (a.empty() || b.empty()) return {};
+	vl c(s);
+	rep(i,0,sz(a)) rep(j,0,sz(b))
+		c[i+j] = (c[i+j] + (ll)a[i] * b[j]) % mod;
+	trav(x, c) if (x < 0) x += mod;
+	return c;
+}
+
+int ra() {
+	static unsigned X;
+	X *= 123671231;
+	X += 1238713;
+	X ^= 1237618;
+	return (X >> 1);
+}
+
+int main() {
+	vl a, b;
+	rep(it,0,6000) {
+		a.resize(ra() % 10);
+		b.resize(ra() % 10);
+		trav(x, a) x = (ra() % 100 - 50+mod)%mod;
+		trav(x, b) x = (ra() % 100 - 50+mod)%mod;
+		auto v1 = simpleConv(a, b);
+		auto v2 = convMod<mod>(a, b);
+		assert(v1 == v2);
+	}
+}
