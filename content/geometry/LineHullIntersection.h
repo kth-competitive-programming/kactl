77acf17ca8a6e27554411a662fa7c00790d72476
Horace He <horacehe2007@yahoo.com>
2019-05-08 23:05:51 -0400

    Removed unnecessary empty line

--- content/geometry/LineHullIntersection.h
+++ content/geometry/LineHullIntersection.h
@@ -23,44 +23,52 @@
 #include "Point.h"
 
 typedef array<P, 2> Line;
-#define isExtr(i) cmp(i+1, i) >= 0 && cmp(i, i-1) < 0
+#define isExtr(i) cmp(i + 1, i) >= 0 && cmp(i, i - 1) < 0
 int extrVertex(vector<P> poly, P dir) {
 	int n = sz(poly), l = 0, r = n;
-	auto cmp = [&](int i, int j) {
-		return sgn(dir.perp().cross(poly[(i+n)%n]-poly[(j+n)%n]));
-	};
-	if (isExtr(0)) return 0;
+	auto cmp = [&](int i, int j) { return sgn(dir.perp().cross(poly[(i + n) % n] - poly[(j + n) % n])); };
+	if (isExtr(0))
+		return 0;
 	while (l + 1 < r) {
 		int m = (l + r) / 2;
-		if (isExtr(m)) return m;
+		if (isExtr(m))
+			return m;
 		int lS = cmp(l + 1, l), mS = cmp(m + 1, m);
-		if (lS != mS ? lS < mS : lS == cmp(l, m)) r = m;
-		else l = m;
+		if (lS != mS ? lS < mS : lS == cmp(l, m))
+			r = m;
+		else
+			l = m;
 	}
 	return l;
-
 }
+
 #define cmpL(i) sgn(line[0].cross(poly[i], line[1]))
 array<int, 2> lineHull(Line line, vector<P> poly) {
 	int endA = extrVertex(poly, (line[0] - line[1]).perp());
 	int endB = extrVertex(poly, (line[1] - line[0]).perp());
-	if (cmpL(endA) < 0 || cmpL(endB) > 0) return {-1, -1};
+	if (cmpL(endA) < 0 || cmpL(endB) > 0)
+		return {-1, -1};
 	array<int, 2> res;
-	rep(i,0,2){
+	rep(i, 0, 2) {
 		int l = endB, r = endA, n = sz(poly);
 		while ((l + 1) % n != r) {
-			int m = ((l + r + (l < r ? 0 : n)) / 2)%n;
-			if (cmpL(m) == cmpL(endB)) l = m;
-			else r = m;
+			int m = ((l + r + (l < r ? 0 : n)) / 2) % n;
+			if (cmpL(m) == cmpL(endB))
+				l = m;
+			else
+				r = m;
 		}
 		res[i] = (l + (cmpL(r) == 0)) % n;
 		swap(endA, endB);
 	}
-	if (res[0] == res[1]) res = {res[0], -1};
-	else if (cmpL(res[0]) == 0 && cmpL(res[1]) == 0){
-		int diff = (res[0] - res[1] + sz(poly) + 1)%sz(poly);
-		if (diff == 0) res = {res[0], res[0]};
-		else if (diff == 2) res = {res[1], res[1]};
+	if (res[0] == res[1])
+		res = {res[0], -1};
+	else if (cmpL(res[0]) == 0 && cmpL(res[1]) == 0) {
+		int diff = (res[0] - res[1] + sz(poly) + 1) % sz(poly);
+		if (diff == 0)
+			res = {res[0], res[0]};
+		else if (diff == 2)
+			res = {res[1], res[1]};
 	}
 	return res;
 }
