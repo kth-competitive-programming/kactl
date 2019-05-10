bad1d58aea4c0a57295507236d85dc84c8056ab2
Horace He <horacehe2007@yahoo.com>
2019-05-10 13:29:02 -0400

    Updated fuzz tests

--- fuzz-tests/graph/MaximumClique.cpp
+++ fuzz-tests/graph/MaximumClique.cpp
@@ -11,19 +11,9 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 #include "../../content/graph/MaximumClique.h"
-
-template<class F>
-void cliques(vector<B>& eds, F f, B P = ~B(), B X={}, B R={}) {
-	if (!P.any()) { if (!X.any()) f(R); return; }
-	auto q = (P | X)._Find_first();
-	auto cands = P & ~eds[q];
-	rep(i,0,sz(eds)) if (cands[i]) {
-		R[i] = 1;
-		cliques(eds, f, P & eds[i], X & eds[i], R);
-		R[i] = P[i] = 0; X[i] = 1;
-	}
+namespace maximal {
+#include "../../content/graph/MaximalCliques.h"
 }
-
 
 struct timeit {
 	decltype(chrono::high_resolution_clock::now()) begin;
@@ -41,15 +31,19 @@ int main() {
     rep(it, 0, 100000) {
         int n =(rand()%32)+1;
         vector<B> ed(n);
+        vector<maximal::B> ed2(n);
         int p =rand()%100;
         rep(i, 0, n) rep(j, 0, i) {
             ed[i][j] = (rand() % 100) < p;
             ed[j][i] = ed[i][j];
+            ed2[i][j] = ed[i][j];
+            ed2[j][i] = ed[j][i];
         }
         auto clique2 = new Maxclique(ed);
         int mx = 0;
-        cliques(ed, [&](auto x){mx = max(mx, int(x.count()));});
+        maximal::cliques(ed2, [&](auto x){mx = max(mx, int(x.count()));});
         assert(mx == sz(clique2->maxClique()));
     }
+    cout<<"Tests passed!"<<endl;
 }
 
