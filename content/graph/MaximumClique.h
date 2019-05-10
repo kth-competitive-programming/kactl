cca0fcbd0ec4fce971f4b9fbd397987efe3ba25d
Horace He <horacehe2007@yahoo.com>
2019-05-10 10:43:41 -0400

    Responded to review

--- content/graph/MaximumClique.h
+++ content/graph/MaximumClique.h
@@ -1,5 +1,5 @@
 /**
- * Author: chilli, Haobin Ni
+ * Author: chilli, SJTU
  * Date: 2019-05-10
  * License: CC0
  * Source: https://en.wikipedia.org/wiki/MaxCliqueDyn_maximum_clique_algorithm,
@@ -12,29 +12,29 @@
  */
 struct Maxclique {
 	double limit=0.025, pk=0;
-	struct Vertex { int i, d; Vertex(int i) : i(i), d(0) {} };
+	struct Vertex { int i, d=0; };
 	typedef vector<Vertex> vv;
 	vector<B> e;
 	vv V;
 	vector<vi> C;
 	vi qmax, q, S, So;
-	bool cut1(const int pi, const vi &A) {
+	bool cut1(int pi, vi& A) {
 		trav(i, A) if (e[pi][i]) return true;
 		return false;
 	}
-	void init(vv &r) {
+	void init(vv& r) {
 		trav(v,r) v.d = 0;
 		trav(v, r) trav(j, r) v.d += e[v.i][j.i];
 		sort(all(r), [](auto a, auto b) { return a.d > b.d; });
 	}
-	void expand(vv &R, int lev = 1) {
+	void expand(vv& R, int lev = 1) {
 		S[lev] += S[lev - 1] - So[lev];
 		So[lev] = S[lev - 1];
 		while (sz(R)) {
 			if (sz(q) + R.back().d <= sz(qmax)) return;
 			q.push_back(R.back().i);
 			vv Rp;
-			trav(v,R) if (e[R.back().i][v.i]) Rp.push_back(v.i);
+			trav(v,R) if (e[R.back().i][v.i]) Rp.push_back({v.i});
 			if (sz(Rp)) {
 				if (S[lev]++ / ++pk < limit) init(Rp);
 				int j = 0, mxk = 1, mnk = max(sz(qmax) - sz(q) + 1, 1);
@@ -56,7 +56,7 @@ struct Maxclique {
 	}
 	vi maxClique() { init(V), expand(V); return qmax; }
 	Maxclique(vector<B> conn) :
-        e(conn), S(sz(e)), So(S), C(sz(e) + 1) {
-		rep(i,0,sz(e)) V.push_back(Vertex(i));
+        e(conn), C(sz(e) + 1), S(sz(e)), So(sz(e))  {
+		rep(i,0,sz(e)) V.push_back({i});
 	}
 };
