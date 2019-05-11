435f382ea4368df641774d44bd4c5c7579f2d680
Horace He <horacehe2007@yahoo.com>
2019-05-11 03:53:43 -0400

    Improved modinverse test

--- fuzz-tests/number-theory/ModInverse.cpp
+++ fuzz-tests/number-theory/ModInverse.cpp
@@ -14,11 +14,22 @@ ll modpow(ll a, ll e, ll mod) {
 	ll x = modpow(a * a % mod, e >> 1, mod);
 	return e & 1 ? x * a % mod : x;
 }
-
+bool isPrime(int x) {
+	if (x <= 1) return false;
+	for (int i = 2; i*i <= x; ++i) {
+		if (x % i == 0) return false;
+	}
+	return true;
+}
 int main() {
-	#include "../../content/number-theory/ModInverse.h"
-	for (int i=1; i<10000; i++)
-		assert(inv[i] == modpow(i, mod-2, mod));
+	rep(it,1,1000) {
+		int mod = it, LIM=1000;
+		if (!isPrime(mod)) continue;
+		#include "../../content/number-theory/ModInverse.h"
+		for (int i=1; i<it; i++){
+			assert(inv[i] == modpow(i, mod-2, mod));
+		}
+	}
 	cout<<"Tests pass!"<<endl;
 }
 
