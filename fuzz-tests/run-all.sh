f6c35121129ac24dfa9d5f38c99eabfd36c80f57
Horace He <horacehe2007@yahoo.com>
2019-05-15 06:35:39 -0400

    Responded to review comments

--- fuzz-tests/run-all.sh
+++ fuzz-tests/run-all.sh
@@ -1,4 +1,4 @@
-DIR=${1:-fuzz-tests}
+DIR=${1:-.}
 tests="$(find $DIR -name '*.cpp')"
 declare -i pass=0
 declare -i fail=0
@@ -6,19 +6,17 @@ failTests=""
 echo -e $failTests
 for test in $tests; do
     echo "$(basename $test): "
-    g++ -std=c++14 -O2 $test
-    ./a.out
+    g++ -std=c++14 -O2 $test && ./a.out
     retCode=$?
     if (($retCode != 0)); then
         echo $retCode
         fail+=1
         failTests="$failTests$test\n"
-        echo -e $failTests
     else
         pass+=1
     fi
-    rm a.out
-    echo ""
+    rm -f a.out
+    echo
 done
 echo "$pass/$(($pass+$fail)) tests passed"
 if (($fail == 0)); then
