f666959faa5348e42bd61644657a5c3011a39ad4
Horace He <horacehe2007@yahoo.com>
2019-05-17 21:06:58 -0400

    Added MaximumIndependentSet and made some minor tweaks to descriptions

---
+++ content/graph/MaximumIndepentSet.h
@@ -0,0 +1,8 @@
+/**
+ * Author: chilli
+ * Date: 2019-05-17
+ * Source: Wikipedia
+ * Description: To obtain the maximum independent set of a graph, find the max
+ * clique of the inverse graph. If the graph is bipartite, see
+ * MinimumVertexCover.h
+ */
