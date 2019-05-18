96ffcfe0c660c6aa122e1db31c350a985f24922d
Horace He <horacehe2007@yahoo.com>
2019-05-17 21:08:05 -0400

    Added to doc

---
+++ content/graph/MaximumIndependentSet.h
@@ -0,0 +1,9 @@
+/**
+ * Author: chilli
+ * Date: 2019-05-17
+ * Source: Wikipedia
+ * Description: To obtain the maximum independent set of a graph, find the max
+ * clique of the inverse graph. Each node in the clique is an element of the
+ * max independent set of the original graph. If the graph is bipartite, see
+ * MinimumVertexCover.h.
+ */
--- runtests.sh
+++
@@ -1,8 +0,0 @@
-tests="$(find fuzz-tests -name '*.cpp')"
-for test in $tests; do
-    basename $test
-    g++ -std=c++14 $test
-    ./a.out
-    echo $?
-    rm a.out
-done
