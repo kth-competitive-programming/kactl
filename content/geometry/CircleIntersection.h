71fa0ad2c51c33a72e29899dbf3a83f31f944cdc
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-20 23:25:54 +0200

    Update

--- content/geometry/CircleIntersection.h
+++ content/geometry/CircleIntersection.h
@@ -1,9 +1,8 @@
 /**
- * Author: Simon Lindholm, Victor Lecomte, chilli
+ * Author: Simon Lindholm
  * Date: 2015-09-01
  * License: CC0
- * Source: https://vlecomte.github.io/cp-geo.pdf
- * Description: Computes a pair of points at which two circles intersect. Returns false in case of no intersection.
+ * Description: Computes the pair of points at which two circles intersect. Returns false in case of no intersection.
  * Status: somewhat tested
  */
 #pragma once
@@ -11,13 +10,13 @@
 #include "Point.h"
 
 typedef Point<double> P;
-int circleInter(P a,P b,double r1,double r2,pair<P, P>* out){
+bool circleInter(P a,P b,double r1,double r2,pair<P, P>* out) {
 	if (a == b) { assert(r1 != r2); return false; }
-	P delta = b - a;
-	double d2 = delta.dist2(), p = (d2 + r1*r1 - r2*r2)/(d2*2);
-	double h2 = r1*r1 - p*p*d2;
-	if (h2 < 0) return false;
-	P mid = a + delta*p, per = delta.perp() * sqrt(h2 / d2);
+	P vec = b - a;
+	double d2 = vec.dist2(), sum = r1+r2, dif = r1-r2,
+	       p = (d2 + r1*r1 - r2*r2)/(d2*2), h2 = r1*r1 - p*p*d2;
+	if (sum*sum < d2 || dif*dif > d2) return false;
+	P mid = a + vec*p, per = vec.perp() * sqrt(fmax(0, h2) / d2);
 	*out = {mid + per, mid - per};
 	return true;
 }
