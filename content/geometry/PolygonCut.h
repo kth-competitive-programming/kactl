201c51cd4a68dbd316e98c552cadae7d581fc0ba
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-20 23:58:19 +0200

    Update PolygonCut after line intersection changes

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -28,10 +28,8 @@ vector<P> polygonCut(const vector<P>& poly, P s, P e) {
 	rep(i,0,sz(poly)) {
 		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
 		bool side = s.cross(e, cur) < 0;
-		if (side != (s.cross(e, prev) < 0)) {
-			res.emplace_back();
-			lineIntersection(s, e, cur, prev, res.back());
-		}
+		if (side != (s.cross(e, prev) < 0))
+			res.push_back(lineInter(s, e, cur, prev).second);
 		if (side)
 			res.push_back(cur);
 	}
