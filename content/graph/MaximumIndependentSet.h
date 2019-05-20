eb8bb57f44a5e2e10e45367b416815f3aa2c7e09
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-20 18:03:56 +0200

    Description tweaks

--- content/graph/MaximumIndependentSet.h
+++ content/graph/MaximumIndependentSet.h
@@ -2,8 +2,6 @@
  * Author: chilli
  * Date: 2019-05-17
  * Source: Wikipedia
- * Description: To obtain the maximum independent set of a graph, find the max
- * clique of the inverse graph. Each node in the clique is an element of the
- * max independent set of the original graph. If the graph is bipartite, see
- * MinimumVertexCover.h.
+ * Description: To obtain a maximum independent set of a graph, find a max
+ * clique of the complement. If the graph is bipartite, see MinimumVertexCover.
  */
