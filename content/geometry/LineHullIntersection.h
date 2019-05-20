c57173135efd17ed1b3b57653c0320192ac1c4ab
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-20 17:43:48 +0200

    Description formatting

--- content/geometry/LineHullIntersection.h
+++ content/geometry/LineHullIntersection.h
@@ -3,9 +3,8 @@
  * Date: 2019-05-07
  * License: Boost Software License
  * Source: https://github.com/AlCash07/ACTL/blob/master/include/actl/geometry/algorithm/intersect/line_convex_polygon.hpp
- * Description: Line-convex polygon intersection. The polygon must be ccw and
- * have no colinear points. lineHull(line, poly) returns a pair describing the
- * intersection of a line with the polygon
+ * Description: Line-convex polygon intersection. The polygon must be ccw and have no colinear points.
+ * lineHull(line, poly) returns a pair describing the intersection of a line with the polygon:
  *  \begin{itemize*}
  *    \item $(-1, -1)$ if no collision,
  *    \item $(i, -1)$ if touching the corner $i$,
