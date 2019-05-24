5115bea65669f26d0277f8d0d35c0b02e20d06e4
Simon Lindholm <simon.lindholm10@gmail.com>
2019-05-24 12:17:32 +0200

    Better toposort API

--- content/graph/TopoSort.h
+++ content/graph/TopoSort.h
@@ -3,28 +3,24 @@
  * Date: 2002-09-13
  * Source: predates tinyKACTL
  * Description: Topological sorting. Given is an oriented graph.
- * Output is an ordering of vertices (array idx), such that there are edges
- * only from left to right. The function returns false if there is a cycle in
- * the graph.
+ * Output is an ordering of vertices, such that there are edges only from left to right.
+ * If there are cycles, the returned list will have size smaller than $n$ -- nodes reachable
+ * from cycles will not be returned.
  * Time: $O(|V|+|E|)$
  */
 #pragma once
 
-template<class E, class I>
-bool topo_sort(const E &edges, I &idx, int n) {
-	vi indeg(n);
-	rep(i,0,n)
-		trav(e, edges[i])
-			indeg[e]++;
+vi topo_sort(const vector<vi>& gr) {
+	vi indeg(sz(gr)), ret;
+	trav(li, gr) trav(x, li) indeg[x]++;
 	queue<int> q; // use priority queue for lexic. smallest ans.
-	rep(i,0,n) if (indeg[i] == 0) q.push(-i);
-	int nr = 0;
-	while (q.size() > 0) {
+	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push(-i);
+	while (!q.empty()) {
 		int i = -q.front(); // top() for priority queue
-		idx[i] = nr++;
+		ret.push_back(i);
 		q.pop();
-		trav(e, edges[i])
-			if (--indeg[e] == 0) q.push(-e);
+		trav(x, gr[i])
+			if (--indeg[x] == 0) q.push(-x);
 	}
-	return nr == n;
+	return ret;
 }
