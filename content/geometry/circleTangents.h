7a7bfd40366f805f5f6fad73fe4ab8db6583b532
Horace He <horacehe2007@yahoo.com>
2019-10-31 22:47:18 -0400

    fix formatting

--- content/geometry/circleTangents.h
+++ content/geometry/circleTangents.h
@@ -19,14 +19,14 @@
 
 template<class P>
 vector<pair<P, P>> tangents(P c1, double r1, P c2, double r2) {
-    P d = c2 - c1;
-    double dr = r1 - r2, d2 = d.dist2(), h2 = d2 - dr * dr;
-    if (d2 == 0 || h2 < 0)  return {};
-    vector<pair<P, P>> out;
-    for (double sign : {-1, 1}) {
-        P v = (d * dr + d.perp() * sqrt(h2) * sign) / d2;
-        out.push_back({c1 + v * r1, c2 + v * r2});
-    }
-    if (h2 == 0) out.pop_back();
-    return out;
+	P d = c2 - c1;
+	double dr = r1 - r2, d2 = d.dist2(), h2 = d2 - dr * dr;
+	if (d2 == 0 || h2 < 0)  return {};
+	vector<pair<P, P>> out;
+	for (double sign : {-1, 1}) {
+		P v = (d * dr + d.perp() * sqrt(h2) * sign) / d2;
+		out.push_back({c1 + v * r1, c2 + v * r2});
+	}
+	if (h2 == 0) out.pop_back();
+	return out;
 }
