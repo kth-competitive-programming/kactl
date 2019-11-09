14d68797c5470ae4b1986d34640e879aedf81d62
Simon Lindholm <simon.lindholm10@gmail.com>
2019-11-09 01:07:42 +0100

    Swap order of lines

--- content/geometry/CircleTangents.h
+++ content/geometry/CircleTangents.h
@@ -6,8 +6,8 @@
  * Description: Finds the external tangents of two circles, or internal if r2 is negated.
  * Can return 0, 1, or 2 tangents -- 0 if one circle contains the other (or overlaps it, in the internal case);
  * 1 if the circles are tangent to each other (in which case .first = .second and the tangent line is perpendicular to the line between the centers).
+ * .first and .second give the tangency points at circle 1 and 2 respectively.
  * To find the tangents of a circle with a point c2 set r2 to 0.
- * .first and .second give the tangency points at circle 1 and 2 respectively. 
  * Status: tested
  */
 #pragma once
