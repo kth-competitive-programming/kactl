d13a4956a06d35eabac391d7b16cb4d4e2d246e4
Simon Lindholm <simon.lindholm10@gmail.com>
2019-11-09 01:43:07 +0100

    Note identical circles as well
    Not sure this is worthwhile to note honestly, but, eh.

--- content/geometry/CircleTangents.h
+++ content/geometry/CircleTangents.h
@@ -4,7 +4,7 @@
  * License: CC0
  * Source: https://vlecomte.github.io/cp-geo.pdf
  * Description: Finds the external tangents of two circles, or internal if r2 is negated.
- * Can return 0, 1, or 2 tangents -- 0 if one circle contains the other (or overlaps it, in the internal case);
+ * Can return 0, 1, or 2 tangents -- 0 if one circle contains the other (or overlaps it, in the internal case, or if the circles are the same);
  * 1 if the circles are tangent to each other (in which case .first = .second and the tangent line is perpendicular to the line between the centers).
  * .first and .second give the tangency points at circle 1 and 2 respectively.
  * To find the tangents of a circle with a point set r2 to 0.
