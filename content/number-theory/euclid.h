3a619b49aff8dc187569f9e0ab0e49acf62c84cc
Horace He <horacehe2007@yahoo.com>
2019-11-12 19:26:04 -0500

    Update content/number-theory/euclid.h
    Co-Authored-By: Simon Lindholm <simon.lindholm10@gmail.com>

--- content/number-theory/euclid.h
+++ content/number-theory/euclid.h
@@ -3,7 +3,7 @@
  * Date: 2002-09-15
  * Source: predates tinyKACTL
  * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
- * you just need gcd, use the built in __gcd instead
+ * you just need gcd, use the built in \texttt{\_\_gcd} instead.
  * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
  */
 #pragma once
