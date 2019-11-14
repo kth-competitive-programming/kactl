4cc8bf525f105e154ec7007726ba4060390878fe
Simon Lindholm <simon.lindholm10@gmail.com>
2019-11-14 15:16:55 +0100

    Shorter

--- content/number-theory/ModLog.h
+++ content/number-theory/ModLog.h
@@ -3,20 +3,19 @@
  * Date: 2019-11-08
  * License: Unlicense
  * Source: https://github.com/indy256/codelibrary/blob/master/cpp/numbertheory/discrete_log.cpp
- * Description: Calculates the discrete log. Returns an x s.t. a^x = b mod m. a and m must be coprime.
- * Time: $O(m^{1/2})$
- * Status: tested for all values under a 1000 for a,x, and m.
+ * Description: Returns the smallest $x \ge 0$ s.t. $a^x = b \pmod m$. a and m must be coprime.
+ * Time: $O(\sqrt m)$
+ * Status: tested for all 0 <= a,x,m < 200.
  */
 #pragma once
 
 ll modLog(ll a, ll b, ll m) {
 	assert(__gcd(a, m) == 1);
-	if (b == 1) return 0;
-	ll n = (ll) sqrt(m) + 1, e = 1;
-	rep(i,0,n) e = e * a % m;
+	ll n = (ll) sqrt(m) + 1, e = 1, x = 1, res = LLONG_MAX;
 	unordered_map<ll, ll> f;
-	for (ll i=1, x = e; i<=n; i++, x = x*e%m) f.emplace(x, i);
-	for (ll i = 0, x = b; i <= n; i++, x = x * a % m)
-		if (f.count(x)) return f[x] * n - i;
-	return -1;
+	rep(i,0,n) e = e * a % m;
+	rep(i,0,n) x = x * e % m, f.emplace(x, i + 1);
+	rep(i,0,n) if (f.count(b = b * a % m))
+		res = min(res, f[b] * n - i - 1);
+	return res;
 }
--- fuzz-tests/number-theory/ModLog.cpp
+++ fuzz-tests/number-theory/ModLog.cpp
@@ -10,25 +10,29 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-#include "../../content/number-theory/ModMulLL.h"
 #include "../../content/number-theory/ModLog.h"
 
-uniform_int_distribution<ll> uni(1, 100);
-const int ITERS = 1e7;
-const int LIM = 1000;
 int main() {
-    rep(m,1,200) {
-        rep(a,0,m) {
+    const int lim = 100;
+    rep(m,1,lim) {
+        rep(a,0,lim) {
+            if (__gcd(a, m) != 1) continue;
+            vector<ll> ans(m, -1);
+            ll b = 1 % m;
             rep(x,0,m) {
-                if (__gcd(a, m) != 1) continue;
-                ull b  = mod_pow(a, x, m);
-                ull res = modLog(a, b, m);
-                ull recover = mod_pow(a, res, m);
-                // cout<<a<<' '<<b<<' '<<m<<endl;
-                if (recover != b) {
-                    cout<<a<<'^'<<x<<" = "<<b<<" mod "<<m<<endl;
-                    cout<<"Found "<<res<<endl;
-                    return 0;
+                if (ans[b] == -1) ans[b] = x;
+                b = b * a % m;
+            }
+            rep(b,0,lim) {
+                ll res = modLog(a, b, m);
+                ll b2 = b % m;
+                if (ans[b2] == -1) assert(res == LLONG_MAX);
+                else {
+                    if (ans[b2] != res) {
+                        cerr << "FAIL" << endl;
+                        cerr << "Expected log(" << a << ", " << b << ", " << m << ") = " << ans[b2] << ", found " << res << endl;
+                        return 1;
+                    }
                 }
             }
         }
