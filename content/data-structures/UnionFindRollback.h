4556180b4079e514f1d0c510b8e1102d3240db2d
Simon Lindholm <simon.lindholm10@gmail.com>
2019-12-26 21:30:05 +0100

    UnionFind with rollback

---
+++ content/data-structures/UnionFindRollback.h
@@ -0,0 +1,33 @@
+/**
+ * Author: Lukas Polacek, Simon Lindholm
+ * Date: 2019-12-26
+ * License: CC0
+ * Source: folklore
+ * Description: Disjoint-set data structure with undo.
+ * If undo is not needed, skip st, time() and rollback().
+ * Usage: int t = uf.time(); ...; uf.rollback(t);
+ * Time: $O(\log(N))$
+ */
+#pragma once
+
+struct RollbackUF {
+	vi e; vector<pii> st;
+	RollbackUF(int n) : e(n, -1) {}
+	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : find(e[x]); }
+	int time() { return sz(st); }
+	void rollback(int t) {
+		for (int i = time(); i --> t;)
+			e[st[i].first] = st[i].second;
+		st.resize(t);
+	}
+	bool join(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		if (e[a] > e[b]) swap(a, b);
+		st.push_back({a, e[a]});
+		st.push_back({b, e[b]});
+		e[a] += e[b]; e[b] = a;
+		return true;
+	}
+};
