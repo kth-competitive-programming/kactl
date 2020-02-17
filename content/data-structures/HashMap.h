4dfe52f62fb37c28d03c04eb9b196469434be228
Simon Lindholm <simon.lindholm10@gmail.com>
2020-02-17 23:23:48 +0100

    Make hash constant more obviously odd

--- content/data-structures/HashMap.h
+++ content/data-structures/HashMap.h
@@ -11,7 +11,7 @@
 #include <bits/extc++.h> /** keep-include */
 // To use most bits rather than just the lowest ones:
 struct chash {
-	const uint64_t C = ll(2e18 * M_PI) + 71; // large odd number
+	const uint64_t C = ll(2e18 * M_PI) | 71; // large odd number
 	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
 };
 __gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
@@ -20,7 +20,7 @@ __gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
 
 const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
 struct chash { // To use most bits rather than just the lowest ones:
-	const uint64_t C = ll(2e18 * M_PI) + 71; // large odd number
+	const uint64_t C = ll(2e18 * M_PI) | 71; // large odd number
 	ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
 };
 __gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
