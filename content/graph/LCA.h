307f1ac4824f4b5723f70e0daab589cfc8992430
Simon Lindholm <simon.lindholm10@gmail.com>
2020-02-19 22:04:00 +0100

    Remove dist from LCA

--- content/graph/LCA.h
+++ content/graph/LCA.h
@@ -3,35 +3,27 @@
  * Date: 2020-02-20
  * License: CC0
  * Source: Folklore
- * Status: Somewhat tested
+ * Status: stress-tested
  * Description: Data structure for computing lowest common ancestors in a tree
  * (with 0 as root). C should be an adjacency list of the tree, either directed
- * or undirected. Can also find the distance between two nodes.
- * Usage:
- *  LCA lca(undirGraph);
- *  lca.query(firstNode, secondNode);
- *  lca.distance(firstNode, secondNode);
+ * or undirected.
  * Time: $O(N \log N + Q)$
  */
 #pragma once
-
-typedef vector<pii> vpi;
-typedef vector<vpi> graph;
 
 #include "../data-structures/RMQ.h"
 
 struct LCA {
 	int T = 0;
 	vi time, path, ret;
-	vector<ll> dist;
 	RMQ<int> rmq;
 
-	LCA(graph& C):time(sz(C)), dist(sz(C)), rmq((dfs(C), ret)) {}
-	void dfs(graph& C, int v = 0, int p = -1, ll di = 0) {
-		time[v] = T++, dist[v] = di;
-		trav(e, C[v]) if (e.first != p) {
+	LCA(vector<vi>& C) : time(sz(C)), rmq((dfs(C,0,-1), ret)) {}
+	void dfs(vector<vi>& C, int v, int par) {
+		time[v] = T++;
+		trav(y, C[v]) if (y != par) {
 			path.push_back(v), ret.push_back(time[v]);
-			dfs(C, e.first, v, di + e.second);
+			dfs(C, y, v);
 		}
 	}
 
@@ -39,9 +31,5 @@ struct LCA {
 		if (a == b) return a;
 		tie(a, b) = minmax(time[a], time[b]);
 		return path[rmq.query(a, b)];
-	}
-	ll distance(int a, int b) {
-		int lca = query(a, b);
-		return dist[a] + dist[b] - 2 * dist[lca];
 	}
 };
