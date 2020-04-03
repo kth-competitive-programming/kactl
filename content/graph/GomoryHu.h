92093ca54e5b1013c92d22eb57e597c8df05d0cb
Horace He <horacehe2007@yahoo.com>
2020-04-03 04:11:41 -0400

    remove accidental inclusion of test

--- content/graph/GomoryHu.h
+++ content/graph/GomoryHu.h
@@ -21,7 +21,7 @@ vector<Edge> gomoryHu(int N, vector<Edge> ed) {
 		trav(t,ed) D.addEdge(t[0], t[1], t[2], t[2]);
 		tree.push_back({i, par[i], D.calc(i, par[i])});
 		rep(j,i+1,N)
-			if (par[j] == par[i] && !D.leftOfMinCut(j)) par[j] = i;
+			if (par[j] == par[i] && D.leftOfMinCut(j)) par[j] = i;
 	}
 	return tree;
 }
