647a318a57b6c530c28438ff504ef927fbe55fed
Horace He <horacehe2007@yahoo.com>
2020-04-11 20:51:57 -0400

    updated NTT

--- content/numerical/NumberTheoreticTransform.h
+++ content/numerical/NumberTheoreticTransform.h
@@ -17,31 +17,37 @@ const ll mod = (119 << 23) + 1, root = 62; // = 998244353
 // For p < 2^30 there is also e.g. 5 << 25, 7 << 26, 479 << 21
 // and 483 << 21 (same root). The last two are > 10^9.
 
-typedef vector<ll> vl;
-void ntt(vl& a, vl& rt, vl& rev, int n) {
-	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
-				ll z = rt[j + k] * a[i + j + k] % mod, &ai = a[i + j];
-				a[i + j + k] = (z > ai ? ai - z + mod : ai - z);
-				ai += (ai + z >= mod ? z - mod : z);
-	}
+
+void ntt(vi &a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vi rt(2, 1);
+    static int curL = mod / 2;
+    for (static int k = 2; k < n; k *= 2) {
+        rt.resize(n);
+        ll z[] = {1, modpow(root, curL /= 2)};
+        rep(i, k, 2 * k) rt[i] = (ll)rt[i / 2] * z[i & 1] % mod;
+    }
+    vi rev(n);
+    rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k)
+            rep(j, 0, k) {
+                int z = (ll)rt[j + k] * a[i + j + k] % mod;
+                int &ai = a[i + j];
+                a[i + j + k] = (z > ai ? ai - z + mod : ai - z);
+                ai += (ai + z >= mod ? z - mod : z);
+            }
 }
 
-vl conv(const vl& a, const vl& b) {
-	if (a.empty() || b.empty())
-		return {};
-	int s = sz(a)+sz(b)-1, B = 32 - __builtin_clz(s), n = 1 << B;
-	vl L(a), R(b), out(n), rt(n, 1), rev(n);
-	L.resize(n), R.resize(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << B) / 2;
-	ll curL = mod / 2, inv = modpow(n, mod - 2);
-	for (int k = 2; k < n; k *= 2) {
-		ll z[] = {1, modpow(root, curL /= 2)};
-		rep(i,k,2*k) rt[i] = rt[i / 2] * z[i & 1] % mod;
-	}
-	ntt(L, rt, rev, n); ntt(R, rt, rev, n);
-	rep(i,0,n) out[-i & (n-1)] = L[i] * R[i] % mod * inv % mod;
-	ntt(out, rt, rev, n);
-	return {out.begin(), out.begin() + s};
+vi conv(const vi &a, const vi &b) {
+    if (a.empty() || b.empty()) return {};
+    int s = sz(a) + sz(b) - 1, B = 32 - __builtin_clz(s), n = 1 << B;
+    int inv = modpow(n, mod - 2);
+    vi L(a), R(b), out(n);
+    L.resize(n), R.resize(n);
+    ntt(L), ntt(R);
+    rep(i, 0, n) out[-i & (n - 1)] = (ll)L[i] * R[i] % mod * inv % mod;
+    ntt(out);
+    return {out.begin(), out.begin() + s};
 }
