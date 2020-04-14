74618c6810a8f631ba02403892bc02e97200805e
Horace He <horacehe2007@yahoo.com>
2020-04-14 11:42:16 -0400

    Readd multinomial

---
+++ content/combinatorial/multinomial.h
@@ -0,0 +1,15 @@
+/**
+ * Author: Mattias de Zalenski, Fredrik Niemelä, Per Austrin, Simon Lindholm
+ * Date: 2002-09-26
+ * Source: Max Bennedich
+ * Description: Computes $\displaystyle \binom{k_1 + \dots + k_n}{k_1, k_2, \dots, k_n} = \frac{(\sum k_i)!}{k_1!k_2!...k_n!}$.
+ * Status: Tested on kattis:lexicography
+ */
+#pragma once
+
+ll multinomial(vi& v) {
+	ll c = 1, m = v.empty() ? 1 : v[0];
+	rep(i,1,sz(v)) rep(j,0,v[i])
+		c = c * ++m / (j+1);
+	return c;
+}
