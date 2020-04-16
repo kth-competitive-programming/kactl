660f91ce3f29f0e212229c56f4dd39f4c15b2150
Horace He <horacehe2007@yahoo.com>
2020-04-16 18:21:12 -0400

    Added Matroid Intersection

--- content/graph/MatroidIntersection.h
+++ content/graph/MatroidIntersection.h
@@ -2,58 +2,79 @@
  * Author: tfg, Aeren, pajenegod, chilli
  * Date: 2020-04-15
  * License: CC0
- * Source:
- * Description:
- * Time:
- * Status:
+ * Source: https://codeforces.com/blog/entry/69287
+ * Description: Given two matroids, finds the largest common independent set.
+ * For the color and graph matroids, this would be the largest forest where no
+ * two edges are the same color. A matroid has 3 functions
+ *  - check(int x): returns if current matroid can add x without becoming dependent
+ *  - add(int x): adds an element to the matroid (guaranteed to never make it dependent)
+ *  - clear(): sets the matroid to the empty matroid
+ * The matroid is given an int representing the element, and is expected to
+ * convert it (e.g: the color or the endpoints)
+ * Pass the matroid with more expensive add/clear operations to M1.
+ * Time: R^2N(M2.add + M1.check + M2.check) + R^3 M1.add + R^2 M1.clear + RN M2.clear
+ * Status: Tested on SWERC 2011D, Pick Your Own Nim
+ * Usage:
+ * Details:
  */
-struct Matroid {
-    bool check(int x) {} // will adding x make matroid dependent
-    void add(int x) {} // adds x to matroid
-    void clear() {}  // resets matroid to empty
+#include "../data-structures/UnionFind.h"
+
+struct ColorMat {
+	vi cnt, clr;
+	ColorMat(int n, vector<int> clr) : cnt(n), clr(clr) {}
+	bool check(int x) { return !cnt[clr[x]]; }
+	void add(int x) { cnt[clr[x]]++; }
+	void clear() { fill(all(cnt), 0); }
 };
-template <class M1, class M2, class T> struct MatroidIsect {
+struct GraphMat {
+	UF uf;
+	vector<array<int, 2>> e;
+	GraphMat(int n, vector<array<int, 2>> e) : uf(n), e(e) {}
+	bool check(int x) { return !uf.sameSet(e[x][0], e[x][1]); }
+	void add(int x) { uf.join(e[x][0], e[x][1]); }
+	void clear() { uf = UF(sz(uf.e)); }
+};
+template <class M1, class M2> struct MatroidIsect {
 	int n;
-	vector<T> gset; vector<bool> iset;
-	vector<M1> m1; vector<M2> m2;
-	MatroidIsect(M1 m1, M2 m2, vector<T> gset) :
-	 n(sz(gset)),gset(gset), iset(n+1), m1(n+1,m1), m2(n+1,m2) {}
-	vector<T> solve() {
-		iset[n] = true; // Add greedily
-		rep(i,0,n) if (test(m1, i, n) && test(m2, i, n)) {
-			iset[i] = true;
-			m1[n].insert(gset[i]), m2[n].insert(gset[i]);
-		}
+	vector<char> iset;
+	M1 m1; M2 m2;
+	MatroidIsect(M1 m1, M2 m2, int n) : n(n), iset(n + 1), m1(m1), m2(m2) {}
+	vi solve() {
+		rep(i,0,n) if (m1.check(i) && m2.check(i))
+			iset[i] = true, m1.add(i), m2.add(i);
 		while (augment());
-		vector<T> ans;
-		rep(i,0,n) if (iset[i]) ans.push_back(gset[i]);
+		vi ans;
+		rep(i,0,n) if (iset[i]) ans.push_back(i);
 		return ans;
 	}
-	template <class M>
-	bool test(vector<M> &m, int add, int rem) {
-		return !iset[add] && iset[rem] && m[rem].check(gset[add]);
-	}
 	bool augment() {
-		rep(i,0,n + 1) { //  Construct matroids
-			if (iset[i]) {
-				m1[i].clear(), m2[i].clear();
-				rep(v, 0, n) if (v != i && iset[v])
-				 m1[i].add(gset[v]), m2[i].add(gset[v]);
-			}
-		}
-		queue<int> q({n});
-		vector<int> frm(n + 1, -1);
-		while (!q.empty()) { // Augment path
-			int on = q.front(); q.pop();
-			rep(v,0,n+1) if (frm[v] == -1 &&
-					(test(m1, v, on) || test(m2, on, v))) {
-				if (v == n) {
-					while (on != n)
-						iset[on] = !iset[on], on = frm[on];
+		vector<int> frm(n, -1);
+		queue<int> q({n}); // starts at dummy node
+		auto fwdE = [&](int a) {
+			vi ans;
+			m1.clear();
+			rep(v, 0, n) if (iset[v] && v != a) m1.add(v);
+			rep(b, 0, n) if (!iset[b] && frm[b] == -1 && m1.check(b))
+				ans.push_back(b), frm[b] = a;
+			return ans;
+		};
+		auto backE = [&](int b) {
+			m2.clear();
+			rep(cas, 0, 2) rep(v, 0, n)
+				if ((v == b || iset[v]) && (frm[v] == -1) == cas) {
+					if (!m2.check(v))
+						return cas ? q.push(v), frm[v] = b, v : -1;
+					m2.add(v);
+				}
+			return n;
+		};
+		while (!q.empty()) {
+			int a = q.front(), c; q.pop();
+			for (int b : fwdE(a))
+				while((c = backE(b)) >= 0) if (c == n) {
+					while (b != n) iset[b] ^= 1, b = frm[b];
 					return true;
 				}
-				q.push(v), frm[v] = on;
-			}
 		}
 		return false;
 	}
