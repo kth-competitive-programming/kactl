b640b4571d8761d367c717c645a465c7273f2931
Horace He <horacehe2007@yahoo.com>
2020-05-03 11:36:06 -0700

    reduced miller rabin bounds

--- stress-tests/number-theory/MillerRabin.cpp
+++ stress-tests/number-theory/MillerRabin.cpp
@@ -8,7 +8,7 @@ namespace sieve {
 ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 int afactors[] = {2, 3, 5, 13, 19, 73, 193, 407521, 299210837};
 
-const ull MR_LIM = 1ULL << 63;
+const ull MR_LIM = 1ULL << 62;
 
 // Accurate for arbitrary 64-bit numbers
 ull int128_mod_mul(ull a, ull b, ull m) { return (ull)((__uint128_t)a * b % m); }
