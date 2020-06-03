49e1efb30281db6ac7418d06912adc9c3fbc1adc
Bjorn <pajenegod@gmail.com>
2020-06-03 02:20:44 +0200

    New more general ModLog

--- content/number-theory/ModLog.h
+++ content/number-theory/ModLog.h
@@ -1,21 +1,22 @@
 /**
- * Author: Andrei Navumenka, chilli
- * Date: 2019-11-08
- * License: Unlicense
- * Source: https://github.com/indy256/codelibrary/blob/master/cpp/numbertheory/discrete_log.cpp
- * Description: Returns the smallest $x \ge 0$ s.t. $a^x = b \pmod m$. a and m must be coprime.
+ * Author: Bjorn Martinsson
+ * Date: 2020-06-03
+ * License: CC0
+ * Source: own work
+ * Description: Returns the smallest $x > 0$ s.t. $a^x = b \pmod m$.
  * Time: $O(\sqrt m)$
- * Status: tested for all 0 <= a,x,m < 200.
+ * Status: tested for all 0 <= a,x < 500 and 0 < m < 500.
  */
 #pragma once
 
 ll modLog(ll a, ll b, ll m) {
-	assert(__gcd(a, m) == 1);
-	ll n = (ll) sqrt(m) + 1, e = 1, x = 1, res = LLONG_MAX;
-	unordered_map<ll, ll> f;
-	rep(i,0,n) e = e * a % m;
-	rep(i,0,n) x = x * e % m, f.emplace(x, i + 1);
-	rep(i,0,n) if (f.count(b = b * a % m))
-		res = min(res, f[b] * n - i - 1);
-	return res;
+    ll n = (ll) sqrt(m) + 1, e = 1, f = 1, j = 1;
+    unordered_map<ll, ll> A;
+    while (j <= n && (e = f = e * a % m) != b % m)
+        A[e * b % m] = j++;
+    if (e == b % m) return j;
+    if (__gcd(m, e) == __gcd(m, b)) 
+        rep(i, 2, n + 2) if (A.count(e = e * f % m))
+            return n * i - A[e];
+    return LLONG_MAX;
 }
--- stress-tests/number-theory/ModLog.cpp
+++ stress-tests/number-theory/ModLog.cpp
@@ -6,21 +6,19 @@ int main() {
     const int lim = 100;
     rep(m,1,lim) {
         rep(a,0,lim) {
-            if (__gcd(a, m) != 1) continue;
             vector<ll> ans(m, -1);
-            ll b = 1 % m;
-            rep(x,0,m) {
+            ll b = a % m;
+            rep(x,1,max(m,2)) {
                 if (ans[b] == -1) ans[b] = x;
                 b = b * a % m;
             }
-            rep(b,0,lim) {
+            rep(b,0,m) {
                 ll res = modLog(a, b, m);
-                ll b2 = b % m;
-                if (ans[b2] == -1) assert(res == LLONG_MAX);
+                if (ans[b] == -1) assert(res == LLONG_MAX);
                 else {
-                    if (ans[b2] != res) {
+                    if (ans[b] != res) {
                         cerr << "FAIL" << endl;
-                        cerr << "Expected log(" << a << ", " << b << ", " << m << ") = " << ans[b2] << ", found " << res << endl;
+                        cerr << "Expected log(" << a << ", " << b << ", " << m << ") = " << ans[b] << ", found " << res << endl;
                         return 1;
                     }
                 }
