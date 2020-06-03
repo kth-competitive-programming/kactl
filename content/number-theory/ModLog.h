bc6c836d2b259ed6a412f3f9c745a6105b1a0085
Bjorn <pajenegod@gmail.com>
2020-06-03 12:15:52 +0200

    Switched spaces to tabs

--- content/number-theory/ModLog.h
+++ content/number-theory/ModLog.h
@@ -10,13 +10,13 @@
 #pragma once
 
 ll modLog(ll a, ll b, ll m) {
-    ll n = (ll) sqrt(m) + 1, e = 1, f = 1, j = 1;
-    unordered_map<ll, ll> A;
-    while (j <= n && (e = f = e * a % m) != b % m)
-        A[e * b % m] = j++;
-    if (e == b % m) return j;
-    if (__gcd(m, e) == __gcd(m, b)) 
-        rep(i, 2, n + 2) if (A.count(e = e * f % m))
-            return n * i - A[e];
-    return LLONG_MAX;
+	ll n = (ll) sqrt(m) + 1, e = 1, f = 1, j = 1;
+	unordered_map<ll, ll> A;
+	while (j <= n && (e = f = e * a % m) != b % m)
+		A[e * b % m] = j++;
+	if (e == b % m) return j;
+	if (__gcd(m, e) == __gcd(m, b)) 
+		rep(i,2,n+2) if (A.count(e = e * f % m))
+			return n * i - A[e];
+	return LLONG_MAX;
 }
