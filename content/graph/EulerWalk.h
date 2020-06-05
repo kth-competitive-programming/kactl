04bf0c2f11e075bd8a934a6c01ff10c627f17079
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-06-05 05:53:29 +0200

    Return the edge indices by default.
    And made default `src` a non-isolated node.

--- content/graph/EulerWalk.h
+++ content/graph/EulerWalk.h
@@ -6,26 +6,44 @@
  * Description: Eulerian undirected/directed path/cycle algorithm.
  * Input should be a vector of (dest, global edge index), where
  * for undirected graphs, forward/backward edges have the same index.
- * Returns a list of nodes in the Eulerian path/cycle with src at both start and end, or
- * empty list if no cycle/path exists.
- * To get edge indices back, add .second to s and ret.
+ * Returns a list of pairs of the nodes in the Eulerian path/cycle with src at both start and end,
+ * and the edge indices, or empty list if no cycle/path exists.
  * Time: O(V + E)
  * Status: stress-tested
  */
 #pragma once
 
-vi eulerWalk(vector<vector<pii>>& gr, int nedges, int src=0) {
-	int n = sz(gr);
-	vi D(n), its(n), eu(nedges), ret, s = {src};
-	D[src]++; // to allow Euler paths, not just cycles
-	while (!s.empty()) {
-		int x = s.back(), y, e, &it = its[x], end = sz(gr[x]);
-		if (it == end){ ret.push_back(x); s.pop_back(); continue; }
-		tie(y, e) = gr[x][it++];
-		if (!eu[e]) {
-			D[x]--, D[y]++;
-			eu[e] = 1; s.push_back(y);
-		}}
-	for (int x : D) if (x < 0 || sz(ret) != nedges+1) return {};
-	return {ret.rbegin(), ret.rend()};
+vector<pii> eulerWalk(const vector<vector<pii>>& gr, int nedges, int src = -1) {
+  int n = sz(gr);
+  if (src == -1) {
+    // Find a non-isolated node.
+    src = find_if(all(gr), [](const vector<pii>& v) { return !v.empty(); }) -
+          begin(gr);
+    if (src == n) return {};
+  }
+
+  vi D(n), its(n), eu(nedges);
+  vector<pii> s = {make_pair(src, -1)};
+  vector<pii> ret;
+  D[src]++;  // to allow Euler paths, not just cycles
+  while (!s.empty()) {
+    auto x = s.back();
+    int y, e, &it = its[x.first], end = sz(gr[x.first]);
+    if (it == end) {
+      ret.emplace_back(x);
+      s.pop_back();
+      continue;
+    }
+    tie(y, e) = gr[x.first][it++];
+    if (!eu[e]) {
+      D[x.first]--, D[y]++;
+      eu[e] = 1;
+      s.emplace_back(y, e);
+    }
+  }
+  for (int x : D)
+    if (x < 0) return {};
+  if (sz(ret) != nedges + 1) return {};
+  reverse(all(ret));
+  return ret;
 }
