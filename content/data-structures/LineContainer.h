bfad0636af287068293f4cdb2d3dd9f738877f2b
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-08-30 11:58:17 +0200

    Make `LineContainer.inf` static member

--- content/data-structures/LineContainer.h
+++ content/data-structures/LineContainer.h
@@ -18,7 +18,7 @@ struct Line {
 
 struct LineContainer : multiset<Line, less<>> {
 	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
-	const ll inf = LLONG_MAX;
+	static const ll inf = LLONG_MAX;
 	ll div(ll a, ll b) { // floored division
 		return a / b - ((a ^ b) < 0 && a % b); }
 	bool isect(iterator x, iterator y) {
