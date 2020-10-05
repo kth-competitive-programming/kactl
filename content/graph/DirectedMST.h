fc1a059f8f9113d5e342316b5a4ab087031c7a0b
Simon Lindholm <simon.lindholm10@gmail.com>
2020-10-05 21:08:17 +0200

    Directed MST reconstruction (#153)
    * UnionFind with rollback
    
    * Directed MST reconstruction
    
    * Include UnionFindRollback in the pdf, remove UnionFind
    
    Its functionality is a superset, and one should know UnionFind by heart
    anyway.

--- content/graph/DirectedMST.h
+++ content/graph/DirectedMST.h
@@ -1,16 +1,17 @@
 /**
- * Author: chilli, Takanori MAEHARA
+ * Author: chilli, Takanori MAEHARA, Benq, Simon Lindholm
  * Date: 2019-05-10
  * License: CC0
  * Source: https://github.com/spaghetti-source/algorithm/blob/master/graph/arborescence.cc
- * Description: Edmonds' algorithm for finding the weight of the minimum spanning
+ * and https://github.com/bqi343/USACO/blob/42d177dfb9d6ce350389583cfa71484eb8ae614c/Implementations/content/graphs%20(12)/Advanced/DirectedMST.h for the reconstruction
+ * Description: Finds a minimum spanning
  * tree/arborescence of a directed graph, given a root node. If no MST exists, returns -1.
  * Time: O(E \log V)
  * Status: Stress-tested, also tested on NWERC 2018 fastestspeedrun
  */
 #pragma once
 
-#include "../data-structures/UnionFind.h"
+#include "../data-structures/UnionFindRollback.h"
 
 struct Edge { int a, b; ll w; };
 struct Node { /// lazy skew heap node
@@ -34,29 +35,41 @@ Node *merge(Node *a, Node *b) {
 }
 void pop(Node*& a) { a->prop(); a = merge(a->l, a->r); }
 
-ll dmst(int n, int r, vector<Edge>& g) {
-	UF uf(n);
+pair<ll, vi> dmst(int n, int r, vector<Edge>& g) {
+	RollbackUF uf(n);
 	vector<Node*> heap(n);
 	for (Edge e : g) heap[e.b] = merge(heap[e.b], new Node{e});
 	ll res = 0;
-	vi seen(n, -1), path(n);
+	vi seen(n, -1), path(n), par(n);
 	seen[r] = r;
+	vector<Edge> Q(n), in(n, {-1,-1}), comp;
+	deque<tuple<int, int, vector<Edge>>> cycs;
 	rep(s,0,n) {
 		int u = s, qi = 0, w;
 		while (seen[u] < 0) {
-			path[qi++] = u, seen[u] = s;
-			if (!heap[u]) return -1;
+			if (!heap[u]) return {-1,{}};
 			Edge e = heap[u]->top();
 			heap[u]->delta -= e.w, pop(heap[u]);
+			Q[qi] = e, path[qi++] = u, seen[u] = s;
 			res += e.w, u = uf.find(e.a);
-			if (seen[u] == s) {
+			if (seen[u] == s) { /// found cycle, contract
 				Node* cyc = 0;
+				int end = qi, time = uf.time();
 				do cyc = merge(cyc, heap[w = path[--qi]]);
 				while (uf.join(u, w));
-				u = uf.find(u);
-				heap[u] = cyc, seen[u] = -1;
+				u = uf.find(u), heap[u] = cyc, seen[u] = -1;
+				cycs.push_front({u, time, {&Q[qi], &Q[end]}});
 			}
 		}
+		rep(i,0,qi) in[uf.find(Q[i].b)] = Q[i];
 	}
-	return res;
+
+	for (auto& [u,t,comp] : cycs) { // restore sol (optional)
+		uf.rollback(t);
+		Edge inEdge = in[u];
+		for (auto& e : comp) in[uf.find(e.b)] = e;
+		in[uf.find(inEdge.b)] = inEdge;
+	}
+	rep(i,0,n) par[i] = in[i].a;
+	return {res, par};
 }
