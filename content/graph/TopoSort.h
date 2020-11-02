b893952be4a6cdbf1c4011a3093f35bd515e5b6f
Simon Lindholm <simon.lindholm10@gmail.com>
2020-11-02 11:54:58 +0100

    Don't optimize for the priority queue case in TopoSort

--- content/graph/TopoSort.h
+++ content/graph/TopoSort.h
@@ -13,14 +13,14 @@
 vi topoSort(const vector<vi>& gr) {
 	vi indeg(sz(gr)), ret;
 	for (auto& li : gr) for (int x : li) indeg[x]++;
-	queue<int> q; // use priority queue for lexic. smallest ans.
-	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push(-i);
+	queue<int> q; // use priority_queue for lexic. largest ans.
+	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push(i);
 	while (!q.empty()) {
-		int i = -q.front(); // top() for priority queue
+		int i = q.front(); // top() for priority queue
 		ret.push_back(i);
 		q.pop();
 		for (int x : gr[i])
-			if (--indeg[x] == 0) q.push(-x);
+			if (--indeg[x] == 0) q.push(x);
 	}
 	return ret;
 }
