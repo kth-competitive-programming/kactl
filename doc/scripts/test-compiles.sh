1eb3c09953e5e5722391ed663dc6053a3f85696e
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2021-02-25 11:12:28 +0100

    Use c++17 in test-compiles.sh

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -4,4 +4,4 @@ echo "
 #include \"../content/contest/template.cpp\"
 #include \"../$FILE\"
 " >build/temp.cpp
-g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++14 build/temp.cpp && rm a.out build/temp.cpp
+g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++17 build/temp.cpp && rm a.out build/temp.cpp
