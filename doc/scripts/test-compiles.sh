88d8daa5b0ce9c027f07629ae0bc54336a8b6301
lrvideckis <lukevideckis@gmail.com>
2021-12-19 12:13:06 -0800

    removing flags for runtime errors

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -5,4 +5,4 @@ echo "
 #include \"../$FILE\"
 " >build/temp.cpp
 # Catching silly mistakes with GCC: https://codeforces.com/blog/entry/15547
-g++  -Wall -Wextra -pedantic -Wconversion -Wno-unused-result -Wfatal-errors -DLOCAL -std=c++2a  -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector build/temp.cpp && rm a.out build/temp.cpp
+g++  -Wall -Wextra -pedantic -Wconversion -Wno-unused-result -Wfatal-errors -std=c++14  -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align build/temp.cpp && rm a.out build/temp.cpp
