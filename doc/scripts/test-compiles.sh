ae04e016296274c981f2c9a21b0c37a2e2dbf112
lrvideckis <lukevideckis@gmail.com>
2021-12-18 23:39:02 -0800

    https://codeforces.com/blog/entry/15547: Kactl is a CP library for CPer's maintained by CPer's. So let's add better compiler flags discovered by other CPers

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -4,4 +4,5 @@ echo "
 #include \"../content/contest/template.cpp\"
 #include \"../$FILE\"
 " >build/temp.cpp
-g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++14 build/temp.cpp && rm a.out build/temp.cpp
+# Catching silly mistakes with GCC: https://codeforces.com/blog/entry/15547
+g++  -Wall -Wextra -pedantic -Wconversion -Wno-unused-result -Wfatal-errors -DLOCAL -std=c++2a  -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector build/temp.cpp && rm a.out build/temp.cpp
