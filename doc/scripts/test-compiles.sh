355df5fb095f0ec2601073effd278dbb3da7e0e4
lrvideckis <lukevideckis@gmail.com>
2021-12-19 12:42:41 -0800

    adding -O2 (some warnings are only enabled together with optimization), -Wlogical-op, -Wduplicated-cond; reordered, so that original flags are still at beginning

--- doc/scripts/test-compiles.sh
+++ doc/scripts/test-compiles.sh
@@ -5,4 +5,4 @@ echo "
 #include \"../$FILE\"
 " >build/temp.cpp
 # Catching silly mistakes with GCC: https://codeforces.com/blog/entry/15547
-g++  -Wall -Wextra -pedantic -Wconversion -Wno-unused-result -Wfatal-errors -std=c++14  -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align build/temp.cpp && rm a.out build/temp.cpp
+g++ -Wall -Wextra -O2 -Wfatal-errors -Wconversion -std=c++14 -pedantic -Wno-unused-result -Wshadow -Wformat=2 -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align build/temp.cpp && rm a.out build/temp.cpp
