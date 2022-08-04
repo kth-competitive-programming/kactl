fcedd4f5a248bcdb5b5e4ca365167715f5ae25e2
hhhhaura <hhhhaura@gmail.com>
2022-08-04 10:27:16 +0800

    modify strings

--- content/strings/KMP.h
+++ content/strings/KMP.h
@@ -1,27 +1,31 @@
 /**
- * Author: Johan Sannemo
- * Date: 2016-12-15
- * License: CC0
- * Description: pi[x] computes the length of the longest prefix of s that ends at x, other than s[0...x] itself (abacaba -> 0010123).
- * Can be used to find all occurrences of a string.
- * Time: O(n)
+ * Author: hhhhaura
+ * Date: 2022/08/04
+ * Description: $f[x]$ = the longest prefix $i$, so that $s(0:i)$ is a suffix of $s(0:x)$.
+ * When $A_i \neq B_{j + 1}$ move $j$ to $f[j]$.
+ * Time: $O(|A| + |B|)$
  * Status: Tested on kattis:stringmatching
  */
 #pragma once
 
-vi pi(const string& s) {
-	vi p(sz(s));
-	rep(i,1,sz(s)) {
-		int g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
-		p[i] = g + (s[i] == s[g]);
+vi fail_function(string &s) {
+	int n = sz(s);
+	vi f(n, -1); // leave an additional space
+	rep(i, 1, n - 1) {
+		int cur = f[i - 1];
+		while(cur != -1 && s[cur + 1] != s[i]) cur = f[cur];
+		cur += (s[cur + 1] == s[i]);
+		f[i] = cur;
 	}
-	return p;
+	return f;
 }
-
-vi match(const string& s, const string& pat) {
-	vi p = pi(pat + '\0' + s), res;
-	rep(i,sz(p)-sz(s),sz(p))
-		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
-	return res;
+int KMP(string &a, string &b) {
+	vi f = fail_function(b);
+	int j = -1, ans = 0;
+	rep(i, 0, sz(a) - 1) {
+		while(j != -1 && b[j + 1] != a[i]) j = f[j];
+		j += (b[j + 1] == a[i]);
+		ans += (j == sz(b));
+	}
+	return ans;
 }
