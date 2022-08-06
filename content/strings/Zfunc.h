69ccb015e4db5d1f8da82a629820bc682d376f73
hhhhaura <hhhhaura@gmail.com>
2022-08-06 22:15:31 +0800

    fix Zfunc.h

--- content/strings/Zfunc.h
+++ content/strings/Zfunc.h
@@ -6,6 +6,7 @@
  * Status: stress-tested
  */
 #pragma once
+<<<<<<< HEAD
 
 vi Z(const string& S) {
 	vi z(sz(S));
@@ -16,6 +17,17 @@ vi Z(const string& S) {
 			z[i]++;
 		if (i + z[i] > r)
 			l = i, r = i + z[i];
+=======
+vi zvalue(string &s) {
+	int n = sz(s);
+	vi z(n, 0);
+	int l = 1, r = 0;
+	rep(i, 1, n - 1) {
+		z[i] = max(0, min(z[i - l], r - i + 1));
+		while(i + z[i] < n && s[i + z[i]] == s[z[i]]) {
+			l = i, r = i + z[i], z[i] ++;
+		}
+>>>>>>> 0efb2e9 (fix Zfunc.h)
 	}
 	return z;
 }
