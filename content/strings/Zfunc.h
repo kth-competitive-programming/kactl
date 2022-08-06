218742f6e4b0c8458e329fa0be77d186cf9ab979
hhhhaura <hhhhaura@gmail.com>
2022-08-06 21:57:48 +0800

    modify strings

--- content/strings/Zfunc.h
+++ content/strings/Zfunc.h
@@ -1,21 +1,22 @@
 /**
- * Author: chilli
+ * Author: hhhhaura
  * License: CC0
  * Description: z[x] computes the length of the longest common prefix of s[i:] and s, except z[0] = 0. (abacaba -> 0010301)
- * Time: O(n)
+ * Time: O(N)
  * Status: stress-tested
  */
 #pragma once
-
-vi Z(string S) {
-	vi z(sz(S));
-	int l = -1, r = -1;
-	rep(i,1,sz(S)) {
-		z[i] = i >= r ? 0 : min(r - i, z[i - l]);
-		while (i + z[i] < sz(S) && S[i + z[i]] == S[z[i]])
-			z[i]++;
-		if (i + z[i] > r)
-			l = i, r = i + z[i];
+vi z-value(string &s) {
+	int n = sz(s);
+	vi z(n, 0);
+	int l = 1, r = 0;
+	rep(i, 1, n - 1) {
+		z[i] = max(0, min(z[i - l], r - i + 1));
+		while(i + z[i] < n && s[i + z[i]] == s[z[i]]) {
+			l = i, r = i + z[i], z[i] ++;
+		}
 	}
 	return z;
 }
+
+
