d303ccc1780ab0b3c0009f50c2325498dc04bbe9
Netanya Robinson <netanyarobinson19@gmail.com>
2022-10-28 17:56:06 +1100

    changes

--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -222,9 +222,9 @@ def print_header(data, outstream):
     def adjust(name):
         return name if name.startswith('.') else name.split('.')[0]
     output = r"\enspace{}".join(map(adjust, lines[:ind]))
-    font_size = 10
+    font_size = 9
     if header_length > 150:
-        font_size = 8
+        font_size = 7
     output = r"\hspace{3mm}\textbf{" + output + "}"
     output = "\\fontsize{%d}{%d}" % (font_size, font_size) + output
     print(output, file=outstream)
