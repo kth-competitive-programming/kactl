6d1dbd7eb6d184c32cd101093e35cb3cb64a535c
akcube <a.kishorekumar@students.iiit.ac.in>
2022-12-05 13:45:00 +0530

    Added assert + removed reorder warning + cosmetic update

--- content/data-structures/RangeUpdateTree.h
+++ content/data-structures/RangeUpdateTree.h
@@ -13,15 +13,16 @@ template <typename T, typename F>
 struct RangeUpdateTree{
     int n; vector<T> tree;
     T identity; F merge;
-    RangeUpdateTree(vector<T> &arr, T id, F _m) : n(sz(arr)), identity(id), merge(_m), tree(2*n){
+    RangeUpdateTree(vector<T> &arr, T id, F _m) : n(sz(arr)), tree(2*n), identity(id), merge(_m){
         for(int i=0; i<n; i++) tree[n+i] = arr[i];
     }
     void update(int l, int r, T value){
+        assert(l >= 0 and r < n and l <= r);
         for(l += n, r += n; l <= r; l>>=1, r>>=1){
-            if(l==r) { tree[l] = merge(value, tree[l]); break; }
             if(l&1) tree[l] = merge(value, tree[l]), l++;
             if(!(r&1)) tree[r] = merge(value, tree[r]), r--;
         }
+        if(l==r) tree[l] = merge(value, tree[l]);
     }
     T query(int v){
         T res = tree[v+=n];
