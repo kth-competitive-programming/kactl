efc0e3382cd7ca3b0a8934adf3a9125aea6b92d6
akcube <a.kishorekumar@students.iiit.ac.in>
2022-12-06 18:15:02 +0530

    Major redesign for HLD, NEEDS TESTING

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -1,65 +1,51 @@
 /**
- * Author: Benjamin Qi, Oleksandr Kulkov, chilli
- * Date: 2020-01-12
- * License: CC0
- * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
- * Description: Decomposes a tree into vertex disjoint heavy paths and light
- * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
- * support commutative segtree modifications/queries on paths and subtrees.
- * Takes as input the full adjacency list. VALS\_EDGES being true means that
- * values are stored in the edges, as opposed to the nodes. All values
- * initialized to the segtree default. Root must be 0.
- * Time: O((\log N)^2)
- * Status: stress-tested against old HLD
- */
-#pragma once
+ * Note: Recheck the indexing for VAL_ON_EDGES = true. Haven't verified beyond testing on a CF problem.
+ * NOT STRESS-TESTED
+*/
 
-#include "../data-structures/LazySegmentTree.h"
-
-template <bool VALS_EDGES> struct HLD {
-	int N, tim = 0;
-	vector<vi> adj;
-	vi par, siz, depth, rt, pos;
-	Node *tree;
-	HLD(vector<vi> adj_)
-		: N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1), depth(N),
-		  rt(N),pos(N),tree(new Node(0, N)){ dfsSz(0); dfsHld(0); }
-	void dfsSz(int v) {
-		if (par[v] != -1) adj[v].erase(find(all(adj[v]), par[v]));
-		for (int& u : adj[v]) {
-			par[u] = v, depth[u] = depth[v] + 1;
-			dfsSz(u);
-			siz[v] += siz[u];
-			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
-		}
-	}
-	void dfsHld(int v) {
-		pos[v] = tim++;
-		for (int u : adj[v]) {
-			rt[u] = (u == adj[v][0] ? rt[v] : u);
-			dfsHld(u);
-		}
-	}
-	template <class B> void process(int u, int v, B op) {
-		for (; rt[u] != rt[v]; v = par[rt[v]]) {
-			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
-			op(pos[rt[v]], pos[v] + 1);
-		}
-		if (depth[u] > depth[v]) swap(u, v);
-		op(pos[u] + VALS_EDGES, pos[v] + 1);
-	}
-	void modifyPath(int u, int v, int val) {
-		process(u, v, [&](int l, int r) { tree->add(l, r, val); });
-	}
-	int queryPath(int u, int v) { // Modify depending on problem
-		int res = -1e9;
-		process(u, v, [&](int l, int r) {
-				res = max(res, tree->query(l, r));
-		});
-		return res;
-	}
-	int querySubtree(int v) { // modifySubtree is similar
-		return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
-	}
+template <typename T, typename F, bool VAL_ON_EDGES>
+struct HLD{
+    int n, timer = 0;
+    vi size, top, tin, dep, par;
+    Segtree<T, F> st;
+    HLD(vvi &adj, const vector<T> &arr, T id, F _m) : n(sz(adj)), size(n, 1), top(n), tin(n), dep(n), par(n, -1){
+        function<void(int)> dfs_hvy = [&](int v){
+            if(adj[v][0] == par[v]) swap(adj[v].front(), adj[v].back());
+            for(auto &to : adj[v]) if(to != par[v]) {
+                par[to] = v, dep[to] = dep[v] + 1;
+                dfs_hvy(to);
+                size[v] += size[to];
+                if(size[to] > size[adj[v][0]]) swap(to, adj[v][0]);
+            }
+        }; dfs_hvy(0);
+        vector<T> temp; temp.reserve(n);
+        function<void(int)> dfs_hld = [&](int v){
+            tin[v] = timer++; temp.pb(arr[v]);
+            for(auto &to : adj[v]) if(to != par[v]) {
+                top[to] = (to == adj[v][0] ? top[v] : to);
+                dfs_hld(to);
+            }
+        }; dfs_hld(0);
+        st = Segtree<T, decltype(_m)>(temp, id, _m);
+    }
+    template <class B> int process(int u, int v, B op) {
+        for (; top[u] != top[v]; v = par[top[v]]) {
+            if(dep[top[u]] > dep[top[v]]) swap(u, v);
+            op(tin[top[v]], tin[v]);
+        }
+        if (dep[u] > dep[v]) swap(u, v);
+        if(!VAL_ON_EDGES or u!=v) op(tin[u] + VAL_ON_EDGES, tin[v]);
+        return u;
+    }
+    int lca(int u, int v) { return process(u, v, [&](int,int){}); }
+    T query(int u, int v){
+        T ans = st.identity;
+        process(u, v, [&](int l, int r){
+            ans = st.merge(ans, st.query(l, r));
+        });
+        return ans;
+    }
+    void update(int v, T val){
+        st.update(tin[v], val);
+    }
 };
