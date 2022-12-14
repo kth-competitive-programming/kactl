d208e1092c9be97490a9bc485b310ad5cfc43163
akcube <a.kishorekumar@students.iiit.ac.in>
2022-12-14 16:58:18 +0530

    Fixes #1 and #2

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -1,16 +1,28 @@
-/**
- * Note: Recheck the indexing for VAL_ON_EDGES = true. Haven't verified beyond testing on a CF problem.
- * NOT STRESS-TESTED
-*/
 
-template <typename T, typename F, bool VAL_ON_EDGES>
+/**
+ * Author: Benjamin Qi, Oleksandr Kulkov, chilli, Kishore Kumar
+ * Date: 2022-12-14
+ * License: CC0
+ * Source: Folklore
+ * Description: Decomposes a tree into vertex disjoint heavy paths and light
+ * edges such that the path from any leaf to the root contains at most log(n)
+ * light edges. Code allows for templatized types and merge functions for both
+ * path and subtree queries / updates. Modify Segtree to RangeUpdate tree or Lazy
+ * and update query / update functions if required. VALS\_EDGES being true means 
+ * that values are stored in the edges, as opposed to the nodes. All values 
+ * initialized to passed array values. Root must be 0.
+ * Time: O((\log N)^2)
+ * Status: Could use some more testing
+ */
+
+template <typename T, typename F, bool VAL_EDGES>
 struct HLD{
     int n, timer = 0;
     vi size, top, tin, dep, par;
     Segtree<T, F> st;
-    HLD(vvi &adj, const vector<T> &arr, T id, F _m) : n(sz(adj)), size(n, 1), top(n), tin(n), dep(n), par(n, -1){
+    vector<T> build(vvi &adj, const vector<T> &arr){
         function<void(int)> dfs_hvy = [&](int v){
-            if(adj[v][0] == par[v]) swap(adj[v].front(), adj[v].back());
+            if(sz(adj[v]) and adj[v][0] == par[v]) swap(adj[v].front(), adj[v].back());
             for(auto &to : adj[v]) if(to != par[v]) {
                 par[to] = v, dep[to] = dep[v] + 1;
                 dfs_hvy(to);
@@ -18,23 +30,25 @@ struct HLD{
                 if(size[to] > size[adj[v][0]]) swap(to, adj[v][0]);
             }
         }; dfs_hvy(0);
-        vector<T> temp; temp.reserve(n);
+        vector<T> temp(n);
         function<void(int)> dfs_hld = [&](int v){
-            tin[v] = timer++; temp.pb(arr[v]);
+            temp[timer] = arr[v], tin[v] = timer++;
             for(auto &to : adj[v]) if(to != par[v]) {
                 top[to] = (to == adj[v][0] ? top[v] : to);
                 dfs_hld(to);
             }
         }; dfs_hld(0);
-        st = Segtree<T, decltype(_m)>(temp, id, _m);
+        return temp;
     }
+    HLD(vvi &adj, const vector<T> &arr, T id, F _m) : n(sz(adj)), size(n, 1), top(n), 
+                        tin(n), dep(n), par(n, -1), st(build(adj, arr), id, _m){}
     template <class B> int process(int u, int v, B op) {
         for (; top[u] != top[v]; v = par[top[v]]) {
             if(dep[top[u]] > dep[top[v]]) swap(u, v);
             op(tin[top[v]], tin[v]);
         }
         if (dep[u] > dep[v]) swap(u, v);
-        if(!VAL_ON_EDGES or u!=v) op(tin[u] + VAL_ON_EDGES, tin[v]);
+        if(!VAL_EDGES or u!=v) op(tin[u] + VAL_EDGES, tin[v]);
         return u;
     }
     int lca(int u, int v) { return process(u, v, [&](int,int){}); }
@@ -48,4 +62,8 @@ struct HLD{
     void update(int v, T val){
         st.update(tin[v], val);
     }
-};
+    T query_subtree(int v) { // update is similar
+        return (size[v] > 1 or !VAL_EDGES) ? st.query(tin[v]+VAL_EDGES, tin[v]+size[v]) : 
+                                            st.identity; 
+    }
+}
