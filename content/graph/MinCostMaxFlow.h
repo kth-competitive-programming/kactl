7297bc272ca8157634f1a8f1404d589330006b23
Henry Cafaro <67771343+hjcafaroUC@users.noreply.github.com>
2023-02-02 10:54:59 -0600

    Update MinCostMaxFlow.h

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -1,93 +1,98 @@
-/**
- * Author: Stanford
- * Date: Unknown
- * Source: Stanford Notebook
- * Description: Min-cost max-flow. cap[i][j] != cap[j][i] is allowed; double edges are not.
- *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
- *  To obtain the actual flow, look at positive values only.
- * Status: Tested on kattis:mincostmaxflow, stress-tested against another implementation
- * Time: Approximately O(E^2)
- */
-#pragma once
+// N vertices, flow and cost are measured with type T
+template<int N, typename T>
+struct mincostflow {
 
-// #include <bits/extc++.h> /// include-line, keep-include
+    const T inf_T = numeric_limits<T>::max(), eps = 0; // set epsilon for non-integral types
+    const bool CYCLIC = true; // false will usually make it run faster, but requires a DAG
 
-const ll INF = numeric_limits<ll>::max() / 4;
-typedef vector<ll> VL;
+    struct flow_edge { int v, u; T f, w, c; };
+    vector<flow_edge> adj[N];
+    T l[N], pot[N];
+    int p[N], vis[N], s, t;
+    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<>> pq;
 
-struct MCMF {
-	int N;
-	vector<vi> ed, red;
-	vector<VL> cap, flow, cost;
-	vi seen;
-	VL dist, pi;
-	vector<pii> par;
+    // add an edge from i to j with capacity w and cost per unit flow c
+    void edge(int i, int j, T w, T c, bool directed = true) {
+        adj[i].push_back({j, sz(adj[j]), 0, w, c});
+        adj[j].push_back({i, sz(adj[i])-1, 0, 0, -c});
+        if (!directed) edge(j, i, w, c);
+    }
 
-	MCMF(int N) :
-		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
-		seen(N), dist(N), pi(N), par(N) {}
+    T dp(int v) {
+        if (vis[v] || v == s) return pot[v];
+        for (auto& e : adj[v])
+            if (adj[e.v][e.u].w != 0)
+                pot[v] = min(pot[v], dp(e.v)-e.c);
+        return vis[v] = 1, pot[v];
+    }
 
-	void addEdge(int from, int to, ll cap, ll cost) {
-		this->cap[from][to] = cap;
-		this->cost[from][to] = cost;
-		ed[from].push_back(to);
-		red[to].push_back(from);
-	}
+    bool dij() {
+        fill(l, l+N, inf_T), pq.push({l[s] = 0, s});
+        fill(vis, vis+N, 0);
+        while (!pq.empty()) {
+            auto [c, v] = pq.top(); pq.pop();
+            if (vis[v]) continue; vis[v] = 1;
+            for (auto& e : adj[v]) {
+                T d = e.c+pot[v]-pot[e.v];
+                if (l[v] != inf_T && e.f < e.w-eps && c+d < l[e.v]-eps)
+                    p[e.v] = e.u, pq.push({l[e.v] = c+d, e.v});
+            }
+        }
+        for (int i = 0; i < N; i++)
+            if (l[i] != inf_T) pot[i] += l[i];
+        return l[t] != inf_T;
+    }
 
-	void path(int s) {
-		fill(all(seen), 0);
-		fill(all(dist), INF);
-		dist[s] = 0; ll di;
+    bool spfa() {
+        fill(l, l+N, inf_T), l[s] = 0;
+        fill(vis, vis+N, 0);
+        queue<int> q({s});
+        while (!q.empty()) {
+            int v = q.front(); q.pop();
+            vis[v] = 0;
+            for (auto& e : adj[v])
+                if (l[v] != inf_T && e.f < e.w-eps && l[v]+e.c < l[e.v]-eps) {
+                    l[e.v] = l[v]+e.c, p[e.v] = e.u;
+                    if (!vis[e.v]) q.push(e.v), vis[e.v] = 1;
+                }
+        }
+        return l[t] != inf_T;
+    }
 
-		__gnu_pbds::priority_queue<pair<ll, int>> q;
-		vector<decltype(q)::point_iterator> its(N);
-		q.push({0, s});
+    // compute the maximum flow from s to t, and the minimum cost needed to do it, one-time use.
+    // for cyclic graphs: SPFA + Edmonds-Karp, complexity is O(EV*min(EV, flow))
+    // for DAGS: Dijkstra's w/ Johnson potentials + Edmonds-Karp, complexity is O(ElogE*min(EV, flow))
+    // negative weight cycles will always break it
+    pair<T, T> solve(int _s, int _t) {
+        s = _s, t = _t; T f = 0, c = 0;
+        fill(pot, pot+N, inf_T), pot[s] = 0;
+        if (!CYCLIC) dp(t);
+        while (CYCLIC ? spfa() : dij()) {
+            T x = inf_T;
+            for (int v = t; v != s; v = adj[v][p[v]].v) {
+                auto& e = adj[v][p[v]];
+                x = min(x, adj[e.v][e.u].w-adj[e.v][e.u].f);
+            }
+            for (int v = t; v != s; v = adj[v][p[v]].v) {
+                auto& e = adj[v][p[v]];
+                c -= e.c*x, e.f -= x, adj[e.v][e.u].f += x;
+            }
+            f += x;
+        }
+        return {f, c};
+    }
 
-		auto relax = [&](int i, ll cap, ll cost, int dir) {
-			ll val = di - pi[i] + cost;
-			if (cap && val < dist[i]) {
-				dist[i] = val;
-				par[i] = {s, dir};
-				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
-				else q.modify(its[i], {-dist[i], i});
-			}
-		};
-
-		while (!q.empty()) {
-			s = q.top().second; q.pop();
-			seen[s] = 1; di = dist[s] + pi[s];
-			for (int i : ed[s]) if (!seen[i])
-				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
-			for (int i : red[s]) if (!seen[i])
-				relax(i, flow[i][s], -cost[i][s], 0);
-		}
-		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
-	}
-
-	pair<ll, ll> maxflow(int s, int t) {
-		ll totflow = 0, totcost = 0;
-		while (path(s), seen[t]) {
-			ll fl = INF;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
-			totflow += fl;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				if (r) flow[p][x] += fl;
-				else flow[x][p] -= fl;
-		}
-		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
-		return {totflow, totcost};
-	}
-
-	// If some costs can be negative, call this before maxflow:
-	void setpi(int s) { // (otherwise, leave this out)
-		fill(all(pi), INF); pi[s] = 0;
-		int it = N, ch = 1; ll v;
-		while (ch-- && it--)
-			rep(i,0,N) if (pi[i] != INF)
-				for (int to : ed[i]) if (cap[i][to])
-					if ((v = pi[i] + cost[i][to]) < pi[to])
-						pi[to] = v, ch = 1;
-		assert(it >= 0); // negative cost cycle
-	}
+    // after calling solve(), return a component vector C s.t. C[i] \in {0, 1}
+    // and i is reachable from s iff C[i] = 1. any edge from a 1 to a 0 is in the min cut
+    vector<bool> cut() {
+        vector<bool> out(N, 0); out[s] = 1;
+        queue<int> q({s});
+        while (!q.empty()) {
+            int v = q.front(); q.pop();
+            for (auto& e : adj[v])
+                if (e.f < e.w && !out[e.v])
+                    out[e.v] = 1, q.push(e.v);
+        }
+        return out;
+    }
 };
