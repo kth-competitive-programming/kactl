808b9e0cd023f9a4dd8fa1d730198795d965f985
Henry Cafaro <67771343+hjcafaroUC@users.noreply.github.com>
2023-02-02 11:00:07 -0600

    Create XorBasis.h

---
+++ content/data-structures/XorBasis.h
@@ -0,0 +1,29 @@
+vector<ll> xor_basis(vector<ll>& a){
+    vector<ll> basis(64, 0);
+    for(auto x : a){
+        for(int i = 63; i >= 0; i--){
+            if(!(x & (1ll << i)))
+                continue;
+            if(basis[i]){
+                x ^= basis[i];
+            } else {
+                basis[i] = x;
+                break;
+            }
+        }
+    }
+    vector<ll> ret;
+    for(int i = 63; i >= 0; i--){
+        if(basis[i]){
+            for(auto& x : ret){
+                if(x & (1ll << i)){
+                    x ^= basis[i];
+                }
+            }
+            ret.push_back(basis[i]);
+        }
+    }
+    sort(all(ret));
+    reverse(all(ret));
+    return ret;
+}
