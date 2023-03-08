a5fca92b2189331d4ad95bf8a3d6fa23bac8c724
akcube <a.kishorekumar@students.iiit.ac.in>
2023-03-08 16:45:22 +0530

    Revised modint class

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -1,30 +1,38 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-09-28
+ * Author: Kishore Kumar
+ * Date: 2023-03-08
  * License: CC0
  * Source: folklore
- * Description: Operators for modular arithmetic. You need to set {\tt mod} to
- * some number first and then you can use the structure.
+ * Description: Operators for modular arithmetic. inv() works for any coprime mod. If mod is 
+ * 			prime, can make it faster by using fermats. 
  */
 #pragma once
 
-#include "euclid.h"
-
-const ll mod = 17; // change to something else
-struct Mod {
-	ll x;
-	Mod(ll xx) : x(xx) {}
-	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
-	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
-	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
-	Mod operator/(Mod b) { return *this * invert(b); }
-	Mod invert(Mod a) {
-		ll x, y, g = euclid(a.x, mod, x, y);
-		assert(g == 1); return Mod((x + mod) % mod);
-	}
-	Mod operator^(ll e) {
-		if (!e) return Mod(1);
-		Mod r = *this ^ (e / 2); r = r * r;
-		return e&1 ? *this * r : r;
-	}
+template<const int mod>
+struct mint {
+    mint(int x = 0) : val((x % mod + mod) % mod) {}
+    mint& operator+=(const mint &b) { val += b.val; val -= mod * (val >= mod); return *this; }
+    mint& operator-=(const mint &b) { val -= b.val; val += mod * (val < 0); return *this; }
+    mint& operator*=(const mint &b) { val = 1ll * val * b.val % mod; return *this; }
+    mint& operator/=(const mint &b) { return *this *= b.inv(); }
+    mint inv() const {
+        int x = 1, y = 0, t;
+        for(int a=val, b=mod; b; swap(a, b), swap(x, y))
+            t = a/b, a -= t * b, x -= t * y;
+        return mint(x);
+    }
+    mint pow(int b) const {
+    	mint a(val), res(1);
+    	for(; b; a = a*a, b /= 2)  if(b&1) res = a * res
+    	return res
+    }
+    friend mint operator+(const mint &a, const mint &b) { return mint(a) += b; }
+    friend mint operator-(const mint &a, const mint &b) { return mint(a) -= b; }
+    friend mint operator*(const mint &a, const mint &b) { return mint(a) *= b; }
+    friend mint operator/(const mint &a, const mint &b) { return mint(a) /= b; }
+    friend bool operator==(const mint &a, const mint &b) { return a.val == b.val; }
+    friend bool operator!=(const mint &a, const mint &b) { return a.val != b.val; }
+    friend std::ostream& operator<<(std::ostream &os, const mint &a) { return os << a.val; }
+    int val;
 };
+using Mint = mint<MOD>;
