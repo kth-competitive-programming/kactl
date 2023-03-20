4f43c49759f2f029e0dca94df40388aedace0464
beedle2017 <choudhuriarka@gmail.com>
2023-03-20 21:35:00 +0530

    Logo change, template change

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define first ff
+#define second ss
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
