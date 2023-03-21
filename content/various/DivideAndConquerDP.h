88f3223bb3252455b27b0ceee1bd6ef166afd4f7
Vidit Jain <viditjain@live.com>
2023-03-21 20:17:24 +0530

    Using slightly modified CP-Algorithms template for Divide and Conquer DP

--- content/various/DivideAndConquerDP.h
+++ content/various/DivideAndConquerDP.h
@@ -1,28 +1,33 @@
 /**
- * Author: Simon Lindholm
+ * Author: Vidit Jain 
  * License: CC0
- * Source: Codeforces
+ * Source: CP-Algorithms 
  * Description: Given $a[i] = \min_{lo(i) \le k < hi(i)}(f(i, k))$ where the (minimal) optimal $k$ increases with $i$, computes $a[i]$ for $i = L..R-1$.
  * Time: O((N + (hi-lo)) \log N)
- * Status: tested on http://codeforces.com/contest/321/problem/E
+ * Status: https://codeforces.com/contest/833/problem/B 
  */
 #pragma once
 
-struct DP { // Modify at will:
-	int lo(int ind) { return 0; }
-	int hi(int ind) { return ind; }
-	ll f(int ind, int k) { return dp[ind][k]; }
-	void store(int ind, int k, ll v) { res[ind] = pii(k, v); }
-
-	void rec(int L, int R, int LO, int HI) {
-		if (L >= R) return;
-		int mid = (L + R) >> 1;
-		pair<ll, int> best(LLONG_MAX, LO);
-		rep(k, max(LO,lo(mid)), min(HI,hi(mid)))
-			best = min(best, make_pair(f(mid, k), k));
-		store(mid, best.second, best.first);
-		rec(L, mid, LO, best.second+1);
-		rec(mid+1, R, best.second, HI);
-	}
-	void solve(int L, int R) { rec(L, R, INT_MIN, INT_MAX); }
+struct DP{
+    int n, m;
+    vi dp[2];
+    DP (int n, int m) : n(n), m(m), dp{vi(n), vi(n)}{}
+    int C(int i, int j, int ind) {return (i ? dp[ind^1][i-1] : 0));}
+    void rec(int l, int r, int optl, int optr, int ind) {
+        if (l > r) return;
+        int mid = (l + r) >> 1;
+        pii best = {-1, -1};
+        rep (k, optl, min(mid, optr)+1)
+            best = max(best, {C(k, mid, ind), k});
+        dp[ind][mid] = best.ff;
+        rec(l, mid-1, optl, best.ss, ind), rec(mid+1, r, best.ss, optr, ind);
+    }
+    int solve() {
+        rep(i, 0, n)
+            dp[0][i] = C(0, i, 0);
+        rep(i, 1, m)
+            rec(0, n-1, 0, n-1, i&1);
+        return dp[(m-1)&1][n-1];
+    }
 };
+
