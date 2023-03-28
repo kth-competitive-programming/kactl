9650a3d649ff608fbe5c27381a00036dda9fb52b
Vidit Jain <viditjain@live.com>
2023-03-28 17:59:48 +0530

    Modified 2sat

--- content/graph/2sat.h
+++ content/graph/2sat.h
@@ -3,15 +3,8 @@
  * Date: 2011-11-29
  * License: CC0
  * Source: folklore
- * Description: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem, so that an expression of the type $(a\|\|b)\&\&(!a\|\|c)\&\&(d\|\|!b)\&\&...$ becomes true, or reports that it is unsatisfiable.
- * Negated variables are represented by bit-inversions (\texttt{\tilde{}x}).
- * Usage:
- *  TwoSat ts(number of boolean variables);
- *  ts.either(0, \tilde3); // Var 0 is true or var 3 is false
- *  ts.setValue(2); // Var 2 is true
- *  ts.atMostOne({0,\tilde1,2}); // <= 1 of vars 0, \tilde1 and 2 are true
- *  ts.solve(); // Returns true iff it is solvable
- *  ts.values[0..N-1] holds the assigned values to the vars
+ * ExtDesc: Calculates a valid assignment to boolean variables a, b, c,... to a 2-SAT problem, so that an expression of the type $(a\|\|b)\&\&(!a\|\|c)\&\&(d\|\|!b)\&\&...$ becomes true, or reports that it is unsatisfiable.
+ * Description: Negated variables are represented by bit-inversions (\texttt{\tilde{}x}).
  * Time: O(N+E), where N is the number of boolean variables, and E is the number of clauses.
  * Status: stress-tested
  */
@@ -19,41 +12,44 @@
 
 struct TwoSat {
 	int N;
-	vector<vi> gr;
+	vvi gr;
 	vi values; // 0 = false, 1 = true
 
 	TwoSat(int n = 0) : N(n), gr(2*n) {}
 
-	int addVar() { // (optional)
+	int add_var() { // (optional)
 		gr.emplace_back();
 		gr.emplace_back();
 		return N++;
 	}
 
-	void either(int f, int j) {
+	void add_or(int f, int j) {
 		f = max(2*f, -1-2*f);
 		j = max(2*j, -1-2*j);
-		gr[f].pb(j^1);
-		gr[j].pb(f^1);
+		gr[f].push_back(j^1);
+		gr[j].push_back(f^1);
 	}
-	void setValue(int x) { either(x, x); }
 
-	void atMostOne(const vi& li) { // (optional)
+	void add_equiv(int f, int j) { add_or(f, ~j), add_or(~f, j); }
+	void add_xor(int f, int j) { add_or(f, j), add_or(~f, ~j); }
+	void add_set(int x) { add_or(x, x); }
+
+	void add_max_one(const vi& li) { // (optional)
 		if (sz(li) <= 1) return;
 		int cur = ~li[0];
 		rep(i,2,sz(li)) {
-			int next = addVar();
-			either(cur, ~li[i]);
-			either(cur, next);
-			either(~li[i], next);
+			int next = add_var();
+			add_or(cur, ~li[i]);
+			add_or(cur, next);
+			add_or(~li[i], next);
 			cur = ~next;
 		}
-		either(cur, ~li[1]);
+		add_or(cur, ~li[1]);
 	}
 
 	vi val, comp, z; int time = 0;
 	int dfs(int i) {
-		int low = val[i] = ++time, x; z.pb(i);
+		int low = val[i] = ++time, x; z.push_back(i);
 		for(int e : gr[i]) if (!comp[e])
 			low = min(low, val[e] ?: dfs(e));
 		if (low == val[i]) do {
