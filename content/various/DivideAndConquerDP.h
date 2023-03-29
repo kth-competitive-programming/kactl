3f052a68d66b19749b4a7d5f9dbf318224f7e9e0
Vidit Jain <viditjain@live.com>
2023-03-29 18:53:12 +0530

    Switch to tabwidth=2, reduce line spillage

--- content/various/DivideAndConquerDP.h
+++ content/various/DivideAndConquerDP.h
@@ -11,25 +11,26 @@
 
 template<typename T>
 struct DP{
-    int n, m;
-    T id;
-    vector<T> dp[2];
-    DP (int n, int m, T id) : n(n), m(m), id(id), dp{vector<T>(n), vector<T>(n)}{}
-    T C(int i, int j, int ind) {return (i ? dp[ind^1][i-1] : 0);}
-    void rec(int l, int r, int optl, int optr, int ind) {
-        if (l > r) return;
-        int mid = (l + r) >> 1;
-        pair<T,int> best = {id, optl};
-        rep (k, optl, min(mid, optr)+1)
-            best = min(best, {C(k, mid, ind), k});
-        dp[ind][mid] = best.ff;
-        rec(l, mid-1, optl, best.ss, ind), rec(mid+1, r, best.ss, optr, ind);
-    }
-    T solve() {
-        rep(i, 0, n)
-            dp[0][i] = C(0, i, 0);
-        rep(i, 1, m)
-            rec(0, n-1, 0, n-1, i&1);
-        return dp[(m-1)&1][n-1];
-    }
+	int n, m;
+	T id;
+	using vt = vector<T>;
+	vt dp[2];
+	DP(int n, int m, T id):n(n), m(m), id(id), dp{vt(n),vt(n)}{}
+	T C(int i, int j, int x) {return (i ? dp[x^1][i-1] : 0);}
+	void rec(int l, int r, int optl, int optr, int x) {
+		if (l > r) return;
+		int mid = (l + r) >> 1;
+		pair<T,int> best = {id, optl};
+		rep (k, optl, min(mid, optr)+1)
+			best = min(best, {C(k, mid, x), k});
+		dp[x][mid] = best.ff;
+		rec(l, mid-1, optl, best.ss, x), rec(mid+1, r, best.ss, optr, x);
+	}
+	T solve() {
+		rep(i, 0, n)
+			dp[0][i] = C(0, i, 0);
+		rep(i, 1, m)
+			rec(0, n-1, 0, n-1, i&1);
+		return dp[(m-1)&1][n-1];
+	}
 };
