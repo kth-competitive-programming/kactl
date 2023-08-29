3174ddda02c3367f7e3ed3538b48135c7ff0c1d8
FHVirus <froghackervirus@gmail.com>
2023-08-29 22:58:33 +0800

    New LazySegmentTree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,66 +1,64 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
+ * Author: FHVirus
+ * Date: 2023-08-29
  * License: CC0
- * Source: me
- * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
- * Can be changed to other things.
- * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
- * Time: O(\log N).
- * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Source: ac-library & https://omeletwithoutegg.github.io/2019/12/07/Iterative-SegmentTree/
+ * Description: ZKW implementation with ACL style nodes.
+ * Time: O(N + Q \log N).
+ * Usage: SGT<Val, Tag> sgt(n);
+ * Status: tested on Library Checker
  */
 #pragma once
 
-#include "../various/BumpAllocator.h"
+struct Val {
+  Val() {} // must return identity element   
+  Val operator + (const Val& o) const {}
+  // merge two Vals, order is important
+};
+struct Tag {
+  Tag() {} // must return identity element
+  Tag operator + (const Tag& o) const {}
+  // compose two Tags, order is important
+  Val operator() (Val v) const {}
+  // apply the Tag to v
+};
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0, val = -inf;
-	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) {
-		if (R <= lo || hi <= L) return -inf;
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) mset = val = x, madd = 0;
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) {
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x;
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+int bc(int u) { return u <= 1 ? 1 : (2 << __lg(u-1)); }
+template <class V, class T> struct SGT {
+  int n; vector<V> val; vector<T> tag;
+  SGT(int n): n(bc(n)), val(n*2), tag(n*2) {}
+  SGT(const vector<V>& v): n(bc(sz(v))), val(n*2), tag(n*2) {
+    rep (i, 0, sz(v)) val[i+n] = v[i];
+    for (int i = n; --i; ) val[i] = val[i*2] + val[i*2+1];
+  }
+  void upd(int u, T t)
+  { val[u] = t(val[u]); if (u < n) tag[u] = tag[u] + t; }
+  void pull(int u)
+  { while (u /= 2) val[u] = tag[u](val[u*2] + val[u*2+1]); }
+  void push(int u) {
+    for (int h = __lg(n)+1, i; --h;) {
+      i = u >> h;
+      upd(i * 2, tag[i]);
+      upd(i * 2 + 1, tag[i]);
+      tag[i] = T();
+    }
+  }
+  void set(int p, V v)
+  { push(p += n); val[p] = v; pull(p); }
+  V query(int l, int r) {
+    V rl, rr;
+    for (push(l+=n), push((r+=n)-1); l < r; l /= 2, r /= 2) {
+      if (l & 1) rl = rl + val[l++];
+      if (r & 1) rr = val[--r] + rr;
+    }
+    return rl + rr;
+  }
+  void modify(int l, int r, T t) {
+    int tl = (l += n), tr = (r += n) - 1;
+    for (push(tl), push(tr); l < r; l >>= 1, r >>= 1) {
+      if (l & 1) upd(l++, t);
+      if (r & 1) upd(--r, t);
+    }
+    pull(tl); pull(tr);
+  }
 };
