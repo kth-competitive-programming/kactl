2cef01de50d883e3a28ae0b822158cea5927d7f8
Joshc88 <joshualiern@gmail.com>
2023-11-23 11:36:32 +0000

    Change team name & LCT

--- content/graph/LinkCutTree.h
+++ content/graph/LinkCutTree.h
@@ -1,7 +1,7 @@
 /**
- * Author: Simon Lindholm
+ * Author: Lucian Bicsi
  * Date: 2016-07-25
- * Source: https://github.com/ngthanhtrung23/ACM_Notebook_new/blob/master/DataStructure/LinkCut.h
+ * Source: https://codeforces.com/blog/entry/75885
  * Description: Represents a forest of unrooted trees. You can add and remove
  * edges (as long as the result is still a forest), and check whether
  * two nodes are in the same tree.
@@ -10,93 +10,108 @@
  */
 #pragma once
 
-struct Node { // Splay tree. Root's pp contains tree's parent.
-	Node *p = 0, *pp = 0, *c[2];
-	bool flip = 0;
-	Node() { c[0] = c[1] = 0; fix(); }
-	void fix() {
-		if (c[0]) c[0]->p = this;
-		if (c[1]) c[1]->p = this;
-		// (+ update sum of subtree elements etc. if wanted)
-	}
-	void pushFlip() {
-		if (!flip) return;
-		flip = 0; swap(c[0], c[1]);
-		if (c[0]) c[0]->flip ^= 1;
-		if (c[1]) c[1]->flip ^= 1;
-	}
-	int up() { return p ? p->c[1] == this : -1; }
-	void rot(int i, int b) {
-		int h = i ^ b;
-		Node *x = c[i], *y = b == 2 ? x : x->c[h], *z = b ? y : x;
-		if ((y->p = p)) p->c[up()] = y;
-		c[i] = z->c[i ^ 1];
-		if (b < 2) {
-			x->c[h] = y->c[h ^ 1];
-			z->c[h ^ 1] = b ? x : this;
-		}
-		y->c[i ^ 1] = b ? this : x;
-		fix(); x->fix(); y->fix();
-		if (p) p->fix();
-		swap(pp, y->pp);
-	}
-	void splay() { /// Splay this up to the root. Always finishes without flip set.
-		for (pushFlip(); p; ) {
-			if (p->p) p->p->pushFlip();
-			p->pushFlip(); pushFlip();
-			int c1 = up(), c2 = p->up();
-			if (c2 == -1) p->rot(c1, 2);
-			else p->p->rot(c2, c1 != c2);
-		}
-	}
-	Node* first() { /// Return the min element of the subtree rooted at this, splayed to the top.
-		pushFlip();
-		return c[0] ? c[0]->first() : (splay(), this);
-	}
+struct SplayTree {
+  struct Node {
+    int ch[2] = {0, 0}, p = 0;
+    long long self = 0, path = 0;        // Path aggregates
+    long long sub = 0, vir = 0;          // Subtree aggregates
+    bool flip = 0;                       // Lazy tags
+  };
+  vector<Node> T;
+ 
+  SplayTree(int n) : T(n + 1) {}
+  
+  void push(int x) {
+    if (!x || !T[x].flip) return;
+    int l = T[x].ch[0], r = T[x].ch[1];
+ 
+    T[l].flip ^= 1, T[r].flip ^= 1;
+    swap(T[x].ch[0], T[x].ch[1]);
+    T[x].flip = 0;
+  }
+  
+  void pull(int x) {
+    int l = T[x].ch[0], r = T[x].ch[1]; push(l); push(r); 
+ 
+    T[x].path = T[l].path + T[x].self + T[r].path;
+    T[x].sub = T[x].vir + T[l].sub + T[r].sub + T[x].self;
+  }
+  
+  void set(int x, int d, int y) {
+    T[x].ch[d] = y; T[y].p = x; pull(x); 
+  }
+ 
+  void splay(int x) { 
+    auto dir = [&](int x) {
+      int p = T[x].p; if (!p) return -1;
+      return T[p].ch[0] == x ? 0 : T[p].ch[1] == x ? 1 : -1;
+    };
+    auto rotate = [&](int x) {
+      int y = T[x].p, z = T[y].p, dx = dir(x), dy = dir(y);
+      set(y, dx, T[x].ch[!dx]); 
+      set(x, !dx, y);
+      if (~dy) set(z, dy, x); 
+      T[x].p = z;
+    };
+    for (push(x); ~dir(x); ) {
+      int y = T[x].p, z = T[y].p;
+      push(z); push(y); push(x);
+      int dx = dir(x), dy = dir(y);
+      if (~dy) rotate(dx != dy ? x : y);
+      rotate(x);
+    }
+  }
 };
-
-struct LinkCut {
-	vector<Node> node;
-	LinkCut(int N) : node(N) {}
-
-	void link(int u, int v) { // add an edge (u, v)
-		assert(!connected(u, v));
-		makeRoot(&node[u]);
-		node[u].pp = &node[v];
-	}
-	void cut(int u, int v) { // remove an edge (u, v)
-		Node *x = &node[u], *top = &node[v];
-		makeRoot(top); x->splay();
-		assert(top == (x->pp ?: x->c[0]));
-		if (x->pp) x->pp = 0;
-		else {
-			x->c[0] = top->p = 0;
-			x->fix();
-		}
-	}
-	bool connected(int u, int v) { // are u, v in the same tree?
-		Node* nu = access(&node[u])->first();
-		return nu == access(&node[v])->first();
-	}
-	void makeRoot(Node* u) { /// Move u to root of represented tree.
-		access(u);
-		u->splay();
-		if(u->c[0]) {
-			u->c[0]->p = 0;
-			u->c[0]->flip ^= 1;
-			u->c[0]->pp = u;
-			u->c[0] = 0;
-			u->fix();
-		}
-	}
-	Node* access(Node* u) { /// Move u to root aux tree. Return the root of the root aux tree.
-		u->splay();
-		while (Node* pp = u->pp) {
-			pp->splay(); u->pp = 0;
-			if (pp->c[1]) {
-				pp->c[1]->p = 0; pp->c[1]->pp = pp; }
-			pp->c[1] = u; pp->fix(); u = pp;
-		}
-		return u;
-	}
+ 
+struct LinkCut : SplayTree {
+  LinkCut(int n) : SplayTree(n) {}
+ 
+  int access(int x) {
+    int u = x, v = 0;
+    for (; u; v = u, u = T[u].p) {
+      splay(u); 
+      int& ov = T[u].ch[1];
+      T[u].vir += T[ov].sub;
+      T[u].vir -= T[v].sub;
+      ov = v; pull(u);
+    }
+    return splay(x), v;
+  }
+ 
+  void reroot(int x) { 
+    access(x); T[x].flip ^= 1; push(x); 
+  }
+  
+  void Link(int u, int v) { 
+    reroot(u); access(v); 
+    T[v].vir += T[u].sub;
+    T[u].p = v; pull(v);
+  }
+  
+  void Cut(int u, int v) {
+    reroot(u); access(v);
+    T[v].ch[0] = T[u].p = 0; pull(v);
+  }
+  
+  // Rooted tree LCA. Returns 0 if u and v arent connected.
+  int LCA(int u, int v) { 
+    if (u == v) return u;
+    access(u); int ret = access(v); 
+    return T[u].p ? ret : 0;
+  }
+  
+  // Query subtree of u where v is outside the subtree.
+  long long Subtree(int u, int v) {
+    reroot(v); access(u); return T[u].vir + T[u].self;
+  }
+  
+  // Query path [u..v]
+  long long Path(int u, int v) {
+    reroot(u); access(v); return T[v].path;
+  }
+  
+  // Update vertex u with value v
+  void Update(int u, long long v) {
+    access(u); T[u].self = v; pull(u);
+  }
 };
