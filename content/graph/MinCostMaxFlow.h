2f16294e65df8858412d8211257e792bb9c0dfd2
matistjati <matistjati@outlook.com>
2023-12-23 04:37:11 +0100

    Improved MCMF

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -16,78 +16,80 @@ const ll INF = numeric_limits<ll>::max() / 4;
 typedef vector<ll> VL;
 
 struct MCMF {
+	struct edge
+	{
+		int from, to, rev;
+		ll cap, cost, flow;
+	};
 	int N;
-	vector<vi> ed, red;
-	vector<VL> cap, flow, cost;
-	vi seen;
-	VL dist, pi;
-	vector<pii> par;
+	vector<vector<edge>> ed;
+	vi seen, ind;
+	vi dist, pi;
+	vector<edge*> par;
 
 	MCMF(int N) :
-		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
-		seen(N), dist(N), pi(N), par(N) {}
+		N(N), ed(N), seen(N), dist(N), pi(N), par(N) {}
 
 	void addEdge(int from, int to, ll cap, ll cost) {
-		this->cap[from][to] = cap;
-		this->cost[from][to] = cost;
-		ed[from].push_back(to);
-		red[to].push_back(from);
+		if (from == to) return;
+		ed[from].emplace_back(edge{ from,to,sz(ed[to]),cap,cost,0});
+		ed[to].emplace_back(edge{ to,from,sz(ed[from])-1,0,-cost,0});
 	}
 
 	void path(int s) {
 		fill(all(seen), 0);
-		fill(all(dist), INF);
+		fill(all(dist), inf);
 		dist[s] = 0; ll di;
 
+		//priority_queue<pair<ll, int>> q;
 		__gnu_pbds::priority_queue<pair<ll, int>> q;
 		vector<decltype(q)::point_iterator> its(N);
-		q.push({0, s});
-
-		auto relax = [&](int i, ll cap, ll cost, int dir) {
-			ll val = di - pi[i] + cost;
-			if (cap && val < dist[i]) {
-				dist[i] = val;
-				par[i] = {s, dir};
-				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
-				else q.modify(its[i], {-dist[i], i});
-			}
-		};
+		q.push({ 0, s });
 
 		while (!q.empty()) {
 			s = q.top().second; q.pop();
 			seen[s] = 1; di = dist[s] + pi[s];
-			for (int i : ed[s]) if (!seen[i])
-				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
-			for (int i : red[s]) if (!seen[i])
-				relax(i, flow[i][s], -cost[i][s], 0);
+			for (edge& e : ed[s]) if (!seen[e.to])
+			{
+				ll val = di - pi[e.to] + e.cost;
+				if (e.cap - e.flow > 0 && val < dist[e.to]) {
+					dist[e.to] = val;
+					par[e.to] = &e;
+					if (its[e.to] == q.end()) its[e.to] = q.push({ -dist[e.to], e.to });
+					else q.modify(its[e.to], { -dist[e.to], e.to });
+				}
+			}
 		}
-		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
+		rep(i, 0, N) pi[i] = min(pi[i] + dist[i], inf);
 	}
 
 	pair<ll, ll> maxflow(int s, int t) {
 		ll totflow = 0, totcost = 0;
 		while (path(s), seen[t]) {
-			ll fl = INF;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
+			ll fl = inf;
+			for (int p, x = t; par[x] && (p = par[x]->from, x != s); x = p)
+				fl = min(fl, par[x]->cap - par[x]->flow);
+
 			totflow += fl;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				if (r) flow[p][x] += fl;
-				else flow[x][p] -= fl;
+			for (int p, x = t; par[x] && (p = par[x]->from, x != s); x = p)
+			{
+				par[x]->flow += fl;
+				ed[par[x]->to][par[x]->rev].flow -= fl;
+			}
 		}
-		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
-		return {totflow, totcost};
+		rep(i, 0, N) for (edge& e : ed[i]) totcost += e.cost * e.flow;
+		return { totflow, totcost/2 };
 	}
 
 	// If some costs can be negative, call this before maxflow:
 	void setpi(int s) { // (otherwise, leave this out)
-		fill(all(pi), INF); pi[s] = 0;
+		fill(all(pi), inf); pi[s] = 0;
 		int it = N, ch = 1; ll v;
 		while (ch-- && it--)
-			rep(i,0,N) if (pi[i] != INF)
-				for (int to : ed[i]) if (cap[i][to])
-					if ((v = pi[i] + cost[i][to]) < pi[to])
-						pi[to] = v, ch = 1;
+			rep(i, 0, N) if (pi[i] != inf)
+				for (edge& e : ed[i]) if (e.cap)
+					if ((v = pi[i] + e.cost) < pi[e.to])
+						pi[e.to] = v, ch = 1;
 		assert(it >= 0); // negative cost cycle
 	}
 };
