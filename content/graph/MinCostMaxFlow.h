dfb9bc8c4b598d08644ff37aa1a03a2bfb8ad1c9
matistjati <matistjati@outlook.com>
2023-12-23 04:38:46 +0100

    Fixed MCMF complexity description

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -6,7 +6,7 @@
  *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
  *  To obtain the actual flow, look at positive values only.
  * Status: Tested on kattis:mincostmaxflow, stress-tested against another implementation
- * Time: Approximately O(E^2)
+ * Time: $O(F \cdot Elog(N))$ if all costs are positive, where F is max flow. Otherwise $O(F \cdot Elog(N)+VE)$
  */
 #pragma once
 
