f65c8b36c9adf667049b5ef9a5856a2a4e2efcd4
matistjati <matistjati@outlook.com>
2024-01-03 23:27:24 +0100

    Fixed line overflows

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -29,8 +29,8 @@ struct MCMF {
 
 	void addEdge(int from, int to, ll cap, ll cost) {
 		if (from == to) return;
-		ed[from].emplace_back(edge{ from,to,sz(ed[to]),cap,cost,0});
-		ed[to].emplace_back(edge{ to,from,sz(ed[from])-1,0,-cost,0});
+		ed[from].push_back(edge{ from,to,sz(ed[to]),cap,cost,0 });
+		ed[to].push_back(edge{ to,from,sz(ed[from]) - 1,0,-cost,0 });
 	}
 
 	void path(int s) {
@@ -50,29 +50,32 @@ struct MCMF {
 				if (e.cap - e.flow > 0 && val < dist[e.to]) {
 					dist[e.to] = val;
 					par[e.to] = &e;
-					if (its[e.to] == q.end()) its[e.to] = q.push({ -dist[e.to], e.to });
-					else q.modify(its[e.to], { -dist[e.to], e.to });
+					if (its[e.to] == q.end())
+						its[e.to] = q.push({ -dist[e.to], e.to });
+					else
+						q.modify(its[e.to], { -dist[e.to], e.to });
 				}
 			}
 		}
-		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
+		rep(i, 0, N) pi[i] = min(pi[i] + dist[i], INF);
 	}
 
 	pair<ll, ll> maxflow(int s, int t) {
 		ll totflow = 0, totcost = 0;
 		while (path(s), seen[t]) {
 			ll fl = INF;
-			for (int p, x = t; par[x] && (p = par[x]->from, x != s); x = p)
-				fl = min(fl, par[x]->cap - par[x]->flow);
+			
+			for (edge* x = par[t]; x; (x = par[x->from]))
+				fl = min(fl, x->cap - x->flow);
 
 			totflow += fl;
-			for (int p, x = t; par[x] && (p = par[x]->from, x != s); x = p) {
-				par[x]->flow += fl;
-				ed[par[x]->to][par[x]->rev].flow -= fl;
+			for (edge* x = par[t]; x; (x = par[x->from])) {
+				x->flow += fl;
+				ed[x->to][x->rev].flow -= fl;
 			}
 		}
-		rep(i,0,N) for (edge& e : ed[i]) totcost += e.cost * e.flow;
-		return {totflow, totcost/2};
+		rep(i, 0, N) for(edge& e : ed[i]) totcost += e.cost*e.flow;
+		return { totflow, totcost / 2 };
 	}
 
 	// If some costs can be negative, call this before maxflow:
@@ -80,10 +83,10 @@ struct MCMF {
 		fill(all(pi), INF); pi[s] = 0;
 		int it = N, ch = 1; ll v;
 		while (ch-- && it--)
-			rep(i,0,N) if (pi[i] != INF)
-				for (edge& e : ed[i]) if (e.cap)
-					if ((v = pi[i] + e.cost) < pi[e.to])
-						pi[e.to] = v, ch = 1;
+			rep(i, 0, N) if (pi[i] != INF)
+			for (edge& e : ed[i]) if (e.cap)
+				if ((v = pi[i] + e.cost) < pi[e.to])
+					pi[e.to] = v, ch = 1;
 		assert(it >= 0); // negative cost cycle
 	}
 };
