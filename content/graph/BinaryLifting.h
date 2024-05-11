d6c796d8f542d01acd4a7e585d3a313f9bfb62a5
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-05-11 20:13:22 -0300

    Use `__lg` to make `treeJump` shorter
    It breaks the case when P is empty, but thats not an useful case

--- content/graph/BinaryLifting.h
+++ content/graph/BinaryLifting.h
@@ -5,15 +5,14 @@
  * Source: Folklore
  * Description: Calculate power of two jumps in a tree,
  * to support fast upward jumps and LCAs.
- * Assumes the root node points to itself.
+ * Assumes that P is not empty and that the root node points to itself.
  * Time: construction $O(N \log N)$, queries $O(\log N)$
  * Status: Tested at Petrozavodsk, also stress-tested via LCA.cpp
  */
 #pragma once
 
 vector<vi> treeJump(vi& P){
-	int on = 1, d = 1;
-	while(on < sz(P)) on *= 2, d++;
+	int d = 1 + __lg(sz(P) - 1);
 	vector<vi> jmp(d, P);
 	rep(i,1,d) rep(j,0,sz(P))
 		jmp[i][j] = jmp[i-1][jmp[i-1][j]];
