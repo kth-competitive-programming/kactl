57c92891599a5fe5e9fe277916dba8ccb677990c
Sachin S <ssk4988@gmail.com>
2024-09-19 18:29:30 -0400

    Fix undefined behavior in Suffix Array (#264)

--- content/strings/SuffixArray.h
+++ content/strings/SuffixArray.h
@@ -21,8 +21,8 @@ struct SuffixArray {
 	vi sa, lcp;
 	SuffixArray(string& s, int lim=256) { // or basic_string<int>
 		int n = sz(s) + 1, k = 0, a, b;
-		vi x(all(s)+1), y(n), ws(max(n, lim)), rank(n);
-		sa = lcp = y, iota(all(sa), 0);
+		vi x(all(s)), y(n), ws(max(n, lim)), rank(n);
+		x.push_back(0), sa = lcp = y, iota(all(sa), 0);
 		for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
 			p = j, iota(all(y), n - j);
 			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
