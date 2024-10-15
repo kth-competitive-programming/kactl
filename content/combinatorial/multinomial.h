dccc967e101ff0660c4cd41426c6a7b132d0256c
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-10-15 15:18:50 -0300

    Make multinomial shorter (#272)

--- content/combinatorial/multinomial.h
+++ content/combinatorial/multinomial.h
@@ -9,7 +9,6 @@
 
 ll multinomial(vi& v) {
 	ll c = 1, m = v.empty() ? 1 : v[0];
-	rep(i,1,sz(v)) rep(j,0,v[i])
-		c = c * ++m / (j+1);
+	rep(i,1,sz(v)) rep(j,0,v[i]) c = c * ++m / (j+1);
 	return c;
 }
