53a1582f017a84f102fb9d6202c2faa33812081f
Luke Videckis <lukevideckis@gmail.com>
2024-10-23 14:08:40 -0700

    more golf

--- content/graph/TopoSort.h
+++ content/graph/TopoSort.h
@@ -15,11 +15,7 @@ vi topoSort(const vector<vi>& gr) {
 	vi indeg(sz(gr)), q;
 	for (auto& li : gr) for (int x : li) indeg[x]++;
 	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push_back(i);
-	int j = 0;
-	while (j < sz(q)) {
-		int i = q[j++];
-		for (int x : gr[i])
-			if (--indeg[x] == 0) q.push_back(x);
-	}
+	rep(j,0,sz(q)) for (int x : gr[q[j]])
+		if (--indeg[x] == 0) q.push_back(x);
 	return q;
 }
