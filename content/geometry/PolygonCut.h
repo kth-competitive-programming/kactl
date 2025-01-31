26106590a0c0938cd261dabccf835453e3c6552d
user202729 <25191436+user202729@users.noreply.github.com>
2025-02-01 06:07:36 +0700

    Fix some issues that may cause floating point issues (#284)

--- content/geometry/PolygonCut.h
+++ content/geometry/PolygonCut.h
@@ -27,10 +27,10 @@ vector<P> polygonCut(const vector<P>& poly, P s, P e) {
 	vector<P> res;
 	rep(i,0,sz(poly)) {
 		P cur = poly[i], prev = i ? poly[i-1] : poly.back();
-		bool side = s.cross(e, cur) < 0;
-		if (side != (s.cross(e, prev) < 0))
-			res.push_back(lineInter(s, e, cur, prev).second);
-		if (side)
+		auto a = s.cross(e, cur), b = s.cross(e, prev);
+		if ((a < 0) != (b < 0))
+			res.push_back(cur + (prev - cur) * (a / (a - b)));
+		if (a < 0)
 			res.push_back(cur);
 	}
 	return res;
