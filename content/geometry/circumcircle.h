95a49837f7797446da7a88f7caeed988fb3c4e7c
Matistjati <matistjati@outlook.com>
2026-01-03 19:07:21 +0100

    Pass points by value

--- content/geometry/circumcircle.h
+++ content/geometry/circumcircle.h
@@ -18,11 +18,11 @@ The circumcirle of a triangle is the circle intersecting all three vertices. ccR
 #include "Point.h"
 
 typedef Point<double> P;
-double ccRadius(const P& A, const P& B, const P& C) {
+double ccRadius(P A, P B, P C) {
 	return (B-A).dist()*(C-B).dist()*(A-C).dist()/
 			abs((B-A).cross(C-A))/2;
 }
-P ccCenter(const P& A, const P& B, const P& C) {
+P ccCenter(P A, P B, P C) {
 	P b = C-A, c = B-A;
 	return A + (b*c.dist2()-c*b.dist2()).perp()/b.cross(c)/2;
 }
--- content/geometry/lineDistance.h
+++ content/geometry/lineDistance.h
@@ -21,6 +21,6 @@ Using Point3D will always give a non-negative distance. For Point3D, call .dist 
 #include "Point.h"
 
 template<class P>
-double lineDist(const P& a, const P& b, const P& p) {
+double lineDist(P a, P b, P p) {
 	return (double)(b-a).cross(p-a)/(b-a).dist();
 }
